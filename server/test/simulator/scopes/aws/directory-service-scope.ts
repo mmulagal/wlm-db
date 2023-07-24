6f04dc8104235cd85f9f8631abbf3e5a701f63ad
Yadav, Sonam <Sonam.Sonam@netapp.com>
2023-07-24 15:56:34 +0530

    Simulator code

---
+++ server/test/simulator/scopes/aws/directory-service-scope.ts
@@ -0,0 +1,10 @@
+import { 
+    DirectoryServiceClient, 
+    DescribeDirectoriesCommand 
+} from "@aws-sdk/client-directory-service";
+import { mockClient } from 'aws-sdk-client-mock';
+import adsResponse from '../../responses/aws/list-ads.json';
+
+const dsMock = mockClient(DirectoryServiceClient);
+
+dsMock.on(DescribeDirectoriesCommand).resolves(adsResponse);
