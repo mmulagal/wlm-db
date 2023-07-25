67738da7f32df227fed47bb6ec5892ab5307798f
rpanwar@netapp.com <rpanwar@netapp.com>
2023-07-25 15:37:35 +0530

    DBS-302 ui implementation of operating system accordion

--- ui/src/components/ApplicationSettings/OperatingSystem/OperatingSystem.tsx
+++ ui/src/components/ApplicationSettings/OperatingSystem/OperatingSystem.tsx
@@ -1,27 +1,48 @@
-import { AccordionCard, AccordionCardContent } from "@netapp/design-system";
-import { GENERAL } from "../../../utils/appConstants";
-import CommonStyles from "../../../utils/CommonStyles.module.scss";
+import { AccordionCard, AccordionCardContent, RadioButton, Typography } from '@netapp/design-system';
+import { GENERAL } from '../../../utils/appConstants';
+import styles from './OperatingSystem.module.scss';
+import CommonStyles from '../../../utils/CommonStyles.module.scss';
+import { useState } from 'react';
 
 const OperatingSystem = () => {
-  //Set the Header text here
-  const setHeader = () => {
-    return ["Windows server 2016"];
-  };
-  return (
-    <div className={""}>
-      <AccordionCard
-        ValueContent={() => (
-          <div className={CommonStyles["heading-content"]}>{setHeader()}</div>
-        )}
-        id="5"
-        title={
-          <div className={CommonStyles.title}>{GENERAL.OPERATING_SYSTEM}</div>
-        }
-      >
-        <AccordionCardContent>Content here</AccordionCardContent>
-      </AccordionCard>
-    </div>
-  );
+    const [operatingSystem, setOperatingSystem] = useState(GENERAL.WIN_SERVER_2016);
+    //Set the Header text here
+    const setHeader = () => {
+        return operatingSystem;
+    };
+    return (
+        <div className={styles['operating-system']}>
+            <AccordionCard
+                ValueContent={() => <div className={CommonStyles['heading-content']}>{setHeader()}</div>}
+                id="5"
+                title={<div className={CommonStyles.title}>{GENERAL.OPERATING_SYSTEM}</div>}
+            >
+                <AccordionCardContent>
+                    <Typography>
+                        <div className={styles.text}>{GENERAL.OP_SYS_TEXT}</div>
+                        <div className={styles.handleRadio}>
+                            <RadioButton
+                                isChecked={operatingSystem === GENERAL.WIN_SERVER_2016}
+                                onChange={() => {
+                                    setOperatingSystem(GENERAL.WIN_SERVER_2016);
+                                }}
+                                children={GENERAL.WIN_SERVER_2016}
+                                className=""
+                            />
+                            <RadioButton
+                                isChecked={operatingSystem === GENERAL.WIN_SERVER_2019}
+                                onChange={() => {
+                                    setOperatingSystem(GENERAL.WIN_SERVER_2019);
+                                }}
+                                children={GENERAL.WIN_SERVER_2019}
+                                className=""
+                            />
+                        </div>
+                    </Typography>
+                </AccordionCardContent>
+            </AccordionCard>
+        </div>
+    );
 };
 
 export default OperatingSystem;
