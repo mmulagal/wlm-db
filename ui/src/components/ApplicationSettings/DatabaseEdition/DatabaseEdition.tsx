d1b46eba5f09e26406e743b563db1770885334aa
rpanwar@netapp.com <rpanwar@netapp.com>
2023-07-25 17:57:06 +0530

    DBS-304 ui implementation of DB edition accordion

--- ui/src/components/ApplicationSettings/DatabaseEdition/DatabaseEdition.tsx
+++ ui/src/components/ApplicationSettings/DatabaseEdition/DatabaseEdition.tsx
@@ -1,27 +1,70 @@
-import { AccordionCard, AccordionCardContent } from "@netapp/design-system";
-import { GENERAL } from "../../../utils/appConstants";
-import CommonStyles from "../../../utils/CommonStyles.module.scss";
+import { useState } from 'react';
+import { AccordionCard, AccordionCardContent, RadioButton, Typography } from '@netapp/design-system';
+import { GENERAL } from '../../../utils/appConstants';
+import styles from './DatabaseEdition.module.scss';
+import CommonStyles from '../../../utils/CommonStyles.module.scss';
 
 const DatabaseEdition = () => {
-  //Set the Header text here
-  const setHeader = () => {
-    return ["Windows server 2016"];
-  };
-  return (
-    <div className={""}>
-      <AccordionCard
-        ValueContent={() => (
-          <div className={CommonStyles["heading-content"]}>{setHeader()}</div>
-        )}
-        id="7"
-        title={
-          <div className={CommonStyles.title}>{GENERAL.DATABASE_EDITION}</div>
-        }
-      >
-        <AccordionCardContent>Content here</AccordionCardContent>
-      </AccordionCard>
-    </div>
-  );
+    const [dbEdition, setDbEdition] = useState(GENERAL.SQL_SERVER_STANDARD_EDITION);
+    //Set the Header text here
+    const setHeader = () => {
+        return ['Windows server 2016'];
+    };
+    return (
+        <div className={styles['db-edition']}>
+            <AccordionCard
+                ValueContent={() => <div className={CommonStyles['heading-content']}>{setHeader()}</div>}
+                id="7"
+                title={<div className={CommonStyles.title}>{GENERAL.DATABASE_EDITION}</div>}
+            >
+                <AccordionCardContent>
+                    <Typography>
+                        <div className={styles.genericStyle}>
+                            <RadioButton
+                                isChecked={dbEdition === GENERAL.SQL_SERVER_STANDARD_EDITION}
+                                onChange={() => {
+                                    setDbEdition(GENERAL.SQL_SERVER_STANDARD_EDITION);
+                                }}
+                                children={GENERAL.SQL_SERVER_STANDARD_EDITION}
+                                className={styles.radio}
+                            />
+                            <Typography variant="Regular_14" className={styles.textEdition}>
+                                {GENERAL.SQL_SERVER_STANDARD_EDITION_TEXT}
+                            </Typography>
+                        </div>
+                        <div className={styles.genericStyle}>
+                            <RadioButton
+                                isChecked={dbEdition === GENERAL.SQL_SERVER_WEB_EDITION}
+                                onChange={() => {
+                                    setDbEdition(GENERAL.SQL_SERVER_WEB_EDITION);
+                                }}
+                                children={GENERAL.SQL_SERVER_WEB_EDITION}
+                                className={styles.radio}
+                            />
+                            <Typography variant="Regular_14" className={styles.textEdition}>
+                                {GENERAL.SQL_SERVER_WEB_EDITION_TEXT}
+                            </Typography>
+                        </div>
+                        <div className={styles.genericStyle}>
+                            <RadioButton
+                                isChecked={dbEdition === GENERAL.SQL_SERVER_ENTERPRiSE_EDITION}
+                                onChange={() => {
+                                    setDbEdition(GENERAL.SQL_SERVER_ENTERPRiSE_EDITION);
+                                }}
+                                children={GENERAL.SQL_SERVER_ENTERPRiSE_EDITION}
+                                className={styles.radio}
+                            />
+                            <Typography variant="Regular_14" className={styles.textEdition}>
+                                {GENERAL.SQL_SERVER_ENTERPRiSE_EDITION_TEXT}
+                            </Typography>
+                            <div className={styles.separator} />
+                            <div className={styles.AddSpace} />
+                        </div>
+                    </Typography>
+                </AccordionCardContent>
+            </AccordionCard>
+        </div>
+    );
 };
 
 export default DatabaseEdition;
