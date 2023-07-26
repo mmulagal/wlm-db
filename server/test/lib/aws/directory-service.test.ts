b2583817f86f9d4776a37ece97a4d3f78b7dd100
srigowri <srigowri@netapp.com>
2023-07-26 09:34:08 +0530

    DBS-278 - List AD for a region and VPC - credentials scope

--- server/test/lib/aws/directory-service.test.ts
+++ server/test/lib/aws/directory-service.test.ts
@@ -1,18 +1,10 @@
 import { faker } from '@faker-js/faker';
 import { describeDirectories } from '../../../src/lib/aws/directory-service';
-import { cloudManagerAwsCredentials } from '../../simulator/scopes/cloud-manager/cloud-manager-credentials-scope';
+import '../../simulator/scopes/cloud-manager/cloud-manager-credentials-scope';
 import adsResponse from '../../simulator/responses/aws/list-ads.json';
 import '../../simulator/scopes/aws/directory-service-scope';
 
 const credentialsid = `${faker.string.alphanumeric(20)}`;
-
-vi.mock('../../../src/lib/cloud-manager/credentials', async () => {
-    return {
-        getCredentialDetails() {
-            return cloudManagerAwsCredentials;
-        }
-    };
-});
 
 describe('describeDirectories method', () => {
     it('describeDirectories method should return mock data', async () => {
