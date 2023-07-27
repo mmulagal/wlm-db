eeea8e149aa3d1e076ee4a981c33bd5071ce6000
Yadav, Sonam <Sonam.Sonam@netapp.com>
2023-07-27 16:19:06 +0530

    type folder added

--- ui/src/store/mssql/mssqlSlice.ts
+++ ui/src/store/mssql/mssqlSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { AD, Ami, Credentials, VPC } from "../../utils/mssqlTypes";
+import { AD, Ami, Credentials, VPC } from "../../utils/types/mssqlTypes";
 
 interface mssqlEntities {
     getCredentials: {
