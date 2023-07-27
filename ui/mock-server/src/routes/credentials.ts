b35b7f4a17b37f6bb1b91553217daf4b9c977aaf
payush <payush@netapp.com>
2023-07-27 18:29:31 +0530

    Added comment

--- ui/mock-server/src/routes/credentials.ts
+++ ui/mock-server/src/routes/credentials.ts
@@ -3,6 +3,7 @@ import { delay, BASE_URL, generateResponse } from '../utils/appUtils';
 import { CredentialsRes } from '../types/credentialsTypes';
 const router = require('express').Router();
 
+//Get credentials mock response
 router.get(`${BASE_URL}/v1/credentials/:credentialsType`, async (req: {}, res: CredentialsRes) => {
     const retData = [
         {
