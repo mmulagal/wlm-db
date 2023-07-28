4920cb936f034f0a36f678671f59e19e8549e53c
rpanwar@netapp.com <rpanwar@netapp.com>
2023-07-28 18:58:12 +0530

    Added state for DB version and security group

--- ui/src/store/mssql/mssqlFormSlice.ts
+++ ui/src/store/mssql/mssqlFormSlice.ts
@@ -7,9 +7,14 @@ const initialState: any = {
         credentials: ''
     },
     regionAndVpc: {},
+    securityGroup: {
+        selectedSecurityType: GENERAL.USE_AN_EXISTING_SECURITY,
+        selectedExistingSecurityGroup: ''
+    },
     operatingSystem: {
         selectedOperatingSystem: GENERAL.WIN_SERVER_2016
-    }
+    },
+    dbVersion: GENERAL.SQL_SERVER_2016
 };
 
 const mssqlFormSlice = createSlice({
@@ -18,9 +23,19 @@ const mssqlFormSlice = createSlice({
     reducers: {
         setSelectedOperatingSystem(state, action: PayloadAction<any>) {
             state.operatingSystem.selectedOperatingSystem = action.payload;
+        },
+        setSelectedSecurityGroup(state, action: PayloadAction<any>) {
+            state.securityGroup.selectedSecurityType = action.payload;
+        },
+        setSelectedExistingSecurityGroup(state, action: PayloadAction<any>) {
+            state.securityGroup.selectedExistingSecurityGroup = action.payload;
+        },
+        setDBVersion(state, action: PayloadAction<any>) {
+            state.dbVersion = action.payload;
         }
     }
 });
 
-export const { setSelectedOperatingSystem } = mssqlFormSlice.actions;
+export const { setSelectedOperatingSystem, setSelectedSecurityGroup, setSelectedExistingSecurityGroup, setDBVersion } =
+    mssqlFormSlice.actions;
 export default mssqlFormSlice;
