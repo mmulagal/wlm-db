7752ac1a5ec7e8b1341b2eb5a44ec72c48384773
krithib <krithib@netapp.com>
2023-07-31 14:22:53 +0530

    DBS-269 Minor log change

--- server/src/operations/aws/cloud-formation-operations.ts
+++ server/src/operations/aws/cloud-formation-operations.ts
@@ -4,7 +4,7 @@ import getLogger from '../../utils/logger';
 const logger = getLogger();
 
 async function currentCfStacksCount(credentialsId: string, region: string) {
-    logger.info('Fetching non-deleted cloudformation stacks  in region ', region);
+    logger.info('Fetching cloudformation stacks in region ', region);
     const cloudformationClient = await getCloudformationClient(credentialsId, region);
     const currentStacksCount = (await getStacks(cloudformationClient)).StackSummaries?.length || 0;
     logger.debug('Completed stacks count ', currentStacksCount);
