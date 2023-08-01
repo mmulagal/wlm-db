8ad093abf3ee243f79426ddf4d0548415ba48bdb
R Ranganatha <rranga@netapp.com>
2023-08-01 21:02:35 +0530

    Added test case for DescribeFilesystems.

--- server/src/lib/aws/fsx.ts
+++ server/src/lib/aws/fsx.ts
@@ -25,7 +25,7 @@ async function describeFSxFileSystems(
     credentialsId: string,
     region: string
 ): Promise<DescribeFileSystemsCommandOutput> {
-    logger.info('Describe Amazon FSx for NetApp ONTAP filesystem:', { credentialsId, region });
+    logger.info('Describe Amazon FSx for NetApp ONTAP filesystems:', { credentialsId, region });
 
     const client = await getFSxClient(credentialsId, region);
     const response = await client.send(new DescribeFileSystemsCommand({}));
--- server/test/lib/aws/fsx.test.ts
+++ server/test/lib/aws/fsx.test.ts
@@ -1,21 +1,20 @@
 import '../../simulator/scopes/aws/fsx-scope';
 import '../../simulator/scopes/cloud-manager/cloud-manager-credentials-scope';
 import fsxFilesystems from '../../simulator/responses/aws/list-fsx-filesystems.json';
+import fsxVolumes from '../../simulator/responses/aws/list-fsx-volumes.json';
 import { DEFAULT_AWS_CREDENTIALS_TYPE, DEFAULT_AWS_REGION } from '../../../src/utils/consts';
 import { describeFSxFileSystems, describeFSxVolumes } from '../../../src/lib/aws/fsx';
 
 const FSX_FILESYSTEM_ID = 'fs-03773e21b2f0e39b4';
 
-describe('List Amazon FSx for NetApp ONTAP filesystems', () => {
-    it('Should return a list of Amazon FSx for NetApp ONTAP filesystems', async () => {
+describe('Testcases for Amazon FSx resources', () => {
+    it('List FSx Filesystems', async () => {
         const response = await describeFSxFileSystems(DEFAULT_AWS_CREDENTIALS_TYPE, DEFAULT_AWS_REGION);
         expect(response).toEqual(fsxFilesystems);
     });
-});
 
-describe('List Amazon FSx for NetApp ONTAP volumes', () => {
-    it('Should return a list of Amazon FSx for NetApp ONTAP volumes', async () => {
+    it('List FSx Volumes', async () => {
         const response = await describeFSxVolumes(DEFAULT_AWS_CREDENTIALS_TYPE, DEFAULT_AWS_REGION, FSX_FILESYSTEM_ID);
-        expect(response).toBeDefined();
+        expect(response).toEqual(fsxVolumes);
     });
 });
