9b55597cb8605396cb489b208d12d363c8ddd1de
Yadav, Sonam <Sonam.Sonam@netapp.com>
2023-08-02 15:23:17 +0530

    adding mock for API calls

--- ui/mock-server/src/routes/aws.ts
+++ ui/mock-server/src/routes/aws.ts
@@ -1,6 +1,6 @@
 import { BASE_URL, generateResponse } from '../utils/appUtils';
 
-import { AdsRes, AmisRes, KmsKeysRes, RegionRes, SnsTopicsRes, VpcRes } from '../types/awsTypes';
+import { AdsRes, AmisRes, FsxnRes, InstanceTypeRes, KeyPairRes, KmsKeysRes, RegionRes, SnsTopicsRes, VpcRes } from '../types/awsTypes';
 
 import vpcsData from '../data/vpcs.json';
 import regionsData from '../data/regions.json';
@@ -9,6 +9,8 @@ import amisData from '../data/amis.json';
 import snsTopicsData from '../data/sns-topics.json';
 import kmsKeysData from '../data/kms-keys.json';
 import keyPairData from '../data/key-pairs.json';
+import instanceTypeData from '../data/instance-types.json';
+import fsxnData from '../data/fsxn.json';
 
 const router = require('express').Router();
 
@@ -49,8 +51,20 @@ router.get(`${BASE_URL}/v1/credentials/:credentialsId/regions/:region/kmsKeys`, 
 });
 
 // Get Key Pair mock response
-router.get(`${BASE_URL}/v1/credentials/:credentialsId/regions/:region/keypairs`, async (req: {}, res: KmsKeysRes) => {
+router.get(`${BASE_URL}/v1/credentials/:credentialsId/regions/:region/keypairs`, async (req: {}, res: KeyPairRes) => {
     const retData = keyPairData;
+    generateResponse(res, 200, retData);
+});
+
+// Get Instance Type mock response
+router.get(`${BASE_URL}/v1/credentials/:credentialsId/regions/:region/instanceTypes`, async (req: {}, res: InstanceTypeRes) => {
+    const retData = instanceTypeData;
+    generateResponse(res, 200, retData);
+});
+
+// Get FSxN mock response
+router.get(`${BASE_URL}/v1/credentials/:credentialsId/regions/:region/vpcs/:vpcId/fsxs`, async (req: {}, res: FsxnRes) => {
+    const retData = fsxnData;
     generateResponse(res, 200, retData);
 });
 
