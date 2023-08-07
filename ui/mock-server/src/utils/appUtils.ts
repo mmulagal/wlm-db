15af595bb974a6fc18cbcbc82c6bd8e8f6f50866
Sathish S <Sathish.S@netapp.com>
2023-08-07 13:50:07 +0530

    test

--- ui/mock-server/src/utils/appUtils.ts
+++ ui/mock-server/src/utils/appUtils.ts
@@ -5,6 +5,7 @@ export const BASE_URL = '/wlmdb/accounts/:accountId/api';
 
 export const generateResponse = function generateResponse(res: any, statusCode: number, result: any) {
     const number = '123';
+    const test = 12;
     return res.status(statusCode).send(result);
 };
 
