77309e32efe649a7278d98fd2cd372640324198a
Sathish S <Sathish.S@netapp.com>
2023-08-07 13:23:30 +0530

    eslint enforce test for ui

--- ui/src/components/InfrastructureSettings/Tags/Tags.tsx
+++ ui/src/components/InfrastructureSettings/Tags/Tags.tsx
@@ -52,6 +52,7 @@ const Tags = () => {
     };
 
     const handleDeleteTag = (idx: number) => {
+        // testing
         const test = '12';
         console.log('test 1');
         let updatedTags = [...tags];
