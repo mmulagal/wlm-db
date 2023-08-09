dd269a3141e5ca855389ae36ab37552edb100976
Yadav, Sonam <Sonam.Sonam@netapp.com>
2023-08-09 18:40:57 +0530

    loader changes

--- ui/src/components/MSSqlServer/MSSqlFooter/MSSqlFooter.tsx
+++ ui/src/components/MSSqlServer/MSSqlFooter/MSSqlFooter.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@netapp/design-system';
 import { useDispatch } from 'react-redux';
+import { setIsLoading } from '../../../store/mssql/msSqlActionSlice';
 import { useAppSelector } from '../../../store/storeHooks';
 import { useDeploySqlTemplateMutation } from '../../../utils/apiService';
 import { cmNavigateTo } from '../../../utils/appConfig';
@@ -19,13 +20,14 @@ const MSSqlFooter = () => {
     const handleCreate = () => {
         const payload = handleCreateSQLServer(state, dispatch);
         if(payload){
+            dispatch(setIsLoading(true));
             deploySqlTemplate({credentialId: selectedCredId, region: selectedRegionCode, payload: payload})
             .then((data:any) => {
-                console.log(data);
+                dispatch(setIsLoading(false));
                 cmNavigateTo('/');
             })
             .catch((error:any) => {
-                console.log(error);
+                dispatch(setIsLoading(false));
             })
         }
     };
