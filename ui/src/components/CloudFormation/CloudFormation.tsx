2c9fd5fb7abf5d415ac1425046c412088e847a27
Yadav, Sonam <Sonam.Sonam@netapp.com>
2023-08-09 14:59:31 +0530

    cloud formation API call

--- ui/src/components/CloudFormation/CloudFormation.tsx
+++ ui/src/components/CloudFormation/CloudFormation.tsx
@@ -1,12 +1,41 @@
 import { Button } from "@netapp/design-system";
+import { useAppSelector } from "../../store/storeHooks";
 import { GENERAL } from "../../utils/appConstants";
 import styles from "./CloudFormation.module.scss";
+import { createMssqlPayload } from "../MSSqlServer/MSSqlFooter/createSqlServer";
+import { useCreateSqlTemplateMutation } from "../../utils/apiService";
 
 const CloudFormation = () => {
+  const state = useAppSelector(state => state);
+
+  const selectedCredId = state.mssqlForm.awsAccount.selectedCredential?.data?.credentialsId;
+  const selectedRegionCode = state.mssqlForm.regionAndVpc.selectedRegion?.data?.regionCode;
+
+  const [createSqlTemplate] = useCreateSqlTemplateMutation();
+
+  const handleTemplateView = () => {
+    const payload = createMssqlPayload(state);
+    if(payload){
+        createSqlTemplate({credentialId: selectedCredId, region: selectedRegionCode, payload: payload})
+        .then((data:any) => {
+            console.log(data);
+            const url = data?.data?.cloudFormationUrl;
+            if(url){
+              window.open(url, '_blank', 'noopener');
+            } else{
+              console.log(data?.warningMessage);
+            }
+        })
+        .catch((error:any) => {
+            console.log(error);
+        })
+    }
+};
+
   return (
     <div className={styles["cloud-formation"]}>
       <div className={styles.inner}>
-        <Button Component="button" variant="link">
+        <Button Component="button" variant="link" onClick={handleTemplateView}>
           {GENERAL.SAVE_FORM_AS_CLOUD}
         </Button>
       </div>
