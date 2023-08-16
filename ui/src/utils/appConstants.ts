e6e8be04b77f707c46ba3dfd1d49151fa7761f2b
Yadav, Sonam <Sonam.Sonam@netapp.com>
2023-08-16 13:40:45 +0530

    resolve conflict

--- ui/src/utils/appConstants.ts
+++ ui/src/utils/appConstants.ts
@@ -226,14 +226,11 @@ export const GENERAL = {
     SELECT_ANY_VPC: 'Please select any VPC',
     CLOUD_FORMATION_URL_TEXT: 'The CloudFormation URL can be loaded using the',
     PASSWORD_ERROR_CHECK: 'Check password criteria',
-<<<<<<< HEAD
     CREATE_INFO_MESSAGE: ['Microsoft SQL Server and FSxN for ONTAP deployment has been triggered. You can track the progress using the ',
     'Timeline', 
     '. Estimated time: Up to 2 hours. We will notify you as soon as the deployment is completed.'],
-=======
     IP_DOMAIN: 'SQL Server IP address or domain name',
     AUTHENTICATION_TYPE: 'Authentication type',
     DISCOVER_FORM_HEADING:
         'Enter connection details for Microsoft SQL Server that is accessible from the BlueXP Connector'
->>>>>>> a0975568b374090a5dc054c5f373b8a520657534
 };
