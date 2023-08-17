e23de02feff5086c8474348146102aa374405a76
krithib <krithib@netapp.com>
2023-08-17 11:46:48 +0530

    Replacing round with Math.ceil

--- server/src/utils/utils.ts
+++ server/src/utils/utils.ts
@@ -4,7 +4,7 @@
  */
 import createError from 'http-errors';
 import { getAsyncLocalStorageResource } from './async-local-storage';
-import { trimEnd, trimStart, round } from 'lodash-es';
+import { trimEnd, trimStart } from 'lodash-es';
 import jwt from 'jsonwebtoken';
 
 import { getVpcsList } from '../operations/aws/ec2-operations';
@@ -77,13 +77,13 @@ function generateFsxParams(FSxDataLunSize: number, isExistingFSx: boolean) {
     const FSxDataLunSizeInMib = FSxDataLunSize * 1024;
 
     // All these in MiB
-    const FSxDataVolumeSize = round(1.1 * FSxDataLunSizeInMib); // FSxDataLunSize + 10% of FSxDataLunSize
-    const FSxLogVolumeSize = round(0.25 * FSxDataVolumeSize); // 25% of FSxDataVolumeSize
-    const FSxTempDbVolumeSize = round(0.1 * FSxDataVolumeSize); // 10% of FSxDataVolumeSize
+    const FSxDataVolumeSize = Math.ceil(1.1 * FSxDataLunSizeInMib); // FSxDataLunSize + 10% of FSxDataLunSize
+    const FSxLogVolumeSize = Math.ceil(0.25 * FSxDataVolumeSize); // 25% of FSxDataVolumeSize
+    const FSxTempDbVolumeSize = Math.ceil(0.1 * FSxDataVolumeSize); // 10% of FSxDataVolumeSize
     const FSxQuorumVolumeSize = 10000; // 10GB
 
     // StorageCapacity in GiB
-    const FSxStorageCapacity = round(
+    const FSxStorageCapacity = Math.ceil(
         (FSxDataVolumeSize + FSxLogVolumeSize + FSxTempDbVolumeSize + FSxQuorumVolumeSize) / 1024
     );
 
