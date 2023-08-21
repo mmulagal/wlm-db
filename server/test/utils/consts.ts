f66030acb7be49715a953efea7d6dc2e3b4fa64a
krithib <krithib@netapp.com>
2023-08-21 13:42:00 +0530

    Removing scope file and adjusting default input

--- server/test/utils/consts.ts
+++ server/test/utils/consts.ts
@@ -10,7 +10,7 @@ const NETWORKING_CONFIGURATION = {
     routeTable1Id: 'rtb-65aeb107',
     availabilityZone1: 'string',
     privateSubnet2Id: 'subnet-4cdd3b29',
-    routeTable2Id: 'rtb-65aeb107',
+    routeTable2Id: 'rtb-65aeb108',
     availabilityZone2: 'string'
 };
 const EC2_CONFIGURATION = {
