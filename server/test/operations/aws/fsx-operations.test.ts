0b75edbda29b28e8aa01dbfb04c321003f63e9a6
Sathish S <Sathish.S@netapp.com>
2023-08-24 21:40:04 +0530

    fixed test case issue in fsx operations

--- server/test/operations/aws/fsx-operations.test.ts
+++ server/test/operations/aws/fsx-operations.test.ts
@@ -1,3 +1,4 @@
+import '../../simulator/scopes/aws/ec2-scope';
 import '../../simulator/scopes/aws/fsx-scope';
 import '../../simulator/scopes/cloud-manager/cloud-manager-credentials-scope';
 import '../../simulator/scopes/opentelemetry-scope';
@@ -58,7 +59,8 @@ describe('Testcases for Amazon FSx resources operations', () => {
                             storageVirtualMachineId: 'svm-03985e26ffd55441d',
                             ontapVolumeType: 'RW'
                         }
-                    ]
+                    ],
+                    securityGroups: ['sg-05f4939d6670b405f', 'sg-3924c15c']
                 }
             ]
         };
