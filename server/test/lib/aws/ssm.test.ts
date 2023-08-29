9f8e47963a273f1849371330debd1e7707e2dcd1
piyush5 <piyush5@netapp.com>
2023-08-29 16:06:16 +0530

    UT for existing APIS

--- server/test/lib/aws/ssm.test.ts
+++ server/test/lib/aws/ssm.test.ts
@@ -3,7 +3,6 @@ import '../../simulator/scopes/aws/ssm-scope';
 import '../../simulator/scopes/opentelemetry-scope';
 import { sendSSMCommand, getCommandInvocation } from '../../../src/lib/aws/ssm';
 import ssmCommandOutput from '../../simulator/responses/aws/ssm-sendcommans-response.json';
-import getssmcoommandoutput from '../../simulator/responses/aws/ssm-getcommands.json';
 import { faker } from '@faker-js/faker';
 
 const credentialsId = `${faker.string.alpha(20)}`;
@@ -29,12 +28,46 @@ describe('sendSSMCommand', () => {
         expect(resp).toEqual(ssmCommandOutput.Command.CommandId);
     });
 
+    const getResponse = {
+        $metadata: {
+            httpStatusCode: 200,
+            requestId: '17601055-7787-4476-8450-2abb4851432a',
+            extendedRequestId: undefined,
+            cfId: undefined,
+            attempts: 1,
+            totalRetryDelay: 0
+        },
+        CloudWatchOutputConfig: { CloudWatchLogGroupName: '', CloudWatchOutputEnabled: false },
+        CommandId: 'da23b63e-d6b2-40d2-8d1a-b1f3d7980679',
+        Comment: '',
+        DocumentName: 'AWS-RunPowerShellScript',
+        DocumentVersion: '$DEFAULT',
+        ExecutionElapsedTime: 'PT1.528S',
+        ExecutionEndDateTime: '2023-08-29T09:54:19.636Z',
+        ExecutionStartDateTime: '2023-08-29T09:54:18.636Z',
+        InstanceId: 'i-0880a21327284f67c',
+        PluginName: 'aws:runPowerShellScript',
+        ResponseCode: 0,
+        StandardErrorContent: '',
+        StandardErrorUrl: '',
+        StandardOutputContent:
+            '{\r\n' +
+            '    "used":  342859776,\r\n' +
+            '    "total":  4294557696,\r\n' +
+            '    "remaining":  3951697920,\r\n' +
+            '    "percentUsed":  7\r\n' +
+            '}\r\n',
+        StandardOutputUrl: '',
+        Status: 'Success',
+        StatusDetails: 'Success'
+    };
+
     it('getCommandInvocation', async () => {
         const params = {
             CommandId: '3b7b2232-67de-4b43-8d52-2997f7b259dd',
             InstanceId: 'i-0880a21327284f67c'
         };
         const resp = await getCommandInvocation(credentialsId, 'us-east-1', params);
-        expect(resp).toEqual(getssmcoommandoutput);
+        expect(resp).toEqual(getResponse);
     });
 });
