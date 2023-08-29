57d1ea49d6237401666ec2f3087a357228a4e09d
piyush5 <piyush5@netapp.com>
2023-08-29 15:43:05 +0530

    Added scope

--- server/test/lib/aws/ssm.test.ts
+++ server/test/lib/aws/ssm.test.ts
@@ -1,15 +1,15 @@
 import '../../simulator/scopes/cloud-manager/cloud-manager-credentials-scope';
-import '../../simulator/scopes/aws/sns-scope';
+import '../../simulator/scopes/aws/ssm-scope';
 import '../../simulator/scopes/opentelemetry-scope';
-import { sendSSMCommand, getSSMClient, getCommandInvocation } from '../../../src/lib/aws/ssm';
-import { faker } from '@faker-js/faker';
+import { sendSSMCommand, getCommandInvocation } from '../../../src/lib/aws/ssm';
 import ssmCommandOutput from '../../simulator/responses/aws/ssm-sendcommans-response.json';
 import getssmcoommandoutput from '../../simulator/responses/aws/ssm-getcommands.json';
+import { faker } from '@faker-js/faker';
+
+const credentialsId = `${faker.string.alpha(20)}`;
 
 describe('sendSSMCommand', () => {
     it('sendSSMCommand', async () => {
-        const credentialsId = `${faker.string.alpha(20)}`;
-        const client = await getSSMClient('us-east-1', credentialsId);
         const params = {
             DocumentName: 'AWS-RunPowerShellScript',
             Documentversion: '1',
@@ -25,18 +25,16 @@ describe('sendSSMCommand', () => {
             },
             InstanceIds: ['i-07e76a4b916548dc0']
         };
-        const resp = await sendSSMCommand(client, params);
-        expect(resp).toEqual(ssmCommandOutput);
+        const resp = await sendSSMCommand(credentialsId, 'us-east-1', params);
+        expect(resp).toEqual(ssmCommandOutput.Command.CommandId);
     });
 
     it('getCommandInvocation', async () => {
-        const credentialsId = `${faker.string.alpha(20)}`;
-        const client = await getSSMClient('us-east-1', credentialsId);
         const params = {
             CommandId: '3b7b2232-67de-4b43-8d52-2997f7b259dd',
             InstanceId: 'i-0880a21327284f67c'
         };
-        const resp = await getCommandInvocation(client, params);
+        const resp = await getCommandInvocation(credentialsId, 'us-east-1', params);
         expect(resp).toEqual(getssmcoommandoutput);
     });
 });
--- server/test/simulator/scopes/aws/ssm-scope.ts
+++ server/test/simulator/scopes/aws/ssm-scope.ts
@@ -2,13 +2,45 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
 
-import { SendCommandCommand, SSMClient } from '@aws-sdk/client-ssm';
+import { GetCommandInvocationCommand, SendCommandCommand, SSMClient } from '@aws-sdk/client-ssm';
 import { mockClient } from 'aws-sdk-client-mock';
 import listSendCommandCommandResponse from '../../responses/aws/ssm-sendcommans-response.json';
 
 const ssmMock = mockClient(SSMClient);
 
+const getResponse = {
+    $metadata: {
+        httpStatusCode: 200,
+        requestId: '17601055-7787-4476-8450-2abb4851432a',
+        extendedRequestId: undefined,
+        cfId: undefined,
+        attempts: 1,
+        totalRetryDelay: 0
+    },
+    CloudWatchOutputConfig: { CloudWatchLogGroupName: '', CloudWatchOutputEnabled: false },
+    CommandId: 'da23b63e-d6b2-40d2-8d1a-b1f3d7980679',
+    Comment: '',
+    DocumentName: 'AWS-RunPowerShellScript',
+    DocumentVersion: '$DEFAULT',
+    ExecutionElapsedTime: 'PT1.528S',
+    ExecutionEndDateTime: '2023-08-29T09:54:19.636Z',
+    ExecutionStartDateTime: '2023-08-29T09:54:18.636Z',
+    InstanceId: 'i-0880a21327284f67c',
+    PluginName: 'aws:runPowerShellScript',
+    ResponseCode: 0,
+    StandardErrorContent: '',
+    StandardErrorUrl: '',
+    StandardOutputContent:
+        '{\r\n' +
+        '    "used":  342859776,\r\n' +
+        '    "total":  4294557696,\r\n' +
+        '    "remaining":  3951697920,\r\n' +
+        '    "percentUsed":  7\r\n' +
+        '}\r\n',
+    StandardOutputUrl: '',
+    Status: 'Success',
+    StatusDetails: 'Success'
+};
 
 ssmMock.on(SendCommandCommand).resolves(listSendCommandCommandResponse);
-
-
+ssmMock.on(GetCommandInvocationCommand).resolves(getResponse);
