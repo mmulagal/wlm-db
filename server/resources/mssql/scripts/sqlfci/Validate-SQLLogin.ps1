89e5f1b04b3f456d0dcd977cc7dbb85a7f108097
krithib <krithib@netapp.com>
2023-08-29 23:30:25 +0530

    latest scripts

--- server/resources/mssql/scripts/sqlfci/Validate-SQLLogin.ps1
+++ server/resources/mssql/scripts/sqlfci/Validate-SQLLogin.ps1
@@ -1,50 +1,31 @@
-﻿
-
+﻿   
 param(
   [Parameter(Mandatory = $true)]
-  $SQLServer,
-  [Parameter(Mandatory = $false)]
-  $SqlUserSecret,
-  [Parameter(Mandatory = $false)]
-  $SqlUser
+  $SQLServer
 )
 
-#Install-Module -Name SqlServer -Force;
+
+ if (Get-Module -ListAvailable -Name SqlServer) {
+    Write-output "Module exists"
+} 
+else {
+    Install-Module -Name SqlServer -AllowClobber -Force;
+} 
 
 $Query = 'SELECT name FROM sys. databases'
 
-#Retrieving MSSQL service account
-$DomainNetBIOSName = $env:USERDOMAIN
-$SqlUser = ConvertFrom-Json -InputObject (Get-SECSecretValue -SecretId $SqlUserSecret).SecretString
-$SqlUserName = $DomainNetBIOSName + '\' + $SqlUser
-$SqlUserPassword = $SqlUser.password 
-
-
-if ($SqlUserSecret)
-{
-  try {
-    #Query with SQL authentication (not recommended because your password is in plaintext on the command line).
-    Write-Output "Connected via SQL Authentication"
-    $results = Invoke-Sqlcmd -ConnectionString "Data Source=$SQLServer; User Id=$SqlUserName; Password=$SqlUserPassword;" -Query "$Query" | Format-Table 
-    Write-Output $results 
+try {
+  #Execute a query with trusted connection. If you omit the server, it will default to localhost.
+  Write-Output "Connecting with Windows Authentication"
+  $results =  Invoke-Sqlcmd -ConnectionString "Data Source=$SqlServer; Integrated Security=True; TrustServerCertificate=True" -Query "$Query" 
+  Write-Output "Connected with Windows Authentication" 
+  Write-Output $results
     }
-  catch {
-    Write-Output "A network-related or instance-specific error occurred while establishing a connection to SQL Server. The server was not 
-    found or was not accessible. Verify that the instance name is correct and that SQL Server is configured to allow remote connections. "
-  }
-}else{
-
-    try {
-      #Execute a query with trusted connection. If you omit the server, it will default to localhost.
-      Write-Output "Connected with Windows Authentication"
-      $results = Invoke-Sqlcmd -ConnectionString "Data Source=$SQLServer; Integrated Security=True;" -Query "$Query" | Format-Table
-      Write-Output $results
-      }
-    catch {
-      Write-Output "A network-related or instance-specific error occurred while establishing a connection to SQL Server. The server was not 
+catch {
+  Write-Output "A network-related or instance-specific error occurred while establishing a connection to SQL Server. The server was not 
         found or was not accessible. Verify that the instance name is correct and that SQL Server is configured to allow remote connections. "
       }
-     }
+     
 
 $cnt = ($results | Measure-Object).Count
 
@@ -55,3 +36,5 @@ catch { if ($_.Exception.Message -eq 1) {
   } 
   } 
 
+ 
+ 
--- server/src/utils/consts.ts
+++ server/src/utils/consts.ts
@@ -417,7 +417,7 @@ const EC2_INSTANCE_TYPE_EXCLUDE_LIST = [
 
 const WLMDB = 'wlmdb';
 
-const BUCKET_NAME = 'wlmbucket';
+const BUCKET_NAME = 'bucketkrithi';
 const ASSETS_BUCKET_REGION = 'ap-southeast-1';
 const BUCKET_PREFIX = 'templates';
 const EC2_ROLE_NAME = 'Ec2RoleName';
