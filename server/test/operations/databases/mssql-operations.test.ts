186415fa55aeb19d8348c04b54fcbf0a8d5645fc
piyush5 <piyush5@netapp.com>
2023-08-30 14:57:01 +0530

    updated the tc for cpu and disk API

--- server/test/operations/databases/mssql-operations.test.ts
+++ server/test/operations/databases/mssql-operations.test.ts
@@ -5,15 +5,25 @@ import '../../simulator/scopes/aws/ssm-scope';
 import { DATABASE_METRIC_TYPE } from '../../../src/utils/consts';
 import { getResourceUtilisation } from '../../../src/operations/workloads/mssql/mssql-operations';
 
-const getMemoryResonse = {
+const getResourceUtilizationResonse = {
     percentUsed: 7,
     used: 342859776,
     total: 4294557696,
     remaining: 3951697920
 };
-describe('getResourceUtilisation', () => {
-    it('getResourceUtilisation', async () => {
+describe('Get Resouce utilization details', () => {
+    it('Get memory utilization', async () => {
         const resp = await getResourceUtilisation('36E53042-04E8-40C9-AE69-26E56CB0D216', DATABASE_METRIC_TYPE.MEMORY);
-        expect(resp).toEqual(getMemoryResonse);
+        expect(resp).toEqual(getResourceUtilizationResonse);
+    });
+
+    it('Get cpu utilization', async () => {
+        const resp = await getResourceUtilisation('36E53042-04E8-40C9-AE69-26E56CB0D216', DATABASE_METRIC_TYPE.CPU);
+        expect(resp).toEqual(getResourceUtilizationResonse);
+    });
+
+    it('Get disk utilization', async () => {
+        const resp = await getResourceUtilisation('36E53042-04E8-40C9-AE69-26E56CB0D216', DATABASE_METRIC_TYPE.DISK);
+        expect(resp).toEqual(getResourceUtilizationResonse);
     });
 });
--- server/test/simulator/index.ts
+++ server/test/simulator/index.ts
@@ -18,6 +18,7 @@ async function initiateSimulator() {
     await import('./scopes/aws/secrets-manager-scope');
     await import('./scopes/aws/s3-scope');
     await import('./scopes/aws/iam-scope');
+    await import('./scopes/aws/ssm-scope');
     // Load server
     await import('../../src/index');
 }
