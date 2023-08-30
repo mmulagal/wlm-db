b4b876eef6227bbb1bbad89cccee634a09866457
mshreyas <mshreyas@netapp.com>
2023-08-30 16:17:31 +0530

    Working environment filter based on account ID and workaround for workspaceid

--- server/src/operations/working-environment-operations.ts
+++ server/src/operations/working-environment-operations.ts
@@ -1,33 +1,40 @@
 import createError from 'http-errors';
-import { RESOURCESTYPE, HttpErrorCodes } from '../utils/consts';
+import { RESOURCESTYPE, HttpErrorCodes, WORKSPACE_ID } from '../utils/consts';
 import { getTenancyResourcesByType, getTenancyResourcesByTypeAndId } from '../lib/cloud-manager/tenancy';
 import getLogger from '../utils/logger';
 import { getDatabasesCount, getResourceDetails } from './workloads/mssql/mssql-operations';
+import { getAsyncLocalStorageResource } from '../utils/async-local-storage';
 
 const logger = getLogger();
 async function getWorkingEnvironments() {
     logger.info('Getting working environment list');
-    const mssqlCredentials = await getTenancyResourcesByType(RESOURCESTYPE.MSSQL);
+    const mssqlResources = await getTenancyResourcesByType(RESOURCESTYPE.MSSQL);
     const workingEnvironments: { id: string; provider: string; name?: string; deploymentState: string }[] =
-        mssqlCredentials.map((credentials: any) => {
-            const { metadata } = credentials || {};
-            let deploymentState = '';
-            try {
-                if (metadata) {
-                    const { properties } = JSON.parse(metadata) || {};
-                    const parsedProperties = JSON.parse(properties);
-                    deploymentState = parsedProperties.deploymentState || '';
+        mssqlResources
+            .filter((resource: any) => {
+                const { metadata } = resource || {};
+                try {
+                    if (metadata) {
+                        const { properties } = JSON.parse(metadata) || {};
+                        const parsedProperties = JSON.parse(properties);
+                        const workspaceid = parsedProperties.workspace || '';
+                        // workaround to filter based on workspaceid passed as header
+                        return workspaceid === getAsyncLocalStorageResource<string>(WORKSPACE_ID);
+                    }
+                } catch (error) {
+                    throw createError(
+                        HttpErrorCodes.INTERNAL_SERVER_ERROR,
+                        `Error parsing resource properties, ${error}`
+                    );
                 }
-            } catch (error) {
-                throw createError(HttpErrorCodes.INTERNAL_SERVER_ERROR, `Error parsing resource properties, ${error}`);
-            }
-            return {
-                id: credentials.resourceIdentifier,
-                provider: credentials.resourceType,
-                name: credentials.name,
-                deploymentState
-            };
-        });
+                return false;
+            })
+            .map((resource: any) => ({
+                id: resource.resourceIdentifier,
+                provider: resource.resourceType,
+                name: resource.name,
+                deploymentState: resource.metadata ? JSON.parse(resource.metadata).properties.deploymentState || '' : ''
+            }));
     return workingEnvironments || [];
 }
 
