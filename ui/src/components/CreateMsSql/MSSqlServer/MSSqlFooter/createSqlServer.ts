86b5732b03f5ae327189d5cb3842f5fa978458eb
Yadav, Sonam <Sonam.Sonam@netapp.com>
2023-08-30 11:02:59 +0530

    removing console

--- ui/src/components/CreateMsSql/MSSqlServer/MSSqlFooter/createSqlServer.ts
+++ ui/src/components/CreateMsSql/MSSqlServer/MSSqlFooter/createSqlServer.ts
@@ -232,9 +232,6 @@ const handleCreateSQLServer = (state: any, dispatch: Dispatch) => {
         !fsxPassVal(state.mssqlForm.fsxN?.fsxNPassword)
     ) {
         payload = createMssqlPayload(state);
-        console.log("=============");
-        console.log(state.mssqlForm);
-        console.log("=============");
         console.log('Deploy Payload', payload);
     } else {
         console.log('Action required');
