099e023cb1727d773b010042f73fa53293900878
Sathish S <Sathish.S@netapp.com>
2023-08-31 12:53:51 +0530

    moved to const file

--- server/src/operations/batch-operations.ts
+++ server/src/operations/batch-operations.ts
@@ -1,7 +1,7 @@
 import Promise from 'bluebird';
 import { gotInstanceForInternalRequest } from '../utils/got';
 import { BatchRequestBodyType, SingleBatchResponseType, BatchResponseType } from '../routes/types/batch.types';
-
+import { METHODS_WITH_PAYLOAD } from '../utils/consts';
 import getLogger from '../utils/logger';
 import { HTTPAlias } from 'got';
 
@@ -10,8 +10,6 @@ const logger = getLogger();
 export default async function executeBatchApiCalls(requestBody: BatchRequestBodyType) {
     logger.info('Executing Batch Api calls', requestBody);
 
-    const methodsWithPayload = ['POST', 'PUT', 'PATCH'];
-
     const allApiResponse: BatchResponseType = await Promise.map(
         requestBody,
         async singleRequest => {
@@ -19,25 +17,23 @@ export default async function executeBatchApiCalls(requestBody: BatchRequestBody
             const { url, headers, payload, method } = singleRequest;
 
             try {
-                // const { token } = await getServiceToken();
                 const response = await gotInstanceForInternalRequest[method.toLowerCase() as HTTPAlias](url, {
                     headers: {
                         ...headers
-                        // [HEADERS.AUTHORIZATION]: token
                     },
-                    ...(methodsWithPayload.includes(method) && { json: payload })
+                    ...(METHODS_WITH_PAYLOAD.includes(method) && { json: payload })
                 });
 
                 responseData.data = response ? response : 'Success';
                 return responseData;
             } catch (err: any) {
                 const errMsg = `Failed to execute the batch api call. ${err.message}`;
-                logger.warn(errMsg);
+                logger.error(errMsg);
                 responseData.error = errMsg;
                 return responseData;
             }
         },
-        { concurrency: 2 }
+        { concurrency: 10 }
     );
 
     return allApiResponse;
