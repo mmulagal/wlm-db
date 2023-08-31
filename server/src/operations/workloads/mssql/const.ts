594a3f1e3170f49b4f9c7ae2eb7b50e6abe29edf
R Ranganatha <rranga@netapp.com>
2023-08-31 11:03:02 +0530

    Removed unused constant HEALTHY.

--- server/src/operations/workloads/mssql/const.ts
+++ server/src/operations/workloads/mssql/const.ts
@@ -2,7 +2,6 @@ const PSSCRIPT = ' C:\\SSM\\ExecuteQueryFromSSM.ps1';
 const SSM_RUN_POWERSHELL_SCRIPT_DOC = 'AWS-RunPowerShellScript';
 const SSM_RUN_POWERSHELL_SCRIPT_DOC_VERSION = '1';
 const DB_ROWS_COUNT = 75;
-const HEALTHY = 'Healthy';
 const DATABASES = (offset: number, rowscount: number) =>
     `SELECT databaseId = d.database_id, databaseName = d.name, creationDate = d.create_date, databaseStatus = d.state_desc, databaseSize = t.databaseSize FROM ( SELECT database_id, logSize = CAST(SUM(CASE WHEN [type] = 1 THEN size END) * 8. * 1024 AS DECIMAL(18,2)), rowSize = CAST(SUM(CASE WHEN [type] = 0 THEN size END) * 8. * 1024 AS DECIMAL(18,2)), databaseSize = CAST(SUM(size) * 8. * 1024 AS DECIMAL(18,2)) FROM sys.master_files GROUP BY database_id ) t JOIN sys.databases d ON d.database_id = t.database_id order by name offset ${offset} rows fetch next ${rowscount} rows only`;
 
@@ -102,7 +101,6 @@ export {
     DISK_UTILISATION,
     SERVER_VERSION_DETAILS,
     NUMBER_OF_CONNECTIONS,
-    HEALTHY,
     TABLES_QUERY,
     TABLES_COUNT_QUERY,
     MEMORY_UTILISATION,
