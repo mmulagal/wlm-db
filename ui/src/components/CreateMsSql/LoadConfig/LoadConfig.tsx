54bf5cc268b1a410f0cf3100db513c3f2d6bd1d5
Yadav, Sonam <Sonam.Sonam@netapp.com>
2023-09-05 18:44:32 +0530

    delete config code check in

--- ui/src/components/CreateMsSql/LoadConfig/LoadConfig.tsx
+++ ui/src/components/CreateMsSql/LoadConfig/LoadConfig.tsx
@@ -2,17 +2,22 @@ import { RadioButton } from '@netapp/design-system';
 import { useEffect, useState } from 'react';
 import { GENERAL } from '../../../utils/appConstants';
 import styles from './LoadConfig.module.scss';
-import { useGetConfigListQuery } from '../../../utils/apiService';
+import { useDeleteConfigMutation, useGetConfigListQuery } from '../../../utils/apiService';
 import { useDispatch } from 'react-redux';
 import { setLoadConfig } from '../../../store/mssql/mssqlFormSlice';
+import { ReactComponent as DeleteIcon1 } from '../../../assets/delete-icon.svg';
 
 const LoadConfig = () => {
     const dispatch = useDispatch();
+    const [hoveredItem, setHoveredItem] = useState(null);
 
     // API call to get configuration list
     const {
         data: configList,
+        refetch: configListRefetch
     } = useGetConfigListQuery({});
+
+    const [deleteConfigApi] = useDeleteConfigMutation();
 
     const [selectedConfig, setSelectedConfig] = useState('');
 
@@ -27,18 +32,39 @@ const LoadConfig = () => {
         setSelectedConfig(item?.name);
         dispatch(setLoadConfig(item?.id));
     };
+
+    const handleMouseEnter = (index: any) => {
+        setHoveredItem(index);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredItem(null);
+    };
+
+    const deleteConfig = (index: any) => {
+        deleteConfigApi({configId: configList[index]?.id});
+        configListRefetch();
+    };
+
     return (
         <div className={styles['load-config']}>
             <div className={styles.content}>{GENERAL.LOAD_CONFIG_CONTENT}</div>
             <div className={styles.radioContainer}>
                 {configList?.map((item: any, index: any) => (
-                    <div className={index === 0 ? `${styles.item} ${styles.firstItem}` : `${styles.item}`} key={index}>
-                        <RadioButton
-                            isChecked={selectedConfig === item?.name}
-                            onChange={() => handleChange(item)}
-                            children={item?.name}
-                            className=""
-                        />
+                    <div className={index === 0 ? `${styles.item} ${styles.firstItem}` : `${styles.item}`} key={index} 
+                        onMouseEnter={() => handleMouseEnter(index)}
+                        onMouseLeave={handleMouseLeave}>
+                        <div className={styles.setRow}>
+                            <RadioButton
+                                isChecked={selectedConfig === item?.name}
+                                onChange={() => handleChange(item)}
+                                children={item?.name}
+                                className=""
+                            />
+                            {hoveredItem === index && <div className={styles.icon}>
+                                <DeleteIcon1 onClick={() => deleteConfig(index)}/>
+                            </div>}
+                        </div>
                     </div>
                 ))}
             </div>
--- ui/src/components/CreateMsSql/MSSqlServer/MSSqlHeader/MSSqlHeader.tsx
+++ ui/src/components/CreateMsSql/MSSqlServer/MSSqlHeader/MSSqlHeader.tsx
@@ -51,7 +51,7 @@ const MSSqlHeader = () => {
             }}
             title={SELECT_CONFIG.WIZARD_HEADING}
         >
-            {/* <div className={styles['header-button']}>
+            <div className={styles['header-button']}>
                 <Button Component="button" onClick={handleLoadConfiguration} variant="text">
                     {SELECT_CONFIG.LOAD_CONFIG}
                 </Button>
@@ -59,7 +59,7 @@ const MSSqlHeader = () => {
                 <Button Component="button" onClick={handleSaveConfig} variant="text">
                     {SELECT_CONFIG.SAVE_CONFIG}
                 </Button>
-            </div> */}
+            </div>
         </Header>
     );
 };
