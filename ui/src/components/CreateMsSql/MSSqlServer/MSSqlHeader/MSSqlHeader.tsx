eff398406a7309da382b177da53f27dceb141563
Yadav, Sonam <Sonam.Sonam@netapp.com>
2023-09-05 21:29:02 +0530

    comment load and config button

--- ui/src/components/CreateMsSql/MSSqlServer/MSSqlHeader/MSSqlHeader.tsx
+++ ui/src/components/CreateMsSql/MSSqlServer/MSSqlHeader/MSSqlHeader.tsx
@@ -55,7 +55,7 @@ const MSSqlHeader = () => {
             }}
             title={SELECT_CONFIG.WIZARD_HEADING}
         >
-            <div className={styles['header-button']}>
+            {/* <div className={styles['header-button']}>
                 <Button Component="button" onClick={handleLoadConfiguration} variant="text">
                     {SELECT_CONFIG.LOAD_CONFIG}
                 </Button>
@@ -63,7 +63,7 @@ const MSSqlHeader = () => {
                 <Button Component="button" onClick={handleSaveConfig} variant="text">
                     {SELECT_CONFIG.SAVE_CONFIG}
                 </Button>
-            </div>
+            </div> */}
         </Header>
     );
 };
