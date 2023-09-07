cef99a86bba1474c0de723b17dfe973800d4f3fa
Yadav, Sonam <Sonam.Sonam@netapp.com>
2023-09-07 21:54:28 +0530

    bug fix

--- ui/src/utils/consts.ts
+++ ui/src/utils/consts.ts
@@ -36,6 +36,9 @@ export const DISABLED_STATE = 'Disabled';
 export const PENDING_DELETION = 'PendingDeletion';
 export const DEFAULT_MASTER_KEY = 'aws/fsx';
 
+// Default instance type
+export const DEAFULT_INSTANCE_VALUE = 'm5.xlarge'
+
 // Add credentials link
 export const CREDENTIAL_STAGE_LINK = 'https://staging.cloudmanager.netapp.com/credentials/account-credentials';
 export const CREDENTIAL_PROD_LINK = 'https://cloudmanager.netapp.com/credentials/account-credentials';
