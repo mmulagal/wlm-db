044d06865355bacf39341574aec0e9235581532e
Yadav, Sonam <Sonam.Sonam@netapp.com>
2023-09-11 23:10:39 +0530

    removing console

--- ui/src/components/CreateMsSql/MSSqlServer/MSSqlHeader/MSSqlHeader.tsx
+++ ui/src/components/CreateMsSql/MSSqlServer/MSSqlHeader/MSSqlHeader.tsx
@@ -43,7 +43,6 @@ const MSSqlHeader = () => {
     const isLoadConfig = useAppSelector(state => state.msSqlAction.isLoadConfig);
     const isMissing = useAppSelector(state => state.msSqlAction.isMissingFieldsInLoad);
     const refetchApiCount = useAppSelector(state => state.msSqlAction.refetchApiCount);
-    console.log(refetchApiCount);
     
     useEffect(() => {
         if(isLoadConfig){
