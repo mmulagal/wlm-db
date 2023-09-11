6b51825918fa452cbee433f8e7649297697fe8e4
Yadav, Sonam <Sonam.Sonam@netapp.com>
2023-09-11 23:59:20 +0530

    stack name added in banner DBS-694

--- ui/src/components/CreateMsSql/MSSqlServer/MSSqlFooter/MSSqlFooter.tsx
+++ ui/src/components/CreateMsSql/MSSqlServer/MSSqlFooter/MSSqlFooter.tsx
@@ -27,22 +27,28 @@ const MSSqlFooter = () => {
                 .then((data: any) => {
                     dispatch(setIsLoading(false));
                     if (!data?.error) {
+                        let stackName = data?.data?.cloudFormationStackI;
+                        if(stackName && stackName.includes('/')){
+                            stackName = stackName.split('/')[1];
+                        }
                         const timelineUrl =
                             process.env.REACT_APP_ENVIRONMENT === PRODUCTION ? TIMELINE_PROD_LINK : TIMELINE_STAGE_LINK;
                         const message = (
                             <>
                                 {GENERAL.CREATE_INFO_MESSAGE[0]}
+                                {stackName ? GENERAL.CREATE_INFO_MESSAGE[1] + stackName: ''}
+                                {GENERAL.CREATE_INFO_MESSAGE[2]}
                                 <Button
                                     Component="button"
                                     variant="text"
                                     onClick={() => window.open(timelineUrl, '_blank', 'noopener')}
                                 >
-                                    {GENERAL.CREATE_INFO_MESSAGE[1]}
+                                    {GENERAL.CREATE_INFO_MESSAGE[3]}
                                 </Button>
-                                {GENERAL.CREATE_INFO_MESSAGE[2]}
+                                {GENERAL.CREATE_INFO_MESSAGE[4]}
                             </>
                         );
-                        dispatch(addNotification({ notificationType: NOTIFICATION_TYPES.INFO, message: message }));
+                        dispatch(addNotification({ notificationType: NOTIFICATION_TYPES.INFO, message: message}));
                         setTimeout(() => {
                             navigateToCanvas('/');
                         }, 3000);
--- ui/src/utils/appConstants.ts
+++ ui/src/utils/appConstants.ts
@@ -248,7 +248,9 @@ export const GENERAL = {
     CLOUD_FORMATION_URL_TEXT: 'The CloudFormation URL can be loaded using the',
     PASSWORD_ERROR_CHECK: 'Check password criteria',
     CREATE_INFO_MESSAGE: [
-        'Microsoft SQL Server and FSxN for ONTAP deployment has been triggered. You can track the progress using the ',
+        'Microsoft SQL Server and FSxN for ONTAP deployment ',
+        'with stack name ',
+        ' has been triggered. You can track the progress using the ',
         'Timeline',
         '. Estimated time: Up to 2 hours. We will notify you as soon as the deployment is completed.'
     ],
