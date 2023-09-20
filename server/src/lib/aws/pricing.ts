b67aeaf939cf6d2ea9ad15a3ec61f4377ca2ace0
R Ranganatha <rranga@netapp.com>
2023-09-20 13:19:09 +0530

    Augmented pricing API to work with AWS credentials.

--- server/src/lib/aws/pricing.ts
+++ server/src/lib/aws/pricing.ts
@@ -7,16 +7,23 @@ import {
 import { DEFAULT_AWS_REGION } from '../../utils/consts';
 
 import getLogger from '../../utils/logger';
+import { getCredentialDetails } from '../cloud-manager/credentials';
 
 const logger = getLogger();
 
-async function getProducts(productFilters: GetProductsCommandInput): Promise<GetProductsCommandOutput> {
+async function getProducts(
+    credentialsId: string,
+    productFilters: GetProductsCommandInput
+): Promise<GetProductsCommandOutput> {
     logger.info('Getting pricing information to calculate estimates', {
         productFilters
     });
 
-    const pricingClient = new PricingClient({ region: DEFAULT_AWS_REGION });
-
+    const {
+        credentials: { accessKey: accessKeyId, secretKey: secretAccessKey, sessionId: sessionToken }
+    } = await getCredentialDetails(credentialsId);
+    const credentials = { accessKeyId, secretAccessKey, sessionToken };
+    const pricingClient = new PricingClient({ credentials, region: DEFAULT_AWS_REGION });
     const command = new GetProductsCommand(productFilters);
 
     return pricingClient.send(command);
