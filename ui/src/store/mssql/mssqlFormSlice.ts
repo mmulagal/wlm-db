6005ae5575f65af3954db6b760d115b7a400d41b
Yadav, Sonam <Sonam.Sonam@netapp.com>
2023-09-22 14:01:16 +0530

    bug fix DBS-741

--- ui/src/store/mssql/mssqlFormSlice.ts
+++ ui/src/store/mssql/mssqlFormSlice.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { GENERAL, SELECT_CONFIG } from '../../utils/appConstants';
 import { FSXADMIN, SQL_DATABASE, SQL_DEPLOYMENT_MODE, SQL_USERNAME } from '../../utils/consts';
+import { generateRandomDBName } from '../../utils/utilityFunctions';
 
 const initialState: any = {
     awsAccount: {
@@ -41,7 +42,7 @@ const initialState: any = {
         selectedLicenseId: null,
         selectedCustomAMI: null
     },
-    dbName: SQL_DATABASE,
+    dbName: '',
     dbCredentials: {
         name: SQL_USERNAME,
         password: ''
--- ui/src/utils/utilityFunctions.ts
+++ ui/src/utils/utilityFunctions.ts
@@ -2,7 +2,7 @@ import { optionType } from '@netapp/design-system/dist/components/Select';
 import { TableProps } from '@netapp/design-system/dist/components/Table';
 import numeral from 'numeral';
 import { GENERAL } from './appConstants';
-import { DEFAULT_MASTER_KEY, DISABLED_STATE, ENABLED_STATE, PENDING_DELETION, REGIONS_CODE_LIST } from './consts';
+import { DEFAULT_MASTER_KEY, DISABLED_STATE, ENABLED_STATE, PENDING_DELETION, REGIONS_CODE_LIST, SQL_DATABASE } from './consts';
 import { AvailabilityZonesObj, KmsKeys, Regions, Subnets } from './types/mssqlTypes';
 import store from '../store/store';
 const moment = require('moment');
@@ -274,3 +274,8 @@ export const formatSizeSplit = (value: number | string) => {
 export const displayFormattedValue = (value: number, msg: string) => {
     return `${formatSize(value)} ${msg}`;
 };
+
+export const generateRandomDBName = () => {
+    const randomNum = Array.from(Array(4), () => Math.floor(Math.random() * 36).toString(36)).join('');
+    return SQL_DATABASE + randomNum;
+}
