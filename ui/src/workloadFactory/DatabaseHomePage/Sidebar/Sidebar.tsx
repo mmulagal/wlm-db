3bf1cefcb354d2eb73af358974f04e4ad2fb8d43
rpanwar@netapp.com <rpanwar@netapp.com>
2023-10-06 09:23:47 +0530

    Side bar svg's added

--- ui/src/workloadFactory/DatabaseHomePage/Sidebar/Sidebar.tsx
+++ ui/src/workloadFactory/DatabaseHomePage/Sidebar/Sidebar.tsx
@@ -1,28 +1,22 @@
 import { Button, Typography } from '@netapp/design-system';
-import { ReactComponent as Favorite } from '../../../assets/favorite service.svg';
+import { ReactComponent as ArrowRight } from '../../../assets/ic_arrow_right.svg';
+import { ReactComponent as ArrowLeft } from '../../../assets/ic_arrow_left.svg';
 import styles from './Sidebar.module.scss';
 
 const Sidebar = ({ isOpen, onClose }: any) => {
-    const jsonObject = {
-        database_name: 'your_database_name',
-        deployment_script: 'your_deployment_script_content',
-        environment: 'staging',
-        backup_before_deployment: true
-    };
-    const jsonObj2 = {
-        status: 'success',
-        message: 'Database deployment successful',
-        deployment_id: 'your_deployment_id'
-    };
     return (
         <div className={`${styles.sidebar} ${isOpen ? styles.open : ''}`}>
             <div className={styles.topBar}>
-                <Typography variant="Regular_16" className={styles.color}>
+                <Typography variant="Regular_16" className={styles.colorAutomation}>
                     Automations
                 </Typography>
-                <Typography variant="Regular_16" className={styles.color} onClick={onClose}>
-                    {!isOpen ? 'Expand' : 'Collapse'}
-                </Typography>
+                <div className={styles.rightSection}>
+                    {!isOpen && <ArrowRight />}
+                    <Typography variant="Regular_16" className={styles.color} onClick={onClose}>
+                        {!isOpen ? 'Expand' : 'Collapse'}
+                    </Typography>
+                    {isOpen && <ArrowLeft />}
+                </div>
             </div>
         </div>
     );
