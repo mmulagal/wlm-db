201c78e89f1c8b993ba82aa5b7c6e4726d9c8f5a
Yadav, Sonam <Sonam.Sonam@netapp.com>
2023-10-09 12:47:42 +0530

    Bug fix DBS-920

--- server/src/utils/consts.ts
+++ server/src/utils/consts.ts
@@ -128,7 +128,7 @@ enum HttpErrorCodes {
 }
 
 enum SqlServerDeploymentModel {
-    SQL_STANDALONE = 'Non-clustered',
+    SQL_STANDALONE = 'Standalone instance',
     SQL_FCI = 'Always On Failover Cluster Instance'
 }
 
