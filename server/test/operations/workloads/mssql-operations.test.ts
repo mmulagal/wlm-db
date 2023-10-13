0591f3c31decb8c9e419823f234a2c99771561bc
krithib <krithib@netapp.com>
2023-10-13 09:37:01 +0530

    UT for getServerState

--- server/test/operations/workloads/mssql-operations.test.ts
+++ server/test/operations/workloads/mssql-operations.test.ts
@@ -21,7 +21,8 @@ import {
     getTablesSummary,
     getServerSummary,
     discoverMsSqlServer,
-    getServerIOLatency
+    getServerIOLatency,
+    getServerState
 } from '../../../src/operations/workloads/mssql/mssql-operations';
 import { createResource, deleteResource } from '../../../src/lib/database/db';
 
@@ -111,6 +112,11 @@ describe('MSSQL Resource methods', () => {
     it('Get Server IO Latency ', async () => {
         const resp = await getServerIOLatency('36E53042-04E8-40C9-AE69-26E56CB0D216');
         expect(resp).toEqual(mssqlResponse.mssqlIOLatencyResponse);
+    });
+
+    it('Get Server state ', async () => {
+        const resp = await getServerState('36E53042-04E8-40C9-AE69-26E56CB0D216');
+        expect(resp).toEqual(mssqlResponse.serverSummaryResponse.serverStatus);
     });
 
     it('Discover MSSQL server ', async () => {
