ef2687eef41992b2406139a9398aca8f5739d892
rpanwar@netapp.com <rpanwar@netapp.com>
2023-10-15 13:40:01 +0530

    Added state variable for dark theme

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -5,18 +5,24 @@ import { Spinner, ThemeProvider } from '@netapp/design-system';
 import ErrorPage from './common/ErrorPage/ErrorPage';
 import { useInitialize } from './utils/appConfig';
 import FullStoryComp from './common/FullStoryComp';
+import { useEffect, useState } from 'react';
 
 function App() {
     const { loading, accountId } = useAppSelector(state => state.auth);
+    const [darkTheme, setDarkTheme] = useState(false);
 
     useInitialize();
 
     //@ts-ignore
     const isDarkTheme = useAppSelector(state => state?.features?.byKey['Platform.BlueXP/DarkTheme'].active);
 
+    useEffect(() => {
+        setDarkTheme(isDarkTheme);
+    }, [isDarkTheme]);
+
     return (
         <>
-            <ThemeProvider isIframe={true} theme={isDarkTheme ? 'dark' : 'light'}>
+            <ThemeProvider isIframe={true} theme={darkTheme ? 'dark' : 'light'}>
                 <FullStoryComp />
                 {loading && (
                     <div className="App">
