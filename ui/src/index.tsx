21e466b5419416809306cd0a5e5bf72ec058fe7b
rpanwar@netapp.com <rpanwar@netapp.com>
2023-10-15 13:18:10 +0530

    Changes for Dark theme

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider as ReduxContextProvider } from 'react-redux';
-import { DialogContextProvider, ThemeProvider } from '@netapp/design-system';
+import { DialogContextProvider } from '@netapp/design-system';
 import { BrowserRouter } from 'react-router-dom';
 import store from './store/store';
 import './index.css';
@@ -11,13 +11,11 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         <ReduxContextProvider store={store}>
-            <ThemeProvider isIframe={true} theme={'light'}>
-                <BrowserRouter>
-                    <DialogContextProvider>
-                        <App />
-                    </DialogContextProvider>
-                </BrowserRouter>
-            </ThemeProvider>
+            <BrowserRouter>
+                <DialogContextProvider>
+                    <App />
+                </DialogContextProvider>
+            </BrowserRouter>
         </ReduxContextProvider>
     </React.StrictMode>
 );
