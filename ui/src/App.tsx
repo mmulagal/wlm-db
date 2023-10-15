57fc5b3737525efe8d017ad0384c4f34586e793a
rpanwar@netapp.com <rpanwar@netapp.com>
2023-10-15 17:47:35 +0530

    Dark theme changes

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
     useInitialize();
 
     //@ts-ignore
-    const isDarkTheme = useAppSelector(state => state?.auth?.features?.byKey['Platform.BlueXP/DarkTheme'].active);
+    const isDarkTheme = useAppSelector(state => state?.auth?.features?.active['Platform.BlueXP/DarkTheme']);
 
     // useEffect(() => {
     //     setDarkTheme(isDarkTheme);
