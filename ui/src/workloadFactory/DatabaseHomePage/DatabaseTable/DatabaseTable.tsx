1f8727823d56a29af7a73eb2d0164e86a6d4b3fd
Yadav, Sonam <Sonam.Sonam@netapp.com>
2023-10-17 20:29:40 +0530

    table menu updates

--- ui/src/workloadFactory/DatabaseHomePage/DatabaseTable/DatabaseTable.tsx
+++ ui/src/workloadFactory/DatabaseHomePage/DatabaseTable/DatabaseTable.tsx
@@ -21,20 +21,27 @@ const DatabaseTable = () => {
 
     const menuItems = [
         {
-            id: '1',
-            displayName: 'View full info'
+            id: 'resourceView',
+            displayName: 'View resource details'
         },
         {
-            id: '2',
-            displayName: 'Clone'
+            id: 'clone',
+            displayName: 'Clone',
+            disabled: true
         },
         {
-            id: '3',
-            displayName: 'Migrate'
+            id: 'migrate',
+            displayName: 'Migrate',
+            disabled: true
         },
         {
-            id: '4',
-            displayName: 'Protect'
+            id: 'protect',
+            displayName: 'Protect',
+            disabled: true
+        },
+        {
+            id: 'remove',
+            displayName: 'Remove'
         }
     ];
 
