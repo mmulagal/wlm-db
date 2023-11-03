3ae4b1aff2f00f3a4b9f8882b43644543136694d
srigowri <srigowri@netapp.com>
2023-11-03 13:02:04 +0530

    DBS-953 - New Credentials service integration - jwt token fix

--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -1,6 +1,7 @@
 import jwksRsa from 'jwks-rsa';
 import jsonwebtoken, { JwtPayload } from 'jsonwebtoken';
 import createError from 'http-errors';
+import { isEmpty } from 'lodash-es';
 import { AUTH0_SERVER_ADDRESS, AUTH0_AUDIENCE, USER_TENANCY_CACHE_TYPE, ADMIN_ROLE, USER_ROLE } from './consts';
 import getLogger from './logger';
 import { getPermissionsForUser, getTenancyAccounts, Account } from '../lib/cloud-manager/tenancy';
@@ -67,7 +68,7 @@ async function authorizeJwt(authToken: string, decodedToken: JwtPayload, account
     logger.debug('Authorize JWT:', { authToken, decodedToken, accountId });
 
     const tokenSub = decodedToken?.sub;
-    if (tokenSub && !tokenSub.endsWith('@clients')) {
+    if (tokenSub && !isEmpty(tokenSub) && !tokenSub?.endsWith('@clients')) {
         // service token ends with @clients, we cant get user permissions using service token so skipping auth for service token requests
         const unauthorizedErrorMessage = 'You do not have permission to access this resource';
 
