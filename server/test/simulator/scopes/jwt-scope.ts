6cd466a97946aa99811a716f978e924cec477bad
Sathish S <Sathish.S@netapp.com>
2023-11-03 11:38:36 +0530

    mocked the authorizeJWT with response for demo

--- server/test/simulator/scopes/jwt-scope.ts
+++ server/test/simulator/scopes/jwt-scope.ts
@@ -3,3 +3,5 @@ import jwtOperation from '../../../src/utils/jwt';
 
 // mocking the verifyToken by directly sending a fake response for simulator purpose only
 sinon.stub(jwtOperation, 'verifyToken').callsFake(async () => 'verified');
+
+sinon.stub(jwtOperation, 'authorizeJwt').callsFake(async () => {});
