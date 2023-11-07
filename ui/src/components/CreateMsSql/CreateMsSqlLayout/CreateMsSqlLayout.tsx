ce0e1217c41f85803cc7ca3bdaf45798b98f8a86
Ayush <payush@netapp.com>
2023-11-07 11:56:50 +0530

    Handling missing values on chatbot create click

--- ui/src/components/CreateMsSql/CreateMsSqlLayout/CreateMsSqlLayout.tsx
+++ ui/src/components/CreateMsSql/CreateMsSqlLayout/CreateMsSqlLayout.tsx
@@ -1,11 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './CreateMsSqlLayout.module.scss';
 import DeploymentTabs from './DeploymentTabs/DeploymentTabs';
 import MSSqlServer from '../MSSqlServer/MSSqlServer';
 import Chatbot from '../Chatbot/Chatbot';
+import { setIsShow } from '../../../store/chatbot/chatbotSlice';
+import { useDispatch } from 'react-redux';
 
 const CreateMsSqlLayout = () => {
     const [selectedTab, setSelectedTab] = useState<'wizard' | 'chatbot'>('wizard');
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(setIsShow(selectedTab === 'chatbot'));
+    }, [selectedTab, dispatch]);
+
     return (
         <div className={styles['create-mssql-layout']}>
             <DeploymentTabs
