ab32f92d34566df7a50c111b1ee460ab1117d5d1
R Ranganatha <rranga@netapp.com>
2023-11-09 08:37:00 +0530

    1. Improved getStorageData() 	- to use only volume tags for savings data retrieval. 	- to not return any data in case of failures. 2. Removed spaceSavingsPercent field from StorageResponse type.

--- server/src/operations/aws/fsx-operations.ts
+++ server/src/operations/aws/fsx-operations.ts
@@ -181,25 +181,21 @@ async function getStorageDataUsingSSM(
 ) {
     logger.info('Fetching storage savings details', credentialsId, region, activeNodeInstanceId);
 
-    try {
-        const commands = [
-            `C:\\SSM\\OntapRestGet.ps1 -FSxSecretName ${fsxSecret} -FSxID ${fileSystemId} -FSxRegion ${region} -OntapResourceEndpoint '${apiEndpoint}' -OntapResourceFilter '${apiFilter}' -OntapResourceQuery '${apiQuery}'`
-        ];
+    const commands = [
+        `C:\\SSM\\OntapRestGet.ps1 -FSxSecretName ${fsxSecret} -FSxID ${fileSystemId} -FSxRegion ${region} -OntapResourceEndpoint '${apiEndpoint}' -OntapResourceFilter '${apiFilter}' -OntapResourceQuery '${apiQuery}'`
+    ];
 
-        const response = await callSsmExecution(
-            credentialsId,
-            region,
-            commands,
-            activeNodeInstanceId,
-            standbyNodeInstanceId
-        );
+    const response = await callSsmExecution(
+        credentialsId,
+        region,
+        commands,
+        activeNodeInstanceId,
+        standbyNodeInstanceId
+    );
 
-        const cleanResponse = response?.replaceAll('\r\n', '');
-        const jsonResponse = JSON.parse(cleanResponse!);
-        return jsonResponse;
-    } catch (error) {
-        logger.error('Failed to fetch storage savings details. Reason:', { error });
-    }
+    const cleanResponse = response?.replaceAll('\r\n', '');
+    const jsonResponse = JSON.parse(cleanResponse!);
+    return jsonResponse;
 }
 
 async function getVolumeIds(credentialsId: string, region: string, fsxId: string) {
