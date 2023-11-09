cf7436261ed3d69b83fb6ca365650f39789528ae
Sathish S <Sathish.S@netapp.com>
2023-11-09 14:20:38 +0530

    validation for route table is modified

--- server/src/utils/utils.ts
+++ server/src/utils/utils.ts
@@ -132,22 +132,16 @@ function isNetworkConfigurationViolated(networkConfiguration: CFNetworkConfigura
     if (deploymentMode === STANDALONE) {
         return !networkConfiguration.privateSubnet1Id || !networkConfiguration.routeTable1Id;
     }
-    // In simulator route table 1 and route table 2 id will be always same, so we cant check that condition
-    if (process.env.NODE_ENV === 'demo' || process.env.NODE_ENV === 'simulator') {
-        return (
-            !networkConfiguration.privateSubnet1Id ||
-            !networkConfiguration.privateSubnet2Id ||
-            !networkConfiguration.routeTable1Id ||
-            !networkConfiguration.routeTable2Id
-        );
-    }
-    return (
+    const isValidated =
         !networkConfiguration.privateSubnet1Id ||
         !networkConfiguration.privateSubnet2Id ||
         !networkConfiguration.routeTable1Id ||
-        !networkConfiguration.routeTable2Id ||
-        networkConfiguration.routeTable1Id === networkConfiguration.routeTable2Id
-    );
+        !networkConfiguration.routeTable2Id;
+    // In simulator route table 1 and route table 2 id will be always same, so we cant check that condition
+    if (process.env.NODE_ENV === 'demo' || process.env.NODE_ENV === 'simulator') {
+        return isValidated;
+    }
+    return isValidated || networkConfiguration.routeTable1Id === networkConfiguration.routeTable2Id;
 }
 
 function checkAndRetrieveJsonObject(str: string | undefined) {
