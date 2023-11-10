18a4f3cf3419130d0583d337868cb4efebae0ec9
rpanwar@netapp.com <rpanwar@netapp.com>
2023-11-10 16:16:39 +0530

    Code box CSS fixes

--- ui/src/workloadFactory/DatabaseHomePage/Sidebar/Sidebar.tsx
+++ ui/src/workloadFactory/DatabaseHomePage/Sidebar/Sidebar.tsx
@@ -458,7 +458,7 @@ const Sidebar = ({ isOpen, onClose }: any) => {
                 <LoadingCodeBox text={CODE_VIEWER.LOADING_AWS_CLI} />
             ) : (
                 <HighlighterWord highlight={searchInput} isAWSCli={true} count={countDetails}>
-                    <Typography variant="Regular_16" className={styles.colorAutomation}>
+                    <Typography variant="Regular_14" className={styles.colorAutomation}>
                         {getRightPanelTemplateResponse(openKey)?.cliCommand || CODE_VIEWER.NO_DATA_MSG}
                     </Typography>
                 </HighlighterWord>
