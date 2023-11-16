610a47b1d1b160386154e6b0d32e0809231d29c3
krithib <krithib@netapp.com>
2023-11-16 14:24:03 +0530

    DBS-1280 - Status code is 500 for PATCH wlmdb/v1/configs/<Invalid config-id> for invalid id

--- server/src/operations/database/database-operations.ts
+++ server/src/operations/database/database-operations.ts
@@ -15,20 +15,24 @@ const logger = getLogger();
 
 async function getSavedConfig(accountId: string, id: string): Promise<FormConfigObjectResponseType> {
     logger.info('Load individual saved config ', accountId);
-
-    const [{ user, creation_time: creationTime, data, name, modified_time: modifiedTime }] = await listConfig(
-        accountId,
-        id
-    );
-    return {
-        accountId,
-        id,
-        user,
-        creationTime: moment(creationTime).unix() * 1000,
-        data,
-        name,
-        ...(modifiedTime && { modifiedTime: moment(modifiedTime).unix() * 1000 })
-    };
+    try {
+        const [{ user, creation_time: creationTime, data, name, modified_time: modifiedTime }] = await listConfig(
+            accountId,
+            id
+        );
+        return {
+            accountId,
+            id,
+            user,
+            creationTime: moment(creationTime).unix() * 1000,
+            data,
+            name,
+            ...(modifiedTime && { modifiedTime: moment(modifiedTime).unix() * 1000 })
+        };
+    } catch (error) {
+        logger.error(`Error occured while deleting saved config ${id}. Error: ${error}`);
+        throw createError(HttpErrorCodes.NOT_FOUND, CONFIG_NOT_FOUND(id));
+    }
 }
 
 async function deleteSavedConfig(accountId: string, id: string): Promise<void> {
@@ -90,11 +94,16 @@ async function modifyConfig(
     logger.info('Modify config ', { accountId, configId });
     logger.debug('Modify config data', data);
 
-    const { id } = await updateConfig(accountId, configId, {
-        name,
-        data
-    });
-    return { id };
+    try {
+        const { id } = await updateConfig(accountId, configId, {
+            name,
+            data
+        });
+        return { id };
+    } catch (error) {
+        logger.error(`Error occured while updating saved config ${configId}. Error: ${error}`);
+        throw createError(HttpErrorCodes.NOT_FOUND, CONFIG_NOT_FOUND(configId));
+    }
 }
 
 async function getAllDeploymentStatus(accountId: string): Promise<DeploymentStatusListResponseType> {
