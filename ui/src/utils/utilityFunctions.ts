53e0dd420da6f6823d211921c4214f419fa47a7f
Ayush <payush@netapp.com>
2023-11-17 12:41:47 +0530

    removed console

--- ui/src/utils/utilityFunctions.ts
+++ ui/src/utils/utilityFunctions.ts
@@ -747,7 +747,6 @@ export const delay = (ms: number) => {
 };
 
 export const getChatbotParamsFromPayload = (payload: any) => {
-    console.log(payload);
     let params: any = {};
     if (payload?.awsAccount?.selectedCredential?.data?.credentialsId) {
         params.credentialsId = payload.awsAccount.selectedCredential.data.credentialsId;
