90ca440cb3942dca46adbbfbbdfdc7e6e542ccbb
yashendr <yashendr@netapp.com>
2023-11-20 14:46:07 +0530

    DBS-1265 - maintain already filled params if dependency changes

--- server/src/lib/chatbot/helpers.ts
+++ server/src/lib/chatbot/helpers.ts
@@ -101,7 +101,7 @@ async function validateParams(
 ): Promise<{ errors: Array<ValidationResponse>; params: Params }> {
     // let errors: { [x: string]: any } = {};
     logger.info('Validate Params', { params, oldParams });
-    let validatedParams: Params = { ...oldParams };
+    let validatedParams: Params = {};
     const promises = [];
     const errors: Array<ValidationResponse> = [];
     for (const reqParam of schemaParams) {
