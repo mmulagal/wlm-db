570068176efa044737174adc6c5756e7dc3c6e69
rpanwar@netapp.com <rpanwar@netapp.com>
2023-12-13 16:35:19 +0530

    Remove CF button from wizard for WF

--- ui/src/components/CreateMsSql/MSSqlServer/MSSqlServer.tsx
+++ ui/src/components/CreateMsSql/MSSqlServer/MSSqlServer.tsx
@@ -3,15 +3,18 @@ import MSSqlAccordions from './MSSqlAccordions';
 import CloudFormation from '../CloudFormation/CloudFormation';
 
 import SelectConfig from '../SelectConfig/SelectConfig';
+import { useAppSelector } from '../../../store/storeHooks';
 
 const MSSqlServer = () => {
+    const { isWorkloadFactory } = useAppSelector(state => state?.auth);
     return (
         <>
             <SelectConfig />
             {/* setting up Accordions Group here */}
             <MSSqlAccordions />
+            {isWorkloadFactory && <div style={{ marginBottom: '32px' }} />}
             {/* Accordions end here */}
-            <CloudFormation />
+            {!isWorkloadFactory && <CloudFormation />}
         </>
     );
 };
