8914ce0b0d778f654101847e54158f8be93fda7d
yashendr <yashendr@netapp.com>
2023-12-14 12:38:38 +0530

    resolve comments

--- server/src/utils/utils.ts
+++ server/src/utils/utils.ts
@@ -224,6 +224,14 @@ function sizeInGigaBytes(size: number, currentUnit: string = 'MB') {
     }
 }
 
+/**
+ *
+ * @param fn - function that returns a boolean when the response is correct
+ * @param delay - interval after which the function should be invoked
+ * @param maxDelay - total delay or timeout, if the function is not resolved within this time, we consider it a failure
+ * @returns Promise that can be awaited
+ */
+
 function waitForResolution(fn: () => boolean, delay: number, maxDelay: number) {
     return Promise.race([
         sleep(maxDelay),
