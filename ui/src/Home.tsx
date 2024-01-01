a24e68461ade2a35ec272e946dcee793d38bafdd
Sonam Sonam <sonamy@netapp.com>
2024-01-01 17:28:05 +0530

    job monitoring table

--- ui/src/Home.tsx
+++ ui/src/Home.tsx
@@ -14,6 +14,7 @@ import { clearNotifications, removeNotification } from './store/notificationSlic
 import DatabaseHomePage from './workloadFactory/DatabaseHomePage/DatabaseHomePage';
 import PreviewPanel from './components/PreviewPanel/PreviewPanel';
 import DatabaseHostOverview from './workloadFactory/ResourcePage/ResourceHomePage/DatabaseHostOverview';
+import JobMonitoring from './workloadFactory/JobMonitoring/JobMonitoring';
 
 const Home = () => {
     const notificationsObj = useSelector((state: any) => state.notifications);
@@ -48,6 +49,7 @@ const Home = () => {
                         <Route path={'tables'} element={<Tables />} />
                     </Route>
                     <Route path={'databases'} element={<DatabaseHomePage />} />
+                    <Route path={'job-monitor'} element={<JobMonitoring />} />
                     <Route path="*" element={<MainComponent />} />
                 </Routes>
             </div>
--- ui/src/workloadFactory/JobMonitoring/JobMonitoring.tsx
+++ ui/src/workloadFactory/JobMonitoring/JobMonitoring.tsx
@@ -1,18 +1,26 @@
 import { Typography } from '@netapp/design-system';
 import LineChart from '../DatabaseHomePage/LineChart/LineChart';
 import styles from './JobMonitoring.module.scss';
+import JobMonitoringTable from './JobMonitoringTable/JobMonitoringTable';
 function JobMonitoring() {
     return (
-        <div className={styles.jobMonitoring}>
-            <div className={styles.headSection}>
-                <Typography variant="Regular_16" className={styles.title}>
-                    Overtime jobs
-                </Typography>
+        <>
+            <div className={styles.jobMonitoring}>
+                <div className={styles.overtimeJobs}>
+                    <div className={styles.headSection}>
+                        <Typography variant="Regular_16" className={styles.title}>
+                            Overtime jobs
+                        </Typography>
+                    </div>
+                    <div className={styles.mainSection}>
+                        <LineChart startColor="#A815F3" endColor="rgba(168, 21, 243, 0.00)" />
+                    </div>
+                </div>
+                <div className={styles.tableSection}>
+                    <JobMonitoringTable />
+                </div>
             </div>
-            <div className={styles.mainSection}>
-                <LineChart startColor="#A815F3" endColor="rgba(168, 21, 243, 0.00)" />
-            </div>
-        </div>
+        </>
     );
 }
 
