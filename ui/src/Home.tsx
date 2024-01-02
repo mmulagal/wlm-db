6e58959a8bb39b2f7e6dd56ad858bc8bbb5ef8e1
rpanwar@netapp.com <rpanwar@netapp.com>
2024-01-02 09:49:07 +0530

    Job monitoring second commit

--- ui/src/Home.tsx
+++ ui/src/Home.tsx
@@ -14,6 +14,7 @@ import { clearNotifications, removeNotification } from './store/notificationSlic
 import DatabaseHomePage from './workloadFactory/DatabaseHomePage/DatabaseHomePage';
 import PreviewPanel from './components/PreviewPanel/PreviewPanel';
 import DatabaseHostOverview from './workloadFactory/ResourcePage/ResourceHomePage/DatabaseHostOverview';
+import JobMonitoring from './workloadFactory/JobMonitoring/JobMonitoring';
 
 const Home = () => {
     const notificationsObj = useSelector((state: any) => state.notifications);
@@ -38,10 +39,8 @@ const Home = () => {
                         path={`add-working-environment/database-services/:storage/discover`}
                         element={<DiscoverPage />}
                     />
-                    <Route
-                        path={`database-overview`}
-                        element={<DatabaseHostOverview />}
-                    />
+                    <Route path={`database-overview`} element={<DatabaseHostOverview />} />
+                    <Route path={`job-monitor`} element={<JobMonitoring />} />
                     <Route path={`mssql/:resourceId/:resourceName/`} element={<ResourcePage />}>
                         <Route path={'overview'} element={<MsSqlOverview />} />
                         <Route path={'databases'} element={<Databases />} />
