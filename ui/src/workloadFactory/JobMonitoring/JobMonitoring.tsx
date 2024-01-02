0b53cb76c994eda8e0bf0b00e8e29bfff88638d1
rpanwar@netapp.com <rpanwar@netapp.com>
2024-01-02 11:35:27 +0530

    Added Breadcrumb and Heading in Job monitoring page

--- ui/src/workloadFactory/JobMonitoring/JobMonitoring.tsx
+++ ui/src/workloadFactory/JobMonitoring/JobMonitoring.tsx
@@ -1,16 +1,44 @@
 import { Typography } from '@netapp/design-system';
+import { useNavigate } from 'react-router-dom';
 import LineChart from '../DatabaseHomePage/LineChart/LineChart';
 import styles from './JobMonitoring.module.scss';
+import BreadCrumbs from '../../common/BreadCrumbs/BreadCrumbs';
+import { GENERAL } from '../../utils/appConstants';
 function JobMonitoring() {
+    const navigate = useNavigate();
     return (
         <div className={styles.jobMonitoring}>
-            <div className={styles.headSection}>
-                <Typography variant="Regular_16" className={styles.title}>
-                    Overtime jobs
+            <div className={styles.breadCrumb}>
+                <BreadCrumbs
+                    items={[
+                        {
+                            title: GENERAL.DATABASES,
+                            onClick: () => {
+                                navigate('../databases');
+                            }
+                        },
+                        {
+                            title: 'Job monitoring'
+                        }
+                    ]}
+                />
+            </div>
+
+            <div className={styles.headingContainer}>
+                <Typography variant="Regular_24" className={styles.heading}>
+                    Job monitoring
                 </Typography>
             </div>
-            <div className={styles.mainSection}>
-                <LineChart startColor="#A815F3" endColor="rgba(168, 21, 243, 0.00)" />
+
+            <div className={styles.overtimeJobs}>
+                <div className={styles.headSection}>
+                    <Typography variant="Regular_16" className={styles.title}>
+                        Overtime jobs
+                    </Typography>
+                </div>
+                <div className={styles.mainSection}>
+                    <LineChart startColor="#A815F3" endColor="rgba(168, 21, 243, 0.00)" />
+                </div>
             </div>
         </div>
     );
