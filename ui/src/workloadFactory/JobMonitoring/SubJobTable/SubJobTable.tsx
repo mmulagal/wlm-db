4b2e0316f88cf53d358150f183333ee75e69e3fa
rpanwar@netapp.com <rpanwar@netapp.com>
2024-01-04 16:00:17 +0530

    DBS-1630 added fixed space in 2nd level table while scrolling

--- ui/src/workloadFactory/JobMonitoring/SubJobTable/SubJobTable.tsx
+++ ui/src/workloadFactory/JobMonitoring/SubJobTable/SubJobTable.tsx
@@ -7,62 +7,69 @@ import { ReactComponent as Success } from '../../../assets/success.svg';
 import { ReactComponent as ErrorIcon } from '../../../assets/error-icon.svg';
 import TaskTable from '../TaskTable/TaskTable';
 
-const SubJobTable = ({statusType}: any) => {
-
+const SubJobTable = ({ statusType }: any) => {
     const ExpandedRow = () => {
-        return (
-            <TaskTable/>
-        )
-      };
+        return <TaskTable />;
+    };
 
     const jobsList: any[] = [
         {
             name: '9876543219236789',
-            description: 'Microsoft SQL server deployed with stack <stack-name>. Microsoft SQL server deployed with stack <stack-name>',
+            description:
+                'Microsoft SQL server deployed with stack <stack-name>. Microsoft SQL server deployed with stack <stack-name>',
             status: 'Completed',
             startTime: 'December 20, 2023, 10:25:45',
             endTime: 'December 20, 2023, 12:25:45'
         },
         {
             name: '2876543219236789',
-            description: 'Microsoft SQL server deployed with stack <stack-name>. Microsoft SQL server deployed with stack <stack-name>',
+            description:
+                'Microsoft SQL server deployed with stack <stack-name>. Microsoft SQL server deployed with stack <stack-name>',
             status: 'Completed',
             startTime: 'December 20, 2023, 10:25:45',
             endTime: 'December 20, 2023, 12:25:45'
         },
         {
             name: '4876543219006789',
-            description: 'Microsoft SQL server deployed with stack <stack-name>. Microsoft SQL server deployed with stack <stack-name>',
+            description:
+                'Microsoft SQL server deployed with stack <stack-name>. Microsoft SQL server deployed with stack <stack-name>',
             status: 'Failed',
             startTime: 'December 20, 2023, 10:25:45',
             endTime: 'December 20, 2023, 12:25:45'
         },
         {
             name: '3876543219006789',
-            description: 'Microsoft SQL server deployed with stack <stack-name>. Microsoft SQL server deployed with stack <stack-name>',
+            description:
+                'Microsoft SQL server deployed with stack <stack-name>. Microsoft SQL server deployed with stack <stack-name>',
             status: 'Running',
             startTime: 'December 20, 2023, 10:25:45',
             endTime: 'December 20, 2023, 12:25:45'
         },
         {
             name: '7876543219036789',
-            description: 'Microsoft SQL server deployed with stack <stack-name>. Microsoft SQL server deployed with stack <stack-name>',
+            description:
+                'Microsoft SQL server deployed with stack <stack-name>. Microsoft SQL server deployed with stack <stack-name>',
             status: 'Running',
             startTime: 'December 20, 2023, 10:25:45',
             endTime: 'December 20, 2023, 12:25:45'
         },
         {
             name: '8876543219036789',
-            description: 'Microsoft SQL server deployed with stack <stack-name>. Microsoft SQL server deployed with stack <stack-name>',
+            description:
+                'Microsoft SQL server deployed with stack <stack-name>. Microsoft SQL server deployed with stack <stack-name>',
             status: 'Completed',
             startTime: 'December 20, 2023, 10:25:45',
             endTime: 'December 20, 2023, 12:25:45'
         }
     ];
 
-    const expandRow = (updateRowState: (arg0: any) => { (arg0: { isExpanded: boolean; }): void; new(): any; }, rowData: { id: any; }, currentRowState: { isExpanded: any; }) => {
+    const expandRow = (
+        updateRowState: (arg0: any) => { (arg0: { isExpanded: boolean }): void; new (): any },
+        rowData: { id: any },
+        currentRowState: { isExpanded: any }
+    ) => {
         updateRowState(rowData.id)({
-            isExpanded: !currentRowState?.isExpanded,
+            isExpanded: !currentRowState?.isExpanded
         });
     };
 
@@ -74,23 +81,20 @@ const SubJobTable = ({statusType}: any) => {
             width: '56px',
             isSticky: true,
             className: styles.firstCol,
-            renderCell: (
-              value: any,
-              rowData: any,
-              { updateRowState, rowsState }: any
-            ) => {
-              const currentRowState = rowsState[rowData.id];
-              const statusType = rowData?.status.toLowerCase();
-              return (
-                <>
-                    <div className={styles.arrow}>
-                        <ArrowIcon 
-                            className={currentRowState?.isExpanded ? styles['arrow-down'] : ''} 
-                            onClick={() => expandRow(updateRowState, rowData, currentRowState)}  />
-                    </div>
-                </>
-              );
-            },
+            renderCell: (value: any, rowData: any, { updateRowState, rowsState }: any) => {
+                const currentRowState = rowsState[rowData.id];
+                const statusType = rowData?.status.toLowerCase();
+                return (
+                    <>
+                        <div className={styles.arrow}>
+                            <ArrowIcon
+                                className={currentRowState?.isExpanded ? styles['arrow-down'] : ''}
+                                onClick={() => expandRow(updateRowState, rowData, currentRowState)}
+                            />
+                        </div>
+                    </>
+                );
+            }
         },
         {
             id: '1',
@@ -98,14 +102,14 @@ const SubJobTable = ({statusType}: any) => {
             accessor: 'name',
             isSortable: true,
             width: '230px',
-            isSticky: true,
+            isSticky: true
         },
         {
             id: '2',
             Header: 'Description',
             accessor: 'description',
             isSortable: true,
-            width: '498px',
+            width: '498px'
         },
         {
             id: '3',
@@ -123,7 +127,7 @@ const SubJobTable = ({statusType}: any) => {
                         </div>
                         <div>{cellData}</div>
                     </div>
-                )
+                );
             }
         },
         {
@@ -131,20 +135,20 @@ const SubJobTable = ({statusType}: any) => {
             Header: 'Start Time',
             accessor: 'startTime',
             isSortable: true,
-            width: '240px',
+            width: '240px'
         },
         {
             id: '5',
             Header: 'End Time',
             accessor: 'endTime',
             isSortable: true,
-            width: '240px',
+            width: '240px'
         },
         {
             id: '6',
             Header: '',
             accessor: '',
-            width: '56px',
+            width: '56px'
         }
     ];
 
@@ -154,7 +158,7 @@ const SubJobTable = ({statusType}: any) => {
         rows: jobsList,
         pageSize: 50,
         selectionType: 'none',
-        isHorizontalScroll: true,
+        isHorizontalScroll: true
     });
 
     const tableComponentProps = {
@@ -165,17 +169,18 @@ const SubJobTable = ({statusType}: any) => {
     return (
         <>
             <div className={styles.subJobTable}>
-                <div className={`${styles.statusbar} ${styles[statusType]}`}>&nbsp;</div>
+                <div className={`${styles.statusbar} ${styles[statusType]} ${styles.extraDiv}`}>&nbsp;</div>
+                <div className={styles.extraDiv2} />
                 <div
                     //  @ts-ignore
                     className={`${styles.table}`}
-                >   
+                >
                     <Table
                         {...tableComponentProps}
                         //@ts-ignore
                         tableProps={tableProps}
                         isDoubleRow={true}
-                        variant='innerTable'
+                        variant="innerTable"
                     />
                 </div>
             </div>
