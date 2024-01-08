774059be4761f51d636e08ad2a4530d3de94c88f
yashendr <yashendr@netapp.com>
2024-01-08 11:31:56 +0530

    DBS-1616 - AI - update the final message as provided in the description

--- server/src/operations/chatbot-operations.ts
+++ server/src/operations/chatbot-operations.ts
@@ -53,7 +53,7 @@ async function queryBot(query: string, oldParams?: { [x: string]: any }) {
                 }
                 return {
                     message:
-                        'Great, we are done with all the requirements, please verify the generated json and proceed to deploy!',
+                        'Congratulations, you have completed filling in all required parameters. Please verify the genereated json including predefined parameters in the Codebox and hit the Create button to deploy. If you would like to modify a predefined parameter please request to modify the field.',
                     intent: {
                         complete: true,
                         params: validationResponse.params
