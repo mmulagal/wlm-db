a0cdcd255a071acc26cebf0e8c1701828c4108cf
mshreyas <mshreyas@netapp.com>
2024-01-10 16:02:25 +0530

    DBS-1725 hiding WE APIs

--- server/src/routes/schemas/working-environment-schemas.ts
+++ server/src/routes/schemas/working-environment-schemas.ts
@@ -9,6 +9,7 @@ import {
 
 const GetWorkingEnvironmentsSchema = {
     tags: [RouteTags.WORKING_ENVIRONMENT],
+    hide: true,
     summary: 'Get working environments',
     description: 'Get working environments for the selected account',
     params: AccountIdParams,
@@ -19,6 +20,7 @@ const GetWorkingEnvironmentsSchema = {
 
 const GetWorkingEnvironmentSchema = {
     tags: [RouteTags.WORKING_ENVIRONMENT],
+    hide: true,
     summary: 'Get working environment details',
     description: 'Get working environment details for the given Id',
     params: AccountIdAndWorkingEnvironmentIdParams,
@@ -29,6 +31,7 @@ const GetWorkingEnvironmentSchema = {
 
 const GetRelationshipsSchema = {
     tags: [RouteTags.WORKING_ENVIRONMENT],
+    hide: true,
     summary: 'Get relationships of working environments',
     description: 'Get relationships of all working environments',
     params: AccountIdParams,
