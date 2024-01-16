da19a8dca3727b87081ee4378f86e8440912ed88
Srivastava, Piyush <Piyush.Srivastava@netapp.com>
2024-01-16 14:50:02 +0530

    UT update

--- server/test/operations/deployment-operations.test.ts
+++ server/test/operations/deployment-operations.test.ts
@@ -1,82 +1,94 @@
-// import { faker } from '@faker-js/faker';
-// import {
-//     createCloudFormationTemplateForUserDeployment,
-//     deployCloudFormationTemplate,
-//     getCloudformationTemplate,
-//     deployStackOrCreateTemplateURL
-// } from '../../src/operations/deployment-operations';
-// import '../simulator/scopes/aws/s3-scope';
-// import '../simulator/scopes/aws/ec2-scope';
-// import '../simulator/scopes/aws/iam-scope';
-// import '../simulator/scopes/aws/secrets-manager-scope';
-// import '../simulator/scopes/aws/cloud-formation-scope';
-// import '../simulator/scopes/cloud-manager/cloud-manager-credentials-scope';
-// import '../simulator/scopes/cloud-manager/workload-factory-credentials-scope';
-// import '../simulator/scopes/cloud-manager/cloud-manager-tenancy-scope';
-// import '../simulator/scopes/cloud-manager/workload-factory-auth-scope';
-// import '../simulator/scopes/aws/service-quota-scope';
-// import '../simulator/scopes/opentelemetry-scope';
-// import '../simulator/scopes/jwt-scope';
-// import {
-//     DEFAULT_AWS_REGION,
-//     SQL_CONFIGURATION,
-//     FSX_CONFIGURATION,
-//     AD_CONFIGURATION,
-//     EC2_CONFIGURATION,
-//     NETWORKING_CONFIGURATION
-// } from '../utils/consts';
-// import { SECRETS } from '../../src/utils/consts';
+import { faker } from '@faker-js/faker';
+import {
+    createCloudFormationTemplateForUserDeployment,
+    deployCloudFormationTemplate,
+    getCloudformationTemplate,
+    deployStackOrCreateTemplateURL
+} from '../../src/operations/deployment-operations';
+import '../simulator/scopes/aws/s3-scope';
+import '../simulator/scopes/aws/ec2-scope';
+import '../simulator/scopes/aws/iam-scope';
+import '../simulator/scopes/aws/secrets-manager-scope';
+import '../simulator/scopes/aws/cloud-formation-scope';
+import '../simulator/scopes/cloud-manager/cloud-manager-credentials-scope';
+import '../simulator/scopes/cloud-manager/workload-factory-credentials-scope';
+import '../simulator/scopes/cloud-manager/cloud-manager-tenancy-scope';
+import '../simulator/scopes/cloud-manager/workload-factory-auth-scope';
+import '../simulator/scopes/aws/service-quota-scope';
+import '../simulator/scopes/opentelemetry-scope';
+import '../simulator/scopes/jwt-scope';
+import {
+    DEFAULT_AWS_REGION,
+    SQL_CONFIGURATION,
+    FSX_CONFIGURATION,
+    AD_CONFIGURATION,
+    EC2_CONFIGURATION,
+    NETWORKING_CONFIGURATION
+} from '../utils/consts';
+import { SECRETS } from '../../src/utils/consts';
 
-// const credentialsid = `${faker.string.alphanumeric(20)}`;
-// SECRETS.AUTH_CLIENT_ID = `${faker.string.alphanumeric(20)}`;
-// SECRETS.SIGNURL_ACCESS_KEY = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-// SECRETS.SIGNURL_SECRET_KEY = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const credentialsid = `${faker.string.alphanumeric(20)}`;
+SECRETS.AUTH_CLIENT_ID = `${faker.string.alphanumeric(20)}`;
+SECRETS.SIGNURL_ACCESS_KEY = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+SECRETS.SIGNURL_SECRET_KEY = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-// describe('Cloud formation operations', () => {
-//     it('Create the cloud formation template url for user deployment', async () => {
-//         const resp = await createCloudFormationTemplateForUserDeployment(
-//             credentialsid,
-//             DEFAULT_AWS_REGION,
-//             NETWORKING_CONFIGURATION,
-//             EC2_CONFIGURATION,
-//             AD_CONFIGURATION,
-//             FSX_CONFIGURATION,
-//             SQL_CONFIGURATION
-//         );
-//         expect(resp).toBeDefined();
-//     });
-//     it('Create the cloud formation template url or deploy stack', async () => {
-//         const resp = await deployStackOrCreateTemplateURL(
-//             credentialsid,
-//             DEFAULT_AWS_REGION,
-//             NETWORKING_CONFIGURATION,
-//             EC2_CONFIGURATION,
-//             AD_CONFIGURATION,
-//             FSX_CONFIGURATION,
-//             SQL_CONFIGURATION
-//         );
-//         expect(resp).toBeDefined();
-//     });
-//     it('Deploy cloud formation template', async () => {
-//         const resp = await deployCloudFormationTemplate(
-//             credentialsid,
-//             DEFAULT_AWS_REGION,
-//             NETWORKING_CONFIGURATION,
-//             EC2_CONFIGURATION,
-//             AD_CONFIGURATION,
-//             FSX_CONFIGURATION,
-//             SQL_CONFIGURATION
-//         );
-//         expect(resp).toBeDefined();
-//     });
-//     it('Get cloud formation template', async () => {
-//         const resp = await getCloudformationTemplate(
-//             NETWORKING_CONFIGURATION,
-//             EC2_CONFIGURATION,
-//             AD_CONFIGURATION,
-//             FSX_CONFIGURATION,
-//             SQL_CONFIGURATION
-//         );
-//         expect(resp).toBeDefined();
-//     });
-// });
+describe('Cloud formation operations', () => {
+    it('Create the cloud formation template url for user deployment', async () => {
+        const resp = await createCloudFormationTemplateForUserDeployment(
+            credentialsid,
+            DEFAULT_AWS_REGION,
+            NETWORKING_CONFIGURATION,
+            EC2_CONFIGURATION,
+            AD_CONFIGURATION,
+            FSX_CONFIGURATION,
+            SQL_CONFIGURATION,
+            '',
+            false,
+            'triggered-from:chatbot,instance-type:m5.large'
+        );
+        expect(resp).toBeDefined();
+    });
+    it('Create the cloud formation template url or deploy stack', async () => {
+        const resp = await deployStackOrCreateTemplateURL(
+            credentialsid,
+            DEFAULT_AWS_REGION,
+            NETWORKING_CONFIGURATION,
+            EC2_CONFIGURATION,
+            AD_CONFIGURATION,
+            FSX_CONFIGURATION,
+            SQL_CONFIGURATION,
+            '',
+            false,
+            'chatbot'
+        );
+        expect(resp).toBeDefined();
+    });
+    it('Deploy cloud formation template', async () => {
+        const resp = await deployCloudFormationTemplate(
+            credentialsid,
+            DEFAULT_AWS_REGION,
+            NETWORKING_CONFIGURATION,
+            EC2_CONFIGURATION,
+            AD_CONFIGURATION,
+            FSX_CONFIGURATION,
+            SQL_CONFIGURATION,
+            '',
+            false,
+            'triggered-from:chatbot,instance-type:m5.large'
+        );
+        expect(resp).toBeDefined();
+    });
+    it('Get cloud formation template', async () => {
+        const resp = await getCloudformationTemplate(
+            NETWORKING_CONFIGURATION,
+            EC2_CONFIGURATION,
+            AD_CONFIGURATION,
+            FSX_CONFIGURATION,
+            SQL_CONFIGURATION,
+            '',
+            false,
+            'chatbot'
+        );
+        expect(resp).toBeDefined();
+    });
+});
