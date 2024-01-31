d37a8458816edeada6ffe2e7e6077b49aad217e9
krithib <krithib@netapp.com>
2024-01-31 14:05:48 +0530

    DBS-1891 - For Successfull Cloudformation template deployment, due to "InvalidCiphertextException: UnknownError" ,parent stack is marked as FAILED

--- server/src/operations/deployment-operations.ts
+++ server/src/operations/deployment-operations.ts
@@ -355,7 +355,11 @@ async function getCloudformationTemplate(
     // Generate parameters list for quick create url command
     let urlParams: string = `stackName=${stackName}`;
     templateParameters.forEach(e => {
-        urlParams += `&param_${e.ParameterKey}=${e.ParameterValue}`;
+        if (e.ParameterKey === TEMPLATE_FSX_PASSWORD && e.ParameterValue) {
+            urlParams += `&param_${TEMPLATE_FSX_PASSWORD}=${encodeURIComponent(e.ParameterValue)}`;
+        } else {
+            urlParams += `&param_${e.ParameterKey}=${e.ParameterValue}`;
+        }
     });
     const signedTemplateURL = `${CLOUD_FORMATION_STACK_URL}?region=${
         region || undefined // explicitly needs to be send if the region is empty string -- cloudformation would not take an empty string
@@ -542,7 +546,7 @@ async function createCloudFormationTemplateForUserDeployment(
     if (fsxConfiguration.fsxPassword) {
         const encryptedFsxPassword = await encryptString(fsxConfiguration.fsxPassword);
         if (encryptedFsxPassword) {
-            templateParams += `&param_${TEMPLATE_FSX_PASSWORD}=${encryptedFsxPassword}`;
+            templateParams += `&param_${TEMPLATE_FSX_PASSWORD}=${encodeURIComponent(encryptedFsxPassword)}`;
         }
     }
 
