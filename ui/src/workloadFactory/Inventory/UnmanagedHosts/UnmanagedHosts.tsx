2c4ad879037fbb1c835647544634a951c9584140
Sonam Sonam <sonamy@netapp.com>
2024-02-02 13:11:20 +0530

    managed tables

---
+++ ui/src/workloadFactory/Inventory/UnmanagedHosts/UnmanagedHosts.tsx
@@ -0,0 +1,11 @@
+import styles from './UnmanagedHosts.module.scss';
+
+const UnmanagedHosts = () => {
+    return (
+        <div className={styles.unmanagedHosts}>
+            Unmanaged Hosts
+        </div>
+    )
+};
+
+export default UnmanagedHosts;
