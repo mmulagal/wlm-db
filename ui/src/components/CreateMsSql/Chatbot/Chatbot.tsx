29e9b98fcf2f0f72aaf2d7df97d51b6b081b7616
Ayush <payush@netapp.com>
2024-02-02 19:05:24 +0530

    Post load config issues fixed

--- ui/src/components/CreateMsSql/Chatbot/Chatbot.tsx
+++ ui/src/components/CreateMsSql/Chatbot/Chatbot.tsx
@@ -79,6 +79,7 @@ import { NOTIFICATION_TYPES, addNotification, clearNotifications } from '../../.
 import { useNavigate } from 'react-router-dom';
 import { navigateToCanvas } from '../../../utils/appConfig';
 import MissingPermissionsMsg from '../AwsSettings/AwsAccount/MissingPermissionsMsg';
+import store from '../../../store/store';
 const _ = require('lodash');
 
 type optionsType = {
@@ -295,6 +296,7 @@ const Chatbot = () => {
     };
 
     const handleCreate = () => {
+        const state = store.getState();
         const payload = handleCreateSQLServer(state, dispatch);
         const selectedCredId = state.mssqlForm.awsAccount.selectedCredential?.data?.credentialsId;
         const selectedRegionCode = state.mssqlForm.regionAndVpc.selectedRegion?.data?.regionCode;
