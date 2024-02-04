d496f39cb209a1b6760d2011cc8a009ab44cc0bf
rpanwar@netapp.com <rpanwar@netapp.com>
2024-02-04 19:28:21 +0530

    DBS-1844 Manage columns feature added in managed hosts table

--- ui/src/utils/utilityFunctions.ts
+++ ui/src/utils/utilityFunctions.ts
@@ -1164,3 +1164,41 @@ export const getCurrentDateTime = () => {
 
     return `${formattedDate}, ${formattedTime}`;
 };
+
+export const initialColStateManagedHosts = {
+    1: {
+        isHidden: false,
+        isRemovalDisabled: true
+    },
+    2: {
+        isHidden: false
+    },
+    3: {
+        isHidden: false
+    },
+    4: {
+        isHidden: false
+    },
+    5: {
+        isHidden: false
+    },
+    6: {
+        isHidden: false
+    },
+    7: {
+        isHidden: false
+    },
+    8: {
+        isHidden: true
+    },
+    9: {
+        isHidden: true
+    },
+
+    10: {
+        isHidden: true
+    },
+    11: {
+        isHidden: true
+    }
+};
