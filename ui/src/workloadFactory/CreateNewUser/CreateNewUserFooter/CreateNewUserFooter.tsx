44ccb25f5cd6dbadbd3f51da12014530b07cfdbe
rpanwar@netapp.com <rpanwar@netapp.com>
2024-02-14 12:42:02 +0530

    DBS-2031 close button function added

--- ui/src/workloadFactory/CreateNewUser/CreateNewUserFooter/CreateNewUserFooter.tsx
+++ ui/src/workloadFactory/CreateNewUser/CreateNewUserFooter/CreateNewUserFooter.tsx
@@ -1,12 +1,17 @@
 import { Button } from '@netapp/design-system';
+import { useNavigate } from 'react-router-dom';
 
 const CreateNewUserFooter = () => {
+    const navigate = useNavigate();
+    const closeHandler = () => {
+        navigate('../databases');
+    };
     return (
         <>
             <Button isThin onClick={() => {}}>
                 {'Create'}
             </Button>
-            <Button isThin variant="secondary" onClick={() => {}}>
+            <Button isThin variant="secondary" onClick={closeHandler}>
                 {'Close'}
             </Button>
         </>
--- ui/src/workloadFactory/CreateNewUser/CreateNewUserHeader/CreateNewUserHeader.tsx
+++ ui/src/workloadFactory/CreateNewUser/CreateNewUserHeader/CreateNewUserHeader.tsx
@@ -1,16 +1,27 @@
-import { Header } from '@netapp/design-system';
+import { DsTypography, Header } from '@netapp/design-system';
 
 import styles from './CreateNewUserHeader.module.scss';
+import { useNavigate } from 'react-router-dom';
 const CreateNewUserHeader = () => {
+    const navigate = useNavigate();
     return (
         <div className={styles.createNewUserHeader}>
             <Header
                 closeButtonProps={{
-                    onClick: () => {}
+                    onClick: () => {
+                        navigate('../databases');
+                    }
                 }}
                 title={'Create new user database'}
                 style={{ width: '100vw' }}
-            />
+            >
+                <div className={styles.leftSideStyle}>
+                    <div className={styles.separator} />
+                    <DsTypography variant="Semibold_14" className={styles.hostName}>
+                        Host: host name
+                    </DsTypography>
+                </div>
+            </Header>
         </div>
     );
 };
