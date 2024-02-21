2b2d73a11aedf1e40e12e20a8c14ae062b04a05b
rpanwar@netapp.com <rpanwar@netapp.com>
2024-02-21 09:23:56 +0530

    DBS-2092 column state persist in inventory

--- ui/src/store/workloadFactory/inventorySlice.ts
+++ ui/src/store/workloadFactory/inventorySlice.ts
@@ -1,5 +1,6 @@
 import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 import { WLF_TABS } from '../../utils/consts';
+import { initialColStateManagedHosts } from '../../utils/utilityFunctions';
 
 const initialInventoryState: any = {
     selectedInventoryTab: WLF_TABS.MANAGED_HOSTS,
@@ -8,13 +9,17 @@ const initialInventoryState: any = {
     detectManagePassword: '',
     detectOntapUsername: '',
     detectOntapPassword: '',
-    detectHostRadio: 'Yes, Manage host via workload factory'
+    detectHostRadio: 'Yes, Manage host via workload factory',
+    managedHostInitialColumns: initialColStateManagedHosts
 };
 
 const inventorySlice = createSlice({
     name: 'inventory',
     initialState: initialInventoryState,
     reducers: {
+        setManagedHostColState: (state, action: PayloadAction<any>) => {
+            state.managedHostInitialColumns = action.payload;
+        },
         setSelectedInventoryTab: (state, action: PayloadAction<any>) => {
             state.selectedInventoryTab = action.payload;
         },
@@ -47,7 +52,8 @@ export const {
     setDetectManagePassword,
     setDetectONTAPUserName,
     setDetectONTAPPassword,
-    setRadioValueDetect
+    setRadioValueDetect,
+    setManagedHostColState
 } = inventorySlice.actions;
 
 export default inventorySlice;
