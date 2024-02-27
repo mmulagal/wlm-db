73803f637f1b883c5917ec21797dd05343b63a75
krithib <krithib@netapp.com>
2024-02-27 09:10:20 +0530

    DBS-1992 : missing and blocked permissions categories

--- server/src/operations/aws/iam-operations.ts
+++ server/src/operations/aws/iam-operations.ts
@@ -2,6 +2,7 @@ import { ContextEntry, SimulatePrincipalPolicyCommandInput } from '@aws-sdk/clie
 import { getRoleDetails } from '../cloud-manager/credentials-operations';
 import getLogger from '../../utils/logger';
 import simulatePrincipalPolicy from '../../lib/aws/iam';
+import { isEmpty } from 'lodash-es';
 
 const logger = getLogger();
 
@@ -33,10 +34,10 @@ export default async function getMissingPermissionsList(
     const missingPermissions =
         results
             ?.filter(
-                ({ EvalDecision, OrganizationsDecisionDetail, PermissionsBoundaryDecisionDetail }) =>
+                ({ EvalDecision, OrganizationsDecisionDetail, MatchedStatements }) =>
                     EvalDecision !== 'allowed' &&
                     OrganizationsDecisionDetail?.AllowedByOrganizations &&
-                    PermissionsBoundaryDecisionDetail?.AllowedByPermissionsBoundary
+                    isEmpty(MatchedStatements)
             )
             .map(({ EvalActionName }) => EvalActionName as string) || [];
     const blockedByOrganisation =
@@ -49,10 +50,11 @@ export default async function getMissingPermissionsList(
     const blockedByPermissionBoundary =
         results
             ?.filter(
-                ({ EvalDecision, OrganizationsDecisionDetail, PermissionsBoundaryDecisionDetail }) =>
+                ({ EvalDecision, OrganizationsDecisionDetail, PermissionsBoundaryDecisionDetail, MatchedStatements }) =>
                     EvalDecision !== 'allowed' &&
                     OrganizationsDecisionDetail?.AllowedByOrganizations &&
-                    !PermissionsBoundaryDecisionDetail?.AllowedByPermissionsBoundary
+                    !PermissionsBoundaryDecisionDetail?.AllowedByPermissionsBoundary &&
+                    !isEmpty(MatchedStatements)
             )
             .map(({ EvalActionName }) => EvalActionName as string) || [];
 
