bac98d813d1eac0257bd3d1a430359c405a3a75a
R Ranganatha <rranga@netapp.com>
2024-03-02 02:04:20 +0530

    1. Kept instance data processing in a try/catch block to limit failure to individual SQL instances. 2. Considered SQL Express Edition in PowerShell script, and improved the same to handle no availability of drive letters.

--- server/src/operations/workloads/mssql/discover-consts.ts
+++ server/src/operations/workloads/mssql/discover-consts.ts
@@ -21,6 +21,13 @@
         "sqlServerEdition":  2022,
         "windowsAuthentication":  true
     }
+
+  Possible causes for unavailability of SQL Server details:
+  - Insufficient permissions on sys.master_files view.
+    Because of this, we won't be able to get the database file locations, due
+    to which the script won't  get/return SerialNumberOrScsiTargets.  As a
+    result, the storageType can't be determined while processing script output,
+    which causes the API to return an empty  response for storageType.
  */
 const hostAndSqlInfoPowerShellScript = [
     `
@@ -32,31 +39,34 @@ const hostAndSqlInfoPowerShellScript = [
         $state = $_.State
         $path = $_.PathName  -Replace "-s.*",""
         $body['windowsAuthentication'] = $False
+        $sqlDriveInfo = $Null
 
         try {
           if ($state -eq "Running") {
             Get-Command sqlcmd > Out-Null
-            $serverInstance = If ($instance -ne "MSSQLSERVER") { "$Env:ComputerName\\$instance" } Else { "$Env:ComputerName" }
+            $serverInstance = If ($instance -ne "MSSQLSERVER" -And -$instance -ne "SQLEXPRESS") { "$Env:ComputerName\\$instance" } Else { "$Env:ComputerName" }
             sqlcmd -Q "SELECT @@serviceName" -C -S $serverInstance -l 1 2> Out-Null | Out-Null
             $body['windowsAuthentication'] = $?
 
             $sqlDrives = sqlcmd -Q " SET NOCOUNT ON; SELECT DISTINCT LEFT(physical_name, 1) AS DriveLetter FROM sys.master_files " -h -1 -C -W -S $serverInstance | ConvertTo-Json
-            $sqlDriveInfo = Get-PhysicalDisk | ForEach-Object {
-              $a = $_
-              Get-Partition | ForEach-Object {
-                $b = $_
-                if (($b.DriveLetter -ne $null) -and ($a.DeviceId -eq $b.diskNumber) -and ($sqlDrives.contains($b.DriveLetter))) {
-                  if ($a.BusType -eq "NVMe") {
-                    New-Object -TypeName PSObject -Property @{ SerialNumberOrScsiTarget = $a.serialnumber }
-                  } elseif ($a.BusType -eq "iSCSI") {
-                    $scsiTarget = (get-disk | Where { $_.BusType -eq  'iSCSI' } | Get-IscsiConnection).TargetAddress | Select -Unique
-                    $scsiTarget | ForEach-Object {
-                      New-Object -TypeName PSObject -Property @{ SerialNumberOrScsiTarget = $_ }
+            if ($sqlDrives) {
+              $sqlDriveInfo = Get-PhysicalDisk | ForEach-Object {
+                $a = $_
+                Get-Partition | ForEach-Object {
+                  $b = $_
+                  if (($b.DriveLetter -ne $null) -and ($a.DeviceId -eq $b.diskNumber) -and ($sqlDrives.contains($b.DriveLetter))) {
+                    if ($a.BusType -eq "NVMe") {
+                      New-Object -TypeName PSObject -Property @{ SerialNumberOrScsiTarget = $a.serialnumber }
+                    } elseif ($a.BusType -eq "iSCSI") {
+                      $scsiTarget = (get-disk | Where { $_.BusType -eq  'iSCSI' } | Get-IscsiConnection).TargetAddress | Select -Unique
+                      $scsiTarget | ForEach-Object {
+                        New-Object -TypeName PSObject -Property @{ SerialNumberOrScsiTarget = $_ }
+                      }
                     }
                   }
                 }
-              }
-            } | ConvertTo-Json
+              } | ConvertTo-Json
+            }
           }
         } catch {
           # Prevent any possible errors from clobbering JSON output
