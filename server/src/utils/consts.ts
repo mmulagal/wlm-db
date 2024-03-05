93ac18ede0387dcedfac60c95704cf8313e4901b
krithib <krithib@netapp.com>
2024-03-05 11:14:13 +0530

    DBS-2215: Some of the task names of sub job "Creating VPC endpoints for S3 CloudFormation, SQS, SSM, CloudWatch services" is coming empty

--- server/src/utils/consts.ts
+++ server/src/utils/consts.ts
@@ -1064,7 +1064,9 @@ const subJobDescriptions: SubJobDescriptions = {
     'Ec2MessagesEndpoint(AWS::EC2::VPCEndpoint)': 'Creating EC2Messages endpoint',
     'SqsEndpoint(AWS::EC2::VPCEndpoint)': 'Creating SQS endpoint',
     'SsmEndpoint(AWS::EC2::VPCEndpoint)': 'Creating SSM endpoint',
-    'SsmMessagesEndpoint(AWS::EC2::VPCEndpoint)': 'Creating SSMMessages endpoint'
+    'SsmMessagesEndpoint(AWS::EC2::VPCEndpoint)': 'Creating SSMMessages endpoint',
+    'FsxEndpoint(AWS::EC2::VPCEndpoint)': 'Creating FSxN endpoint',
+    'CloudwatchLogsEndpoint(AWS::EC2::VPCEndpoint)': 'Creating CloudWatch logs endpoint'
 };
 const CF_STACK_RESOURCE_TYPE = 'AWS::CloudFormation::Stack';
 const RESOURCE_SOURCE = {
@@ -1072,7 +1074,17 @@ const RESOURCE_SOURCE = {
     DISCOVER: 'discovery'
 };
 
-const ENDPOINTS_DEPLOYMENT = ['s3', 'cloudformation', 'sqs', 'ssm', 'ssmmessages', 'ec2messages', 'monitoring', 'logs', 'fsx'];
+const ENDPOINTS_DEPLOYMENT = [
+    's3',
+    'cloudformation',
+    'sqs',
+    'ssm',
+    'ssmmessages',
+    'ec2messages',
+    'monitoring',
+    'logs',
+    'fsx'
+];
 
 const SSM_PARAMETERS_BASE_PATH = '/netapp/wlmdb';
 const COMPLETE = 'Complete';
