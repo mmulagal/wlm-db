3332307f2c6da3be610e5fe36b8f2e2f1081005c
nithinr <nithinr@netapp.com>
2024-03-05 22:09:29 +0530

    IE updates and Potential fix for DBS-2196

--- server/resources/mssql/scripts/dbcreate/Configure-LUNs.ps1
+++ server/resources/mssql/scripts/dbcreate/Configure-LUNs.ps1
@@ -108,7 +108,7 @@ function callGetApi{
         }
     }catch{
         $result.Add('Status','Failed')
-        $result.Add('Message','REST API call to FSx for ONTAP failed')
+        $result.Add('Message','REST API call to FSx for NetApp ONTAP failed')
         $result.Add('Exception',$_)
         $resultjson = ($result | ConvertTo-Json) 
         $resultjson  
@@ -150,7 +150,7 @@ function callrestapi{
         
     }catch{
         $result.Add('Status','Failed')
-        $result.Add('Message','REST API call to FSx for ONTAP failed')
+        $result.Add('Message','REST API call to FSx for NetApp ONTAP failed')
         $result.Add('Exception',$_)
         $resultjson = ($result | ConvertTo-Json) 
         $resultjson  
@@ -165,7 +165,7 @@ try {
 if(($LogNew -eq "false") -And ($DataNew -eq "false")) { throw }
 } catch {
     $result.Add('Status','Failed')
-    $result.Add('Message','Need to define at least one new drive to configure volume and LUN')
+    $result.Add('Message','Need to define at least one new drive to configure storage')
     $result.Add('Exception',$_)
     $resultjson = ($result | ConvertTo-Json) 
     $resultjson  
@@ -185,7 +185,7 @@ $igroups = (callGetApi -uri $URI -region $region -creds $base64 -result $result)
 $IGROUP = $igroups[0].name
 } catch {
     $result.Add('Status','Failed')
-    $result.Add('Message','Unable to fetch igroup from SVM')
+    $result.Add('Message','Unable to fetch initiator group from SVM')
     $result.Add('Exception',$_)
     $resultjson = ($result | ConvertTo-Json) 
     $resultjson     
@@ -200,7 +200,7 @@ if ([string]::IsNullOrEmpty($IGROUP)) {
   else { throw}
   } catch {
     $result.Add('Status','Failed')
-    $result.Add('Message','Unable to find initiator group allowing the node IQN')
+    $result.Add('Message','Unable to find initiator group allowing access to the node')
     $result.Add('Exception',$_)
     $resultjson = ($result | ConvertTo-Json) 
     $resultjson  
@@ -216,7 +216,7 @@ if ([string]::IsNullOrEmpty($IGROUP)) {
   if ([string]::IsNullOrEmpty($IGROUP)) { throw }
   } catch {
     $result.Add('Status','Failed')
-    $result.Add('Message','Unable to find initiator group allowing the node IQN')
+    $result.Add('Message','Unable to find initiator group allowing access to the node')
     $result.Add('Exception',$_)
     $resultjson = ($result | ConvertTo-Json) 
     $resultjson  
@@ -264,7 +264,7 @@ $logvolcreate = (callrestapi -MgmtDNS $MgmtDNS -uri $volUriDynamicPart -region $
 }
 } catch {
     $result.Add('Status','Failed')
-    $result.Add('Message','Failed to create volume on FSx for ONTAP')
+    $result.Add('Message','Failed to create volume on FSx for NetApp ONTAP')
     $result.Add('Exception',$_)
     $resultjson = ($result | ConvertTo-Json) 
     $resultjson  
@@ -326,7 +326,7 @@ try{
         }
 }catch{
     $result.Add('Status','Failed')
-    $result.Add('Message','Volume modification to set best practise parameters failed')
+    $result.Add('Message','Failed to set best practise parameters on the volume')
     $result.Add('Exception',$_)
     $resultjson = ($result | ConvertTo-Json) 
     $resultjson  
@@ -422,7 +422,7 @@ foreach ($perlun in $pathlist) {
         }
         catch{
             $result.Add('Status','Failed')
-            $result.Add('Message','LUN modification to set space-reserve failed')
+            $result.Add('Message','Failed to set best practise parameters on the storage')
             $result.Add('Exception',$_)
             $resultjson = ($result | ConvertTo-Json) 
             $resultjson  
@@ -449,7 +449,7 @@ foreach ($perlun in $pathlist) {
         }
         catch{
             $result.Add('Status','Failed')
-            $result.Add('Message','LUN modification to set space-reserve failed')
+            $result.Add('Message','Failed to set best practise parameters on the storage')
             $result.Add('Exception',$_)
             $resultjson = ($result | ConvertTo-Json) 
             $resultjson  
@@ -458,7 +458,7 @@ foreach ($perlun in $pathlist) {
         Start-Sleep 3
     }
     $result.Add('Status','Complete')
-    $result.Add('Message','Provisioning volumes and LUNs complete')
+    $result.Add('Message','Provisioning storage on FSx for NetApp ONTAP complete')
     $resultjson = ($result | ConvertTo-Json) 
     $resultjson 
  
