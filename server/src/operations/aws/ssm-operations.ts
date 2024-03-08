0f53c201a1d265093a58c6a23278865c315dfab1
mshreyas <mshreyas@netapp.com>
2024-03-08 17:28:31 +0530

    DBS-2258 For timed out and cancelled SSM command status, throwing appropriate error

--- server/src/operations/aws/ssm-operations.ts
+++ server/src/operations/aws/ssm-operations.ts
@@ -16,7 +16,7 @@ import {
     putParameter
 } from '../../lib/aws/ssm';
 import { sleep } from '../../utils/utils';
-import { AWS_REGIONS } from '../../utils/consts';
+import { AWS_REGIONS, HttpErrorCodes } from '../../utils/consts';
 import getLogger from '../../utils/logger';
 import { FSxAvailableRegionType } from '../../routes/types/aws.types';
 import { SSMParamterObject } from '../../utils/common-types';
@@ -40,9 +40,13 @@ async function pollCommandStatus(
         switch (status) {
             case CommandInvocationStatus.SUCCESS:
                 return response;
-            case CommandInvocationStatus.FAILED:
+
             case CommandInvocationStatus.TIMED_OUT:
             case CommandInvocationStatus.CANCELLED:
+                const errorMessage = `SSM execution ${status} for command ${pollParams.CommandId} on instance ${pollParams.InstanceId}`;
+                logger.error(errorMessage);
+                throw createError(HttpErrorCodes.INTERNAL_SERVER_ERROR, errorMessage);
+            case CommandInvocationStatus.FAILED:
                 logger.error(
                     `SSM execution ${status} for command ${pollParams.CommandId} on instance ${pollParams.InstanceId}`
                 );
@@ -141,3 +145,6 @@ async function ssmPutParameters(credentialsId: string, region: string, credentia
 }
 
 export { executeSSMDocument, getFSxOntapRegionsList, getSSMConnectionStatus, ssmPutParameters, pollCommandStatus };
+function createError(INTERNAL_SERVER_ERROR: any, errorMessage: string) {
+    throw new Error('Function not implemented.');
+}
