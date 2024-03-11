fcd457d128aef5779e095db1a830fe6d4060b7ba
nithinr <nithinr@netapp.com>
2024-03-11 10:35:52 +0530

    DBS-2265:Removing Update-ISCSITarget that could hang in some cases

--- server/resources/mssql/scripts/dbcreate/NewDB_Initialize-Iscsidisk.ps1
+++ server/resources/mssql/scripts/dbcreate/NewDB_Initialize-Iscsidisk.ps1
@@ -25,7 +25,8 @@ $ErrorActionPreference = "Stop"
 $result = [ordered]@{}
 
 #Refresh the cached information on iSCSI target
-Update-IscsiTarget
+#This command hangs in few cases for any issue with iSCSI. Even passing -AsJob won't help as multiple jobs would be stuck. 
+#Update-IscsiTarget
 
 echo "RESCAN" | Out-File -FilePath C:\SSM\rescan.txt
 $rescan =(diskpart /s C:\SSM\rescan.txt)
