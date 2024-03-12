138e7a9adb4f33894ab0b9e84c6094570753166f
R Ranganatha <rranga@netapp.com>
2024-03-13 02:34:09 +0530

    Added names of EC2 nodes for SQL Server instance.

--- server/src/operations/discover-operations.ts
+++ server/src/operations/discover-operations.ts
@@ -325,9 +325,10 @@ async function getHostAndSqlInfoFromPsOutput(
                         }ms`
                     );
 
-                    const {
+                    let {
                         sqlServerVersion,
                         sqlServerName,
+                        sqlServerNodes,
                         sqlServerInstance,
                         sqlServerState,
                         windowsAuthentication,
@@ -337,9 +338,14 @@ async function getHostAndSqlInfoFromPsOutput(
                         `API1Performance: Time taken to execute PowerShell script for instance ${sqlServerInstance}: ${scriptExecutionTime}ms`
                     );
 
+                    if (!Array.isArray(sqlServerNodes)) {
+                        sqlServerNodes = [sqlServerNodes];
+                    }
+
                     ssmTargetSqlServerInstancesInfo.push({
                         sqlServerVersion,
                         ...(sqlServerName && { sqlServerName }),
+                        sqlServerNodes,
                         sqlServerInstance,
                         sqlServerState,
                         sqlServerEdition,
