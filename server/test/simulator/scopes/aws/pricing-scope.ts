2743b53ea1b0f7bd7943149d1d23eafd23526d4d
srigowri <srigowri@netapp.com>
2024-03-14 19:08:32 -0700

    DBS-1967 - API2: Discovery of comprehensive EC2/storage details- TC fix

--- server/test/simulator/scopes/aws/pricing-scope.ts
+++ server/test/simulator/scopes/aws/pricing-scope.ts
@@ -393,22 +393,11 @@ const EC2INSTANCERATEFILTER: GetProductsCommandInput = {
 //     FormatVersion: 'aws_v1'
 // };
 
-const price: LazyJsonString = LazyJsonString.fromObject(
-    '{"product":{"productFamily":"Compute Instance","attributes":{"servicecode":"AmazonEC2","instanceType":"t3.micro","location":"US East (N. Virginia)","memory":"1 GiB","vcpu":"2"}},"terms":{"OnDemand":{"us-east-1":{"priceDimensions":{"us-east-1-ondemand":{"pricePerUnit":{"USD":"0.00158"}}}}}}}'
-);
+// const price: LazyJsonString = LazyJsonString.fromObject(
+//     '{"product":{"productFamily":"Compute Instance","attributes":{"servicecode":"AmazonEC2","instanceType":"t3.micro","location":"US East (N. Virginia)","memory":"1 GiB","vcpu":"2"}},"terms":{"OnDemand":{"us-east-1":{"priceDimensions":{"us-east-1-ondemand":{"pricePerUnit":{"USD":"0.00158"}}}}}}}'
+// );
 
-const mockGetProductsResponse = {
-    $metadata: {
-        httpStatusCode: 200,
-        requestId: 'c8501a83-530c-47ce-9ebd-3a8b9674f759',
-        extendedRequestId: undefined,
-        cfId: undefined,
-        attempts: 1,
-        totalRetryDelay: 0
-    },
-    FormatVersion: 'aws_v1',
-    PriceList: [price]
-};
+const mockGetProductsResponse = mockfsxStoragePriceGetProductsResponse;
 const VPCFILTER: GetProductsCommandInput = {
     Filters: [
         {
