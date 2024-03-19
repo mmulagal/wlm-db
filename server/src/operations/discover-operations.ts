f3cbb11a5a80d6c4b23fe48ee820a7f32525b8d0
srigowri <srigowri@netapp.com>
2024-03-19 09:41:49 -0700

    DBS-1967 - API2: Discovery of comprehensive EC2/storage details - dbcount added

--- server/src/operations/discover-operations.ts
+++ server/src/operations/discover-operations.ts
@@ -518,7 +518,7 @@ async function fetchUnmanagedHostsInformation(
             getDatabaseHostSummary(
                 accountId,
                 resourceDetail.resource_id,
-                'performance,usageEstimation,resourceUtilization',
+                'dbCount,performance,usageEstimation,resourceUtilization',
                 resourceDetail,
                 false // unmanaged host
             )
