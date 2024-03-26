495e913c59c5d843041166ff2d0136ca58b50059
rpanwar@netapp.com <rpanwar@netapp.com>
2024-03-26 16:20:37 +0530

    DBS-2393 Sandbox distribution by age complete with 1440

--- ui/src/workloadFactory/Sandbox/CreateNewSandbox/CreateNewSandboxContent/CreateNewSandboxContent.tsx
+++ ui/src/workloadFactory/Sandbox/CreateNewSandbox/CreateNewSandboxContent/CreateNewSandboxContent.tsx
@@ -1,7 +1,14 @@
+import { DsTypography } from '@netapp/design-system';
 import styles from './CreateNewSandboxContent.module.scss';
 
 const CreateNewSandboxContent = () => {
-    return <div>Content here</div>;
+    return (
+        <div className={styles.createNewSandboxContent}>
+            <DsTypography variant="Semibold_16" className={styles.heading}>
+                Create Sandbox
+            </DsTypography>
+        </div>
+    );
 };
 
 export default CreateNewSandboxContent;
