803473cb88f5d9b3cd422973ad2c4f9f2cf42c44
Sonam Sonam <sonamy@netapp.com>
2024-03-29 17:50:19 +0530

    DBS-2458 filter change

--- ui/src/utils/utilityFunctions.ts
+++ ui/src/utils/utilityFunctions.ts
@@ -393,6 +393,21 @@ export const formatHostData = (val: any) => {
     }
     const serverInstallationMode = val?.topology?.serverInstallationMode || type;
 
+    // fileSystemType
+    const typeList: string[] = [];
+    val?.sqlServerInstances?.[0]?.storage?.map((storageObj: any) => {
+        if (storageObj.type === DETECT_HOST_VAR.FSXN && !typeList.includes(GENERAL.FSX_FOR_ONTAP)) {
+            typeList.push(GENERAL.FSX_FOR_ONTAP);
+        }
+        if (storageObj.type === DETECT_HOST_VAR.EBS && !typeList.includes(GENERAL.EBS)) {
+            typeList.push(GENERAL.EBS);
+        }
+        if (storageObj.type === DETECT_HOST_VAR.FSXW && !typeList.includes(GENERAL.FSX_FOR_WINDOWS)) {
+            typeList.push(GENERAL.FSX_FOR_WINDOWS);
+        }
+    });
+    const fileSystemType = typeList.join(', ') || val?.topology?.fileSystemType;
+
     val = {
         ...val,
         type: DB_HOME_DATA_TYPE.HOSTS,
@@ -415,7 +430,8 @@ export const formatHostData = (val: any) => {
         instanceNames: instanceNames.join(',') || val?.ec2InstanceName,
         vpcNames: val?.topology?.vpcName || val?.vpc?.name,
         azType: azType,
-        serverInstallationMode: serverInstallationMode
+        serverInstallationMode: serverInstallationMode,
+        fileSystemType: fileSystemType
     };
     return val;
 };
