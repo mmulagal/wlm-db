0916b7f983464763d7810bbd09c3c17c097101fb
rpanwar@netapp.com <rpanwar@netapp.com>
2024-03-29 10:54:34 +0530

    DBS-2425 Header color and icon change for dark and light mode

--- ui/src/components/CreateMsSql/CreateMsSqlLayout/DeploymentTabs/DeploymentTabs.tsx
+++ ui/src/components/CreateMsSql/CreateMsSqlLayout/DeploymentTabs/DeploymentTabs.tsx
@@ -1,8 +1,13 @@
 import { ReactComponent as WizardIcon } from '../../../../assets/wizard-icon.svg';
+import { ReactComponent as WizardSelectedInLightIcon } from '../../../../assets/Wizard_Selected_light.svg';
 import { ReactComponent as ChatbotIcon } from '../../../../assets/chatbot-tab-icon.svg';
+import { ReactComponent as ChatbotLightModeIcon } from '../../../../assets/ChatInLightModeNotSelected.svg';
+import { ReactComponent as ChatbotDarkModeSelectedIcon } from '../../../../assets/ChatInDarkSelected.svg';
+import { ReactComponent as ChatbotDarkModeNotSelectedIcon } from '../../../../assets/ChatInDarkNotSelected.svg';
 import { Typography } from '@netapp/design-system';
 
 import styles from './DeploymentTabs.module.scss';
+import { useAppSelector } from '../../../../store/storeHooks';
 
 type DeploymentTabsProps = {
     selectedTab: 'wizard' | 'chatbot';
@@ -10,25 +15,82 @@ type DeploymentTabsProps = {
 };
 
 const DeploymentTabs = ({ selectedTab, onTabChange }: DeploymentTabsProps) => {
+    const isDarkTheme = useAppSelector(state => state?.auth?.features?.active['Platform.BlueXP/DarkTheme']);
+    const darkModeClassSet = () => {
+        if (isDarkTheme) {
+            if (selectedTab === 'wizard') {
+                return styles.darkModeWizardSelected;
+            } else if (selectedTab === 'chatbot') {
+                return styles.darkModeChatbotSelected;
+            } else {
+                return '';
+            }
+        }
+        return '';
+    };
+
+    const wizardIcon = () => {
+        if (selectedTab === 'wizard') {
+            return (
+                <div>
+                    <WizardSelectedInLightIcon />
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <WizardIcon />
+                </div>
+            );
+        }
+    };
+
+    const setChatbotIcon = () => {
+        if (true) {
+            if (selectedTab === 'wizard') {
+                return <ChatbotDarkModeNotSelectedIcon />;
+            } else {
+                return <ChatbotDarkModeSelectedIcon />;
+            }
+        } else {
+            if (selectedTab === 'chatbot') {
+                return <ChatbotIcon />;
+            } else {
+                return <ChatbotLightModeIcon />;
+            }
+        }
+    };
+
     return (
         <div className={styles['deployment-tabs']}>
             <div
                 className={`${styles['single-tab']} ${
                     selectedTab === 'wizard' ? styles['selected-tab'] : styles['non-selected-tab']
-                }`}
+                } 
+                ${selectedTab === 'wizard' && isDarkTheme && styles['selected-tab-dark-theme']}
+                `}
                 onClick={() => onTabChange('wizard')}
             >
-                <WizardIcon />
+                {isDarkTheme && (
+                    <div className={darkModeClassSet()}>
+                        <WizardIcon />
+                    </div>
+                )}
+
+                {!isDarkTheme && wizardIcon()}
+
                 <Typography variant="Semibold_16">Database wizard</Typography>
             </div>
             <div
                 className={`${styles['single-tab']} ${
                     selectedTab === 'chatbot' ? styles['selected-tab'] : styles['non-selected-tab']
-                }`}
+                }
+                ${selectedTab === 'chatbot' && isDarkTheme && styles['selected-tab-dark-theme']}
+                `}
                 onClick={() => onTabChange('chatbot')}
                 id="chatbot-tab"
             >
-                <ChatbotIcon />
+                <div>{setChatbotIcon()}</div>
                 <Typography variant="Semibold_16">Database chatbot</Typography>
             </div>
         </div>
