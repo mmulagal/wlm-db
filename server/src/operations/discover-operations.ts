7d0436ab7315b265691d55e7b20d7b3ec9cec388
U, Srigowri <Srigowri.U@netapp.com>
2024-04-03 15:56:16 -0700

    DBS-2356 - Backend validation for instance-storage maping - refactor and comments update

--- server/src/operations/discover-operations.ts
+++ server/src/operations/discover-operations.ts
@@ -641,41 +641,38 @@ async function fetchUnmanagedHostsInformation(
 ) {
     logger.info('Fetching hosts information:', { accountId, credentialsId, region, instances });
 
-    // Modified implementation to fetch SQL Server instance details for EC2 instances with underlying storage details. The code now accepts instances ID array instead of object with ec2InstanceId and storageType. If there are multiple sql instances in an ec2 instance each using a different storage type, the code will return multiple resource details for the same ec2 instance. Every item in resourceDetailsList is an ec2 instance - sql instance pair with storage type. The code will return multiple resource details for the same ec2 instance if there are multiple sql instances in the ec2 instance each using a different storage type.
+    // Modified implementation to fetch SQL Server instance details for EC2 instances with underlying storage details. The code now accepts instances ID array instead of object with ec2InstanceId and storageType. If there are multiple sql instances in an ec2 instance, the code will return multiple resource details for the same ec2 instance. Every item in resourceDetailsList is an ec2 instance - sql instance pair with storage type.
 
-    const { items: sqlServerInstanceDetails } = await getHostAndSqlServerInfo(
+    const { items: ec2HostDetails } = await getHostAndSqlServerInfo(
         accountId,
         credentialsId,
         region,
-        instances.length,
+        instances.length >= 5 ? instances.length : 5,
         undefined,
         instances
     );
     const resourceDetailsList: ResourceDetails[] = [];
 
-    sqlServerInstanceDetails?.forEach(sqlServerInstance => {
-        const storageDetails = sqlServerInstance?.sqlServerInstances?.map(sqlServer => sqlServer.storage);
-        const storageTypeWithId: { type: string; id: string }[] = [];
-        storageDetails?.forEach(storageDetail =>
-            storageDetail?.forEach(elem =>
-                storageTypeWithId.push({
-                    type: elem.type,
-                    id: elem.id
-                })
-            )
-        );
-
-        storageTypeWithId?.forEach(storage => {
-            const ebsVolumeId = storage.type === STORAGE_TYPE.EBS ? storage.id : undefined;
-            const fsxwId = storage.type === STORAGE_TYPE.FSXW ? storage.id : undefined;
-            const fsxnId = storage.type === STORAGE_TYPE.FSXN ? storage.id : undefined;
+    ec2HostDetails?.forEach(ec2Instance => {
+        ec2Instance?.sqlServerInstances?.forEach(sqlInstance => {
+            const { storage } = sqlInstance;
+            let ebsVolumeId;
+            let fsxwId;
+            let fsxnId;
+            storage?.forEach(({ type, id }) => {
+                // if there are multiple entries in storage for the same type then only the last entry will be considered. For eg: if the same sql instance has fsxn-1 and fsxn-2, then only fsxn-2 will be considered. Such a scenario occurs when system dbs use one storage and user dbs use another storage. The reason for this limitation currently is wlmdb resources are not expecting multiple co-relation ids for the same resource.
+                // If the storage is of different type, then both will be considered while calculating protection and storage savings details.
+                ebsVolumeId = type === STORAGE_TYPE.EBS ? id : undefined;
+                fsxwId = type === STORAGE_TYPE.FSXW ? id : undefined;
+                fsxnId = type === STORAGE_TYPE.FSXN ? id : undefined;
+            });
 
             resourceDetailsList.push({
                 id: null,
                 account_id: accountId,
-                resource_id: sqlServerInstance.ec2InstanceId,
+                resource_id: ec2Instance.ec2InstanceId,
                 resource_type: RESOURCESTYPE.MSSQL,
-                resource_name: sqlServerInstance.ec2InstanceId,
+                resource_name: ec2Instance.ec2InstanceId,
                 cloud_provider_name: CloudProviders.AWS,
                 co_relation_id: fsxnId || null,
                 cloud_provider_account_id: null,
@@ -684,7 +681,7 @@ async function fetchUnmanagedHostsInformation(
                 storage_type: STORAGE_TYPE.FSXN,
                 metadata: {
                     creationDate: Date.now(),
-                    node1InstanceId: sqlServerInstance.ec2InstanceId
+                    node1InstanceId: ec2Instance.ec2InstanceId
                 },
                 ebsVolumeId,
                 fsxwId
