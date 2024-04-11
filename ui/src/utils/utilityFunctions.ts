963942255dcefdbacc384a86f46a1f6aed59ab43
Sonam Sonam <sonamy@netapp.com>
2024-04-11 15:03:44 +0530

    bug fix

--- ui/src/utils/utilityFunctions.ts
+++ ui/src/utils/utilityFunctions.ts
@@ -455,7 +455,7 @@ export const formatHostData = (val: any) => {
         performanceText: val?.performance && val.performance?.assessment,
         // Storage saving table text to search in table
         storageSavingsText: storageSavingsText,
-        sizeformat: formatSizeOnePrecision(totalSize),
+        sizeformat: val?.storage ? formatSizeOnePrecision(totalSize) : '',
         instanceNames: instanceNames.join(',') || val?.ec2InstanceName,
         vpcNames: val?.topology?.vpcName || val?.vpc?.name,
         azType: azType,
