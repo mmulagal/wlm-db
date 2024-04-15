4fc6a1d86fc391cf56c54e740ed684a96223e22a
rpanwar@netapp.com <rpanwar@netapp.com>
2024-04-15 13:27:00 +0530

    DBS-2581 Data layout inside accordion - commit 1

--- ui/src/workloadFactory/ExploreSavings/ViewCalculations/OntapCalculation/OntapCalculation.tsx
+++ ui/src/workloadFactory/ExploreSavings/ViewCalculations/OntapCalculation/OntapCalculation.tsx
@@ -1,8 +1,72 @@
 import { AccordionCard, AccordionCardContent, DsTypography } from '@netapp/design-system';
 import styles from './OntapCalculation.module.scss';
 import CommonStyles from '../../../../utils/CommonStyles.module.scss';
+import { viewCalculation } from '../../SavingsCalculator/savingsUtil';
+import { Grid, GridItem } from '../../../../ui-components/Layout/Grid';
+import { Text } from '../../../../ui-components/Typography';
+
+const TableLayout = ({ data }: any) => {
+    const styleHandler = (data: any) => {
+        if (
+            data.label === 'Total monthly cost' ||
+            data.label === 'Total snapshot monthly cost' ||
+            data.label === 'Amazon Elastic Block Storage (EBS) total cost (monthly)'
+        ) {
+            return {
+                backgroundColor: 'var(--table-header-background)',
+                height: 88,
+                fontWeight: 490,
+                marginBottom: 3
+            };
+        } else if (data.label === 'Total storage charge (monthly)') {
+            return {
+                backgroundColor: 'var(--main-background)',
+                height: 88,
+                marginBottom: 3
+            };
+        } else {
+            return { backgroundColor: 'var(--main-background)', minHeight: 64, height: 64, marginBottom: 2 };
+        }
+    };
+    return (
+        <Grid className={styles['table-column']} style={styleHandler(data)}>
+            <GridItem lg="4">
+                <Text bold={!data.value && true} style={data.mainHeading ? { fontSize: 16 } : { fontSize: 14 }}>
+                    {data.label}
+                </Text>
+            </GridItem>
+            <GridItem lg="3">
+                <Text>{data.value}</Text>
+            </GridItem>
+            <GridItem lg="5">
+                <Text>{data.text}</Text>
+            </GridItem>
+        </Grid>
+    );
+};
 
 const OntapCalculation = () => {
+    const viewCalculationData = {
+        Ec2InstanceCalculation: {
+            instanceType: 'c5.2xlarge',
+            instanceHourlyPrice: '1.68',
+            ec2MachineCost: '1,226.40'
+        },
+        FSxNCalculation: {
+            storageCapacity: '23',
+            ebsCapacity: '1000',
+            volumes: '120',
+            ssdStorage: '60',
+            deduplication: '20',
+            priceCalculation: {
+                deduplication: '35',
+                storageCapacity: '25',
+                ssdStorage: '30',
+                minSSDStorage: '10',
+                ssdMonthlyCost: '50'
+            }
+        }
+    };
     const setHeader = () => {
         return <DsTypography variant="Regular_14">$XXX</DsTypography>;
     };
@@ -14,7 +78,20 @@ const OntapCalculation = () => {
                 title={<div>Microsoft SQL server on FSx for ONTAP calculation</div>}
             >
                 <AccordionCardContent>
-                    <DsTypography>Content</DsTypography>
+                    <DsTypography className={styles.accordionContentSet}>
+                        {viewCalculation(viewCalculationData).Ec2InstanceCalculation.map(
+                            (data: { label: string; text?: string; value?: string }, index: number) => (
+                                <TableLayout key={index} data={data} />
+                            )
+                        )}
+                        <div style={{ marginTop: '16px' }}>
+                            {viewCalculation(viewCalculationData).FSxNCalculation.map(
+                                (data: { label: string; text?: string; value?: string }, index: number) => (
+                                    <TableLayout key={index} data={data} />
+                                )
+                            )}
+                        </div>
+                    </DsTypography>
                 </AccordionCardContent>
             </AccordionCard>
         </div>
