575e7aace98b28afaa87a379ae0457046e053040
Ayush <payush@netapp.com>
2024-05-02 14:43:03 +0530

    DBS-2575: Create Sandbox job type filter added

--- ui/src/workloadFactory/JobMonitoring/JobMonitoringTable/JobMonitoringTable.tsx
+++ ui/src/workloadFactory/JobMonitoring/JobMonitoringTable/JobMonitoringTable.tsx
@@ -305,7 +305,8 @@ const JobMonitoringTable = () => {
             filterOptions: [
                 { value: JOB_MONITORING_TYPE.DEPLOYMENT, label: GENERAL.JM_TYPE_DEPLOYMENT },
                 { value: JOB_MONITORING_TYPE.CREATE_RESOURCE, label: GENERAL.JM_TYPE_CREATE_RESOURCE },
-                { value: JOB_MONITORING_TYPE.PREPARE_RESOURCE, label: GENERAL.JM_TYPE_PREPARE_RESOURCE }
+                { value: JOB_MONITORING_TYPE.PREPARE_RESOURCE, label: GENERAL.JM_TYPE_PREPARE_RESOURCE },
+                { value: JOB_MONITORING_TYPE.SANDBOX, label: GENERAL.JM_TYPE_SANDBOX }
             ],
             renderCell: (cellData: any) => {
                 return jobMonitoringTypeMapping(cellData);
