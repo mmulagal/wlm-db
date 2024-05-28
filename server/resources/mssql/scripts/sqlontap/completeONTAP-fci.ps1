44b4a55f5e02b56dae846159c8a371a847bc2fa6
krithib <krithib@netapp.com>
2024-05-28 11:14:24 +0530

    DBS-2688: Adding log lines to identify between custom and aws sql ami

--- server/resources/mssql/scripts/sqlontap/completeONTAP-fci.ps1
+++ server/resources/mssql/scripts/sqlontap/completeONTAP-fci.ps1
@@ -93,6 +93,7 @@ try {
        $SQLMediaPath = "C:\SQLServerSetup\setup.exe"
     }
     else {
+        Write-Output "Base Windows ami: Attemting complete failover fci from available or s3 downloaded sql installer."
         $SQLMediaPath = 'C:\cfn\Installer\SQLServerSetup\setup.exe'
         If (Test-Path -path "C:\SQL*") {
             $SQLInstallerPaths = (Get-ChildItem "C:\SQL*" -Recurse | where {$_.name -eq "setup.exe"} ).fullname
