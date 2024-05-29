231ef339845614e797044096cf6c5d2d729c0289
krithib <krithib@netapp.com>
2024-05-29 22:34:36 +0530

    DBS-2688: FCI Custom ami deployment - Named instance

--- server/resources/mssql/scripts/sqlfci/Validate-SQLLogin.ps1
+++ server/resources/mssql/scripts/sqlfci/Validate-SQLLogin.ps1
@@ -3,40 +3,14 @@ param(
   [Parameter(Mandatory = $true)]
   [string]$SQLServer
 )
-
-# Get SQL server instance name
-$SQLServiceList = Get-WmiObject win32_service | ?{$_.DisplayName -like 'sql server (*'}
-$SQLInstanceName = "MSSQLSERVER"
-$SQLInstanceNames = @()
-ForEach ($sqlService in $sqlServiceList) {
-$sqlServiceBinaryPath = $sqlService.PathName  -Replace "-s.*", ""
-  If (Test-Path $sqlServiceBinaryPath.Replace('"', '')) {
-    $SqlVersion = Invoke-Expression -Command "(dir $sqlServiceBinaryPath).VersionInfo"}
-    $ValidSqlVersion = $SqlVersion.ProductVersion -match '^1[3-9]'
-    If ($ValidSqlVersion -eq $true) {
-        $InstanceName =  $sqlService.Name.Replace("MSSQL$", "") 
-        $SQLInstanceNames += $InstanceName
-    }} 
-
-If ($SQLInstanceNames -NotContains "MSSQLSERVER") {
-    $SQLInstanceName = $SQLInstanceNames[0]
-}
-Write-Output "SQL instance name $SQLInstanceName."
-
-# Instance name to be passed to sqlcmd
-$ServerInstanceName = "$env:COMPUTERNAME"
-If($SQLInstanceName -ne "MSSQLSERVER") {
-    $ServerInstanceName = "$env:COMPUTERNAME\$SQLInstanceName"
-    
-}
-Write-Output "Sql server name $ServerInstanceName."
+ 
 
 $Query = 'SELECT name FROM sys. databases'
 
 try {
   #Execute a query with trusted connection. If you omit the server, it will default to localhost.
   Write-Output "Connecting with Windows Authentication"
-  $results =  Invoke-Sqlcmd -ServerInstance $ServerInstanceName -ConnectionString "Data Source=$SqlServer; Integrated Security=True; TrustServerCertificate=True" -Query "$Query" 
+  $results =  Invoke-Sqlcmd -ConnectionString "Data Source=$SqlServer; Integrated Security=True; TrustServerCertificate=True" -Query "$Query" 
   Write-Output "Connected with Windows Authentication" 
   Write-Output $results
     }
