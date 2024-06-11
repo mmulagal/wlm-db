627c31e2d6658057cb4acec002c3bb8dd010f8dd
Ayush <payush@netapp.com>
2024-06-11 13:39:49 +0530

    fixed success message

--- ui/src/utils/appConstants.ts
+++ ui/src/utils/appConstants.ts
@@ -645,7 +645,7 @@ export const GENERAL = {
             ],
             refresh: ['Sandbox of database ', ' refreshed successfully.'],
             split: ['Sandbox database ', ' split successfully.'],
-            integrityCheck: ['Integrity check of ', ' successfull']
+            integrityCheck: ['Integrity check of ', ' is completed successfully']
         },
         FAILED: {
             delete: ['Sandbox database ', ' failed to delete.'],
