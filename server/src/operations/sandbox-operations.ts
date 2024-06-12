2482d2f91e36a0485db654c891278454ca05ef72
Goyal, Yashendra <Yashendra.Goyal@netapp.com>
2024-06-12 13:38:54 +0530

    DBS-3029,DBS-3079 - S14_Clone: Need of volume name instead of volume uuid in error message, S15_Clone: ONTAP error message is not handled during flexclone failure

--- server/src/operations/sandbox-operations.ts
+++ server/src/operations/sandbox-operations.ts
@@ -2381,7 +2381,10 @@ async function performSplitOperation(
             credentialsId,
             region,
             parentJobId,
-            [mappings.data.volumeUuid, mappings.log.volumeUuid],
+            JSON.stringify([
+                { volumeId: mappings.data.volumeUuid, volumeName: mappings.data.volumeName },
+                { volumeId: mappings.log.volumeUuid, volumeName: mappings.log.volumeName }
+            ]),
             resDetails
         );
 
@@ -2460,10 +2463,10 @@ async function splitVolumes(
     credentialsId: string,
     region: string,
     parentJobId: string,
-    volumeIds: Array<string>,
+    volumes: string,
     resourceDetail: HostAndDbInfo
 ) {
-    logger.info('Split volumes', { accountId, credentialsId, region, parentJobId, volumeIds, resourceDetail });
+    logger.info('Split volumes', { accountId, credentialsId, region, parentJobId, volumes, resourceDetail });
 
     let status: string = JOBSTATUS.IN_PROGRESS;
     let errorMsg;
@@ -2478,9 +2481,7 @@ async function splitVolumes(
     });
 
     try {
-        const command = [
-            splitFlexCloneVolumes(resourceDetail.fsxId, region, JSON.stringify(volumeIds), resourceDetail.instanceName)
-        ];
+        const command = [splitFlexCloneVolumes(resourceDetail.fsxId, region, volumes, resourceDetail.instanceName)];
 
         const resp = await callSsmExecution(
             credentialsId,
