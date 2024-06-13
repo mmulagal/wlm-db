ac3c34ab0a94522a49d24bbb9bd510b1f0887656
rpanwar@netapp.com <rpanwar@netapp.com>
2024-06-13 09:45:31 +0530

    DBS-3145 small ui fix

--- ui/src/workloadFactory/Sandbox/SandboxTable/SandboxTable.tsx
+++ ui/src/workloadFactory/Sandbox/SandboxTable/SandboxTable.tsx
@@ -510,7 +510,8 @@ const SandboxTable = () => {
                 header={'Integrity check'}
                 content={
                     <DsTypography variant="Regular_14">
-                        `Do you want to perform integrity check for sandbox ${rowData.name}`
+                        Do you want to perform integrity check for sandbox{' '}
+                        <span style={{ fontWeight: '590' }}>{rowData.name}</span>
                     </DsTypography>
                 }
                 primaryButton={'Integrity check'}
