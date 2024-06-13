c5a22a39ed025abe06e4905103f252ca67c21322
U, Srigowri <Srigowri.U@netapp.com>
2024-06-13 11:39:11 -0700

    DBS-3146 - Explore Savings - Wrong recommended monthly charges for SQL standard edition - license price update

--- server/src/operations/recommendation-operations.ts
+++ server/src/operations/recommendation-operations.ts
@@ -430,8 +430,8 @@ export default async function getSqlInstanceLicenseRecommendations(
                     sqlServerDeploymentType,
                     recommendedSqlLicenseType,
                     ec2InstanceType,
-                    existingInstanceHourlyPrice,
-                    existingInstanceHourlyPriceWithoutLicense
+                    recommendedInstanceHourlyPrice, // license type is already identified, so use the price for the recommended license type which is essentially existingInstanceTypePricingDetails?.[recommendedSqlLicenseType]?.pricePerUnit
+                    recommendedInstanceHourlyPriceWithoutLicense
                 );
                 recommendedInstanceHourlyPrice = recommendedCompute.price;
                 recommendedInstanceHourlyPriceWithoutLicense = recommendedCompute.baseInstancePrice;
@@ -444,7 +444,7 @@ export default async function getSqlInstanceLicenseRecommendations(
                     recommendedSqlLicenseType !== existingLicenseType
                 ) {
                     recommendedLicense = {
-                        sqlServerEdition: `Standard Edition ${processorArchitecture}`,
+                        sqlServerEdition: `Standard Edition (${processorArchitecture})`,
                         sqlServerVersion,
                         price:
                             recommendedInstanceHourlyPrice && recommendedInstanceHourlyPriceWithoutLicense
