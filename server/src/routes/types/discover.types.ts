f3427a941d98b1a3545b6c39991a795bd5979324
R Ranganatha <rranga@netapp.com>
2024-06-20 12:00:49 +0530

    - Provided SQL Server instance GUID in manage API response. - Corrected/added updation of WLMDB tables.

--- server/src/routes/types/discover.types.ts
+++ server/src/routes/types/discover.types.ts
@@ -150,6 +150,7 @@ const MultiInstanceManageResponseBody = Type.Object({
     items: Type.Array(
         Type.Object({
             databaseInstanceName: Type.String({ description: 'SQL Server database instance name.' }),
+            databaseInstanceGuid: Type.Optional(Type.String({ description: 'SQL Server database instance GUID.' })),
             status: Type.String({ description: 'Status of database instance unmanage operation.' }),
             errorMessage: Type.Optional(
                 Type.String({ description: 'Error details, if any, of a failed database instance management.' })
