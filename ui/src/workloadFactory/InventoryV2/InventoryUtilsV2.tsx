3c9e8efb3fc15dbc0939fc9460d509a4a94da668
Sonam Sonam <sonamy@netapp.com>
2024-06-20 23:10:21 +0530

    Initial commit for detect

--- ui/src/workloadFactory/InventoryV2/InventoryUtilsV2.tsx
+++ ui/src/workloadFactory/InventoryV2/InventoryUtilsV2.tsx
@@ -1069,8 +1069,10 @@ export const updateInstanceStatus = (action: InstanceActions, hostData: any, ins
                     : INVENTORY_ACTIONS.MANAGE,
             actionDisable: false,
             sqlServerInstances: inventoryTableData[targettedHostId].sqlServerInstances.map((instanceItem: any) => {
-                if (instanceItem?.databaseInstanceId === instanceData?.databaseInstanceId) {
+                if (instanceItem?.databaseInstanceName === instanceData?.databaseInstanceName) {
                     return { ...instanceItem, statusColText: INVENTORY_STATUS.UNMANAGED };
+                } else {
+                    return { ...instanceItem };
                 }
                 return instanceItem;
             })
