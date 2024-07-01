776f3a3ba10c12bae1c9962f6c190792156a5942
krithib <krithib@netapp.com>
2024-07-01 08:46:10 +0530

    DBS-3319: Support Calgary region (Canada) AMI & deployment

--- server/src/operations/aws/ec2-operations.ts
+++ server/src/operations/aws/ec2-operations.ts
@@ -334,7 +334,8 @@ async function getAmiList(
                 '442396546477', // af-south-1
                 '777534740333', // ap-east-1
                 '460214486919', // eu-south-1
-                '162367869970' // me-south-1
+                '162367869970', // me-south-1
+                '194652444849' // ca-west-1
             ]
         });
 
