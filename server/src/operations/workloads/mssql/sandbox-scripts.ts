4ab62115f02447a8fbe71bb2c6720a80443b5294
Goyal, Yashendra <Yashendra.Goyal@netapp.com>
2024-07-05 11:54:29 +0530

    DBS-2882 - In case of mix protocol setup validate if source database is on iSCSI drive

--- server/src/operations/workloads/mssql/sandbox-scripts.ts
+++ server/src/operations/workloads/mssql/sandbox-scripts.ts
@@ -222,6 +222,12 @@ const getDbMappedOntapVolumes = (fsxid: string, fsxregion: string, dbName: strin
             $responseObject = [ordered]@{}
             $winvolumes = $sqlresponse | foreach { $_ | ConvertFrom-Json }
             foreach ($winvolume in $winvolumes) {
+
+                # check in winvolume volume id is null or empty string
+
+                if ([string]::IsNullOrEmpty($winvolume.volumeid)) {
+                    throw "Could not get volume id for database $dbname, please make sure the database is using iscsi protocol"
+                }
                 $vol = get-volume -Path $winvolume.volumeid | Get-Partition | get-disk | Select serialnumber, bustype
 
                 if ($vol.bustype -ne 'iscsi') {
