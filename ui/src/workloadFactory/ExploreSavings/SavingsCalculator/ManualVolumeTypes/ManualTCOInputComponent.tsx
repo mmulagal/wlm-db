9ef7c1581e39eeca81b38023c53499be406b2260
rpanwar@netapp.com <rpanwar@netapp.com>
2024-07-22 12:04:13 +0530

    DBS-3510 Validation applied on volume type input fields

--- ui/src/workloadFactory/ExploreSavings/SavingsCalculator/ManualVolumeTypes/ManualTCOInputComponent.tsx
+++ ui/src/workloadFactory/ExploreSavings/SavingsCalculator/ManualVolumeTypes/ManualTCOInputComponent.tsx
@@ -17,6 +17,57 @@ type ManualInputs = {
 const ManualTCOInputComponent = ({ type, throughPutDisable = false, IOPSDisable = false, from }: ManualInputs) => {
     const dispatch = useDispatch();
     const { manualTCOVolumeTypes, manualTCOVolumeTypes2 } = useAppSelector(state => state.exploreSavings);
+
+    const handleIOPSError = () => {
+        if (type === 'gp3') {
+            const primaryVol = manualTCOVolumeTypes?.[type]?.manualTCOProvisionedIOPS;
+            const secondaryVol = manualTCOVolumeTypes2?.[type]?.manualTCOProvisionedIOPS;
+            if (
+                (primaryVol > 0 && (primaryVol < 3000 || primaryVol > 16000)) ||
+                (secondaryVol > 0 && (secondaryVol < 3000 || secondaryVol > 16000))
+            ) {
+                return 'IOPS must be between 3000 and 16000.';
+            }
+        }
+
+        if (type === 'io1') {
+            const primaryVol = manualTCOVolumeTypes?.[type]?.manualTCOProvisionedIOPS;
+            const secondaryVol = manualTCOVolumeTypes2?.[type]?.manualTCOProvisionedIOPS;
+            if (
+                (primaryVol > 0 && (primaryVol < 100 || primaryVol > 64000)) ||
+                (secondaryVol > 0 && (secondaryVol < 100 || secondaryVol > 64000))
+            ) {
+                return 'IOPS must be between 100 and 64000.';
+            }
+        }
+
+        if (type === 'io2') {
+            const primaryVol = manualTCOVolumeTypes?.[type]?.manualTCOProvisionedIOPS;
+            const secondaryVol = manualTCOVolumeTypes2?.[type]?.manualTCOProvisionedIOPS;
+            if (
+                (primaryVol > 0 && (primaryVol < 100 || primaryVol > 256000)) ||
+                (secondaryVol > 0 && (secondaryVol < 100 || secondaryVol > 256000))
+            ) {
+                return 'IOPS must be between 100 and 256000.';
+            }
+        }
+        return '';
+    };
+
+    const handleThroughputError = () => {
+        if (type === 'gp3') {
+            const primaryVol = manualTCOVolumeTypes?.[type]?.manualTCOThroughput;
+            const secondaryVol = manualTCOVolumeTypes2?.[type]?.manualTCOThroughput;
+            if (
+                (primaryVol > 0 && (primaryVol < 125 || primaryVol > 1000)) ||
+                (secondaryVol > 0 && (secondaryVol < 125 || secondaryVol > 1000))
+            ) {
+                return 'Throughput must be between 125 and 1000.';
+            }
+        }
+
+        return '';
+    };
     return (
         <div className={styles.mainSection}>
             <div className={styles.row}>
@@ -112,6 +163,7 @@ const ManualTCOInputComponent = ({ type, throughPutDisable = false, IOPSDisable 
                             : manualTCOVolumeTypes2?.[type]?.manualTCOProvisionedIOPS
                     }
                     className={styles.deploymentModelWidth}
+                    error={handleIOPSError()}
                 />
 
                 <TextField
@@ -143,6 +195,7 @@ const ManualTCOInputComponent = ({ type, throughPutDisable = false, IOPSDisable 
                             : manualTCOVolumeTypes2?.[type]?.manualTCOThroughput
                     }
                     className={styles.deploymentModelWidth}
+                    error={handleThroughputError()}
                 />
             </div>
         </div>
