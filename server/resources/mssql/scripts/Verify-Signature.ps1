b2e95ca8a59137286a7b5e8fe010d0aa6a74d65a
krithib <krithib@netapp.com>
2024-07-22 15:52:50 +0530

    DBS-3444

--- server/resources/mssql/scripts/Verify-Signature.ps1
+++ server/resources/mssql/scripts/Verify-Signature.ps1
@@ -1,37 +1,44 @@
 param(
         [Parameter(Mandatory=$true)]
         $FilePath,
+
         [Parameter(Mandatory=$true)]
         $SignatureFilePath,
+
         [Parameter(Mandatory=$true)]
-        $PubFilePath
+        $PubFilePath,
+
+        [Parameter(Mandatory=$true)]
+        [string]$ResourceID,   
+
+        [Parameter(Mandatory=$true)]
+        [string]$Stackname
     )
 
 $ErrorActionPreference = "Stop"
-$LogFilePath = "C:\cfn\log\$($MyInvocation.MyCommand.Name).log"
-Start-Transcript -Path $LogFilePath -Append
+Start-Transcript -Path C:\cfn\log\verifysignature.log -Append
 
-try 
-    {
-        & "C:\cfn\OpenSSL-Win64\bin\openssl.exe" dgst -sha256 -verify $PubFilePath -signature $SignatureFilePath  $FilePath > $LogFilePath 2>&1
-    }
-catch 
-    {
-        Write-Host "Error while verifying signature for $FilePath"
+#get Instance ID
+$token = Invoke-RestMethod -Headers @{"X-aws-ec2-metadata-token-ttl-seconds" = "21600"} -Method PUT -Uri "http://169.254.169.254/latest/api/token"
+$instanceID = Invoke-RestMethod -Headers @{"X-aws-ec2-metadata-token" = $token} -Method GET -Uri http://169.254.169.254/latest/meta-data/instance-id
+
+$logfilename = Split-Path $FilePath -leaf
+
+try {
+    & "C:\Program Files\OpenSSL-Win64\bin\openssl" dgst -sha256 -verify  $PubFilePath -signature $SignatureFilePath  $FilePath >  C:\cfn\log\$logfilename.txt 2>&1
+}catch { 
+    Write-host "Error while verifying signature for $FilePath : $_."
+    Send-CFNResourceSignal -StackName $Stackname -Status FAILURE -LogicalResourceId $ResourceID -UniqueId $instanceId
      } 
     
+# Check if verified or not
+$IsVerified = Select-String -Path C:\cfn\log\$logfilename.txt -Pattern "Verified OK"
+if (-not ([string]::IsNullOrEmpty($IsVerified)) ) {
 
-Add-Type -TypeDefinition $code -Language CSharp
-$VerifySig = [Crypto.CryptoHelper]::VerifySignature($FilePath,$SignatureFilePath)
-#$VerifySig = Invoke-Expression $code
-If ($VerifySig)
-{
-    Write-Output " Signature Verified for "+$FilePath
+    Write-Host "Signature verification for $FilePath passed."
 }
 else {
      Write-Output "Signature verification failed "+$FilePath
-     exit 1
+     Send-CFNResourceSignal -StackName $Stackname -Status FAILURE -LogicalResourceId $ResourceID -UniqueId $instanceId
 }
-
-
 
