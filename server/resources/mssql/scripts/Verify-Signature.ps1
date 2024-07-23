edf6af9a38865a7c4880db3b219d632af96a4e5c
krithib <krithib@netapp.com>
2024-07-23 12:58:05 +0530

    DBS-3444: Correction in verification script

--- server/resources/mssql/scripts/Verify-Signature.ps1
+++ server/resources/mssql/scripts/Verify-Signature.ps1
@@ -25,20 +25,23 @@ $instanceID = Invoke-RestMethod -Headers @{"X-aws-ec2-metadata-token" = $token} 
 $logfilename = Split-Path $FilePath -leaf
 
 try {
-    & "C:\Program Files\OpenSSL-Win64\bin\openssl" dgst -sha256 -verify  $PubFilePath -signature $SignatureFilePath  $FilePath >  C:\cfn\log\$logfilename.txt 2>&1
+    & "C:\cfn\OpenSSL-Win64\bin\openssl" dgst -sha256 -verify  $PubFilePath -signature $SignatureFilePath  $FilePath >  C:\cfn\log\$logfilename.txt 2>&1
+
+    # Check if verified or not
+    $IsVerified = Select-String -Path C:\cfn\log\$logfilename.txt -Pattern "Verified OK"
+    if (-not ([string]::IsNullOrEmpty($IsVerified)) ) {
+
+        Write-Host "Signature verification for $FilePath passed."
+    }
+    else {
+        Write-Output "Signature verification failed "+$FilePath
+        Send-CFNResourceSignal -StackName $Stackname -Status FAILURE -LogicalResourceId $ResourceID -UniqueId $instanceId
+        exit(1)
+    }
 }catch { 
     Write-host "Error while verifying signature for $FilePath : $_."
     Send-CFNResourceSignal -StackName $Stackname -Status FAILURE -LogicalResourceId $ResourceID -UniqueId $instanceId
+    exit(1)
      } 
     
-# Check if verified or not
-$IsVerified = Select-String -Path C:\cfn\log\$logfilename.txt -Pattern "Verified OK"
-if (-not ([string]::IsNullOrEmpty($IsVerified)) ) {
-
-    Write-Host "Signature verification for $FilePath passed."
-}
-else {
-     Write-Output "Signature verification failed "+$FilePath
-     Send-CFNResourceSignal -StackName $Stackname -Status FAILURE -LogicalResourceId $ResourceID -UniqueId $instanceId
-}
 
