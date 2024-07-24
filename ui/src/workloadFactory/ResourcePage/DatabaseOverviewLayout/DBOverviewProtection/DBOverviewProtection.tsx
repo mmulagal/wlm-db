228440a6a7b90b6bbb56998143e0877a9a5950a2
rpanwar@netapp.com <rpanwar@netapp.com>
2024-07-24 13:05:34 +0530

    DBS-3515 Resource page 1600 Resolution

--- ui/src/workloadFactory/ResourcePage/DatabaseOverviewLayout/DBOverviewProtection/DBOverviewProtection.tsx
+++ ui/src/workloadFactory/ResourcePage/DatabaseOverviewLayout/DBOverviewProtection/DBOverviewProtection.tsx
@@ -1,4 +1,4 @@
-import { FlashingDotsLoader, Typography } from '@netapp/design-system';
+import { DsTypography, FlashingDotsLoader, Typography } from '@netapp/design-system';
 import styles from './DBOverviewProtection.module.scss';
 import { GENERAL } from '../../../../utils/appConstants';
 import MultiRingDoughnut from '../../../DatabaseHomePage/MultiRingDoughnut/MultiRingDoughnut';
@@ -6,52 +6,97 @@ import SquareComponent from '../../../DatabaseHomePage/SquareComponent/SquareCom
 import { useAppSelector } from '../../../../store/storeHooks';
 import { getAggrProtection } from '../../../../utils/utilityFunctions';
 import { useMemo } from 'react';
+import useResize from '../../../../common/hooks/useResize';
 
 const DBOverviewProtection = () => {
     const { databaseList, databaseListLoading } = useAppSelector(state => state.workloadFactoryResource);
+    const windowSize = useResize();
 
     const protectionData = useMemo(() => {
         return getAggrProtection(databaseList);
     }, [databaseList]);
 
     return (
-        <div className={styles.dbOverviewProtection}>
-            <div className={styles.headSection}>
-                <Typography variant="Regular_16" className={styles.title}>
-                    {GENERAL.DB_HOST_PROTECTION}
-                </Typography>
-                {databaseListLoading && <FlashingDotsLoader />}
-            </div>
+        <>
+            {windowSize.width > 1800 && (
+                <div className={styles.dbOverviewProtection}>
+                    <div className={styles.headSection}>
+                        <Typography variant="Regular_16" className={styles.title}>
+                            {GENERAL.DB_HOST_PROTECTION}
+                        </Typography>
+                        {databaseListLoading && <FlashingDotsLoader />}
+                    </div>
 
-            <div className={styles.mainContainer}>
-                <div className={styles.chartContainer}>
-                    <MultiRingDoughnut
-                        unProtectColor={'var(--chart-disabled)'}
-                        hostData={protectionData}
-                    />
+                    <div className={styles.mainContainer}>
+                        <div className={styles.chartContainer}>
+                            <MultiRingDoughnut unProtectColor={'var(--chart-disabled)'} hostData={protectionData} />
+                        </div>
+
+                        <div className={styles.protectionSeparator} />
+
+                        <div className={styles.textSection}>
+                            <SquareComponent
+                                value={`${protectionData.protectedDb} Databases`}
+                                color="var(--chart-4)"
+                                text={'Protected'}
+                                boldValue={true}
+                            />
+
+                            <div className={styles.dbHostSeparator} />
+
+                            <SquareComponent
+                                value={`${protectionData.unprotectedDb} Databases`}
+                                color="var(--chart-disabled)"
+                                text={'Unprotected'}
+                                boldValue={true}
+                            />
+                        </div>
+                    </div>
                 </div>
+            )}
 
-                <div className={styles.protectionSeparator} />
+            {windowSize.width <= 1800 && (
+                <div className={styles.dbOverviewProtectionSmallScreen}>
+                    <div className={styles.headSection}>
+                        <Typography variant="Regular_16" className={styles.title}>
+                            {GENERAL.DB_HOST_PROTECTION}
+                        </Typography>
+                        {databaseListLoading && <FlashingDotsLoader />}
+                    </div>
 
-                <div className={styles.textSection}>
-                    <SquareComponent
-                        value={`${protectionData.protectedDb} Databases`}
-                        color="var(--chart-4)"
-                        text={'Protected'}
-                        boldValue={true}
-                    />
+                    <div className={styles.mainContainer}>
+                        <div className={styles.chartContainer}>
+                            <MultiRingDoughnut unProtectColor={'var(--chart-disabled)'} hostData={protectionData} />
+                        </div>
 
-                    <div className={styles.dbHostSeparator} />
+                        <div className={styles.textAreaSection}>
+                            <DsTypography variant="Semibold_14">Protection distribution</DsTypography>
+                            <div className={styles.protectionSeparator} />
 
-                    <SquareComponent
-                        value={`${protectionData.unprotectedDb} Databases`}
-                        color="var(--chart-disabled)"
-                        text={'Unprotected'}
-                        boldValue={true}
-                    />
+                            <div className={styles.textSection}>
+                                <SquareComponent
+                                    value={`${protectionData.protectedDb} Databases`}
+                                    color="var(--chart-4)"
+                                    text={'Protected'}
+                                    boldValue={true}
+                                />
+
+                                <div className={styles.dbHostSeparator} />
+
+                                <SquareComponent
+                                    value={`${protectionData.unprotectedDb} Databases`}
+                                    color="var(--chart-disabled)"
+                                    text={'Unprotected'}
+                                    boldValue={true}
+                                />
+                            </div>
+
+                            <div className={styles.protectionSeparator} />
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            )}
+        </>
     );
 };
 
