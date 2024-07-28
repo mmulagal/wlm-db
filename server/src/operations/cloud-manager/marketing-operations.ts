9f019292630eff9c7a64a99d4738b2ebad17240a
U, Srigowri <Srigowri.U@netapp.com>
2024-07-28 10:50:46 -0700

    DBS-2753 - DB TCO for EBS to FsxN- Manual mode - auto mode demo fix

--- server/src/operations/cloud-manager/marketing-operations.ts
+++ server/src/operations/cloud-manager/marketing-operations.ts
@@ -149,11 +149,23 @@ async function invokeMarketingApi(
         const { clonedCopiesCount, monthlyChangeRatePercentage } = params;
         region = STORAGE_SERVICE_DEFAULT_REGION;
         let volumes = [
-            { volumeType: 'io2', volumeNumber: 2, storageAmount: 1024 * 2, volumeIops: 40000, throughput: 128 }
+            {
+                volumeType: 'io2',
+                volumeNumber: 2,
+                storageAmount: convertToBytes(1024 * 2, 'GiB') || 0,
+                volumeIops: 40000,
+                throughput: 128
+            }
         ];
         if (sqlServerDeploymentType === SqlServerDeploymentModel.SQL_AOAG_SHORT) {
             volumes = [
-                { volumeType: 'io2', volumeNumber: 2, storageAmount: 1024 * 10, volumeIops: 40000, throughput: 128 }
+                {
+                    volumeType: 'io2',
+                    volumeNumber: 2,
+                    storageAmount: convertToBytes(1024 * 10, 'GiB') || 0,
+                    volumeIops: 40000,
+                    throughput: 128
+                }
             ];
         }
 
