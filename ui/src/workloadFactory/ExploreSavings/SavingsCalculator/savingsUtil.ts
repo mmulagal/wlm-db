938a07d34a1d2f1b990bf898e7edf07ff34576c2
rpanwar@netapp.com <rpanwar@netapp.com>
2024-07-30 14:54:17 +0530

    DBS-3568 hiding BYOL in manual mode

--- ui/src/workloadFactory/ExploreSavings/SavingsCalculator/savingsUtil.ts
+++ ui/src/workloadFactory/ExploreSavings/SavingsCalculator/savingsUtil.ts
@@ -1508,9 +1508,9 @@ export const generateManualStorageSavingsPayload = () => {
     payloadObj.clonedCopiesCount = Number(numberOfClonedCopies);
     payloadObj.snapshotFrequency = selectedSnapshotFrequency?.value;
     payloadObj.monthlyChangeRatePercentage = Number(monthlyChangeRate);
-    if (monthlyBYOLCost) {
-        payloadObj.monthlySqlByolCost = Number(monthlyBYOLCost);
-    }
+    // if (monthlyBYOLCost) {
+    //     payloadObj.monthlySqlByolCost = Number(monthlyBYOLCost);
+    // }
     payloadObj.sqlServerEdition = setSQLServerEdition(selectedManualServerEdition?.value);
     payloadObj.ec2Instances = createInstances(state);
     return payloadObj;
