1eca608e5f1a0445d6fb537895c5bb1505112f6a
rpanwar@netapp.com <rpanwar@netapp.com>
2024-07-31 16:04:19 +0530

    DBS-3582 Explore savings 1600 and 1440

--- ui/src/utils/appConstants.ts
+++ ui/src/utils/appConstants.ts
@@ -974,12 +974,12 @@ export const GENERAL = {
     ES_SAVINGS_PERCENTAGE: 'Percentage savings',
     ES_HEADING: 'Explore savings of selected host',
     ES_HEADER:
-        'Explore cost savings with Databases automatically. select a Microsoft SQL server host and then click "Explore savings". We\'ll calculate your potential savings by moving to Microsoft SQL server using FSx for ONTAP file systems.',
+        'Automatically explore savings by selecting a Microsoft SQL Server host and then click "Explore savings". We\'ll calculate your potential savings with FSx for ONTAP storage for your database workloads.',
     ES_SAVINGS: 'Explore savings',
     MANUAL_EXLORE_SAVINGS: 'Manual explore savings',
     EXPLORE_SAVING_MANUALLY: 'Explore savings Manually',
     MANUAL_EXPLORE_SAVINGS_CONTENT:
-        'Alternatively, select the "manual explore savings" option for analysis of potential savings according to your needs and specifications of your Microsoft SQL server configuration.',
+        'Alternatively, select "Explore savings manually". You\'ll  provide Microsoft SQL Server configuration details to get an accurate cost estimate comparison.',
     INSTANCE_INFORMATION: 'Instance information:',
     ES_SAVE_CONFIG: 'Save configuration',
     ES_SAVE_CONFIG_DESC:
--- ui/src/workloadFactory/ExploreSavings/ExploreSavingHeader/ExploreSavingHeader.tsx
+++ ui/src/workloadFactory/ExploreSavings/ExploreSavingHeader/ExploreSavingHeader.tsx
@@ -1,5 +1,7 @@
 import { DsButton, DsTypography } from '@netapp/design-system';
 import { ReactComponent as ExploreSaving } from '../../../assets/explore-saving.svg';
+import { ReactComponent as ExploreSaving1600 } from '../../../assets/exploreSaving1600.svg';
+import { ReactComponent as ExploreSaving1440 } from '../../../assets/exploreSaving1440.svg';
 import styles from './ExploreSavingHeader.module.scss';
 import ExploreSavingsTable from '../ExploreSavingsTable/ExploreSavingsTable';
 import { GENERAL } from '../../../utils/appConstants';
@@ -7,49 +9,133 @@ import { useAppSelector } from '../../../store/storeHooks';
 import ExploreSavingsTableV2 from '../ExploreSavingsTableV2/ExploreSavingsTableV2';
 import { handleManualTCO } from '../ExploreSavingsUtils';
 import { useDispatch } from 'react-redux';
+import useResize from '../../../common/hooks/useResize';
 
 const ExploreSavingHeader = () => {
     const isInventoryV2 = useAppSelector(state => state.auth.isInventoryV2);
     const dispatch = useDispatch();
+    const windowSize = useResize();
 
     return (
-        <div className={styles.exploreSavingsHeader}>
-            <div className={styles.topPart}>
-                <div className={styles.svgContainer}>
-                    <ExploreSaving />
-                </div>
-                <div className={styles.contentSection}>
-                    <div className={styles.leftSide}>
-                        <div className={styles.headingPart}>
-                            <DsTypography variant="Semibold_16" style={{ lineHeight: '32px' }}>
-                                {GENERAL.ES_HEADING}
-                            </DsTypography>
+        <>
+            {windowSize.width > 1715 && (
+                <div className={styles.exploreSavingsHeader}>
+                    <div className={styles.topPart}>
+                        <div className={styles.svgContainer}>
+                            <ExploreSaving />
                         </div>
+                        <div className={styles.contentSection}>
+                            <div className={styles.leftSide}>
+                                <div className={styles.headingPart}>
+                                    <DsTypography variant="Semibold_16" style={{ lineHeight: '32px' }}>
+                                        {GENERAL.ES_HEADING}
+                                    </DsTypography>
+                                </div>
 
-                        <DsTypography variant="Regular_16" className={styles.subText}>
-                            {GENERAL.ES_HEADER}
-                        </DsTypography>
-                    </div>
+                                <DsTypography variant="Regular_16" className={styles.subText}>
+                                    {GENERAL.ES_HEADER}
+                                </DsTypography>
+                            </div>
 
-                    <div className={styles.rightSide}>
-                        <div className={styles.headingPart}>
-                            <DsTypography variant="Semibold_16" style={{ lineHeight: '32px' }}>
-                                {GENERAL.MANUAL_EXLORE_SAVINGS}
-                            </DsTypography>
+                            <div className={styles.rightSide}>
+                                <div className={styles.headingPart}>
+                                    <DsTypography variant="Semibold_16" style={{ lineHeight: '32px' }}>
+                                        {GENERAL.MANUAL_EXLORE_SAVINGS}
+                                    </DsTypography>
+                                </div>
+
+                                <span className={styles.subText}>
+                                    <span>{GENERAL.MANUAL_EXPLORE_SAVINGS_CONTENT}</span>
+                                    <span className={styles.link} onClick={() => handleManualTCO(dispatch)}>
+                                        {GENERAL.EXPLORE_SAVING_MANUALLY}
+                                    </span>
+                                </span>
+                            </div>
                         </div>
-
-                        <span className={styles.subText}>
-                            <span>{GENERAL.MANUAL_EXPLORE_SAVINGS_CONTENT}</span> &nbsp;
-                            <span className={styles.link} onClick={() => handleManualTCO(dispatch)}>
-                                {GENERAL.EXPLORE_SAVING_MANUALLY}
-                            </span>
-                        </span>
                     </div>
+                    {!isInventoryV2 && <ExploreSavingsTable />}
+                    {isInventoryV2 && <ExploreSavingsTableV2 />}
                 </div>
-            </div>
-            {!isInventoryV2 && <ExploreSavingsTable />}
-            {isInventoryV2 && <ExploreSavingsTableV2 />}
-        </div>
+            )}
+            {windowSize.width > 1429 && windowSize.width <= 1715 && (
+                <div className={styles.exploreSavingsHeader}>
+                    <div className={styles.topPart}>
+                        <div className={styles.svgContainer}>
+                            <ExploreSaving1600 />
+                        </div>
+                        <div className={styles.contentSection}>
+                            <div className={styles.leftSide}>
+                                <div className={styles.headingPart}>
+                                    <DsTypography variant="Semibold_16" style={{ lineHeight: '32px' }}>
+                                        {GENERAL.ES_HEADING}
+                                    </DsTypography>
+                                </div>
+
+                                <DsTypography variant="Regular_16" className={styles.subText}>
+                                    {GENERAL.ES_HEADER}
+                                </DsTypography>
+                            </div>
+
+                            <div className={styles.rightSide}>
+                                <div className={styles.headingPart}>
+                                    <DsTypography variant="Semibold_16" style={{ lineHeight: '32px' }}>
+                                        {GENERAL.MANUAL_EXLORE_SAVINGS}
+                                    </DsTypography>
+                                </div>
+
+                                <span className={styles.subText}>
+                                    <span>{GENERAL.MANUAL_EXPLORE_SAVINGS_CONTENT}</span>
+                                    <span className={styles.link} onClick={() => handleManualTCO(dispatch)}>
+                                        {GENERAL.EXPLORE_SAVING_MANUALLY}
+                                    </span>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    {!isInventoryV2 && <ExploreSavingsTable />}
+                    {isInventoryV2 && <ExploreSavingsTableV2 />}
+                </div>
+            )}
+            {windowSize.width <= 1428 && (
+                <div className={styles.exploreSavingsHeader}>
+                    <div className={styles.topPart}>
+                        <div className={styles.svgContainer}>
+                            <ExploreSaving1440 />
+                        </div>
+                        <div className={styles.contentSection}>
+                            <div className={styles.leftSide}>
+                                <div className={styles.headingPart}>
+                                    <DsTypography variant="Semibold_16" style={{ lineHeight: '32px' }}>
+                                        {GENERAL.ES_HEADING}
+                                    </DsTypography>
+                                </div>
+
+                                <DsTypography variant="Regular_16" className={styles.subText}>
+                                    {GENERAL.ES_HEADER}
+                                </DsTypography>
+                            </div>
+
+                            <div className={styles.rightSide}>
+                                <div className={styles.headingPart}>
+                                    <DsTypography variant="Semibold_16" style={{ lineHeight: '32px' }}>
+                                        {GENERAL.MANUAL_EXLORE_SAVINGS}
+                                    </DsTypography>
+                                </div>
+
+                                <span className={styles.subText}>
+                                    <span>{GENERAL.MANUAL_EXPLORE_SAVINGS_CONTENT}</span>
+                                    <span className={styles.link} onClick={() => handleManualTCO(dispatch)}>
+                                        {GENERAL.EXPLORE_SAVING_MANUALLY}
+                                    </span>
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    {!isInventoryV2 && <ExploreSavingsTable />}
+                    {isInventoryV2 && <ExploreSavingsTableV2 />}
+                </div>
+            )}
+        </>
     );
 };
 
