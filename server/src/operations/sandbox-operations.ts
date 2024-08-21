02715feb1e43a1cbca6206962815f6272e544a66
Goyal, Yashendra <Yashendra.Goyal@netapp.com>
2024-08-21 16:02:29 +0530

    DBS-3670 - S18_Sandbox: job monitor error message should say instance name instead of instance id

--- server/src/operations/sandbox-operations.ts
+++ server/src/operations/sandbox-operations.ts
@@ -798,14 +798,14 @@ async function validateCloneParams(
         if (destDatabaseExists) {
             throw createError(
                 412,
-                `Database ${destDetails.database} already exists on destination host ${srcDetails.host}`
+                `Database ${destDetails.database} already exists on destination host ${destDetails.resourceName}\\${destDetails.databaseInstanceName}`
             );
         }
 
         if (!srcDatabaseExists && !isDemoFlow) {
             throw createError(
                 412,
-                `Database ${srcDetails.database} does not exists on source host ${destDetails.host}`
+                `Database ${srcDetails.database} does not exists on source host ${srcDetails.resourceName}\\${srcDetails.databaseInstanceName}`
             );
         }
 
@@ -1852,7 +1852,7 @@ async function validateDeleteSandboxParams(
         if (!dbExists && !isDemoFlow) {
             throw createError(
                 412,
-                `Database ${resourceDetails.database} does not exists on source host ${resourceDetails.resourceName}`
+                `Database ${resourceDetails.database} does not exists on source host ${resourceDetails.resourceName}\\${resourceDetails.databaseInstanceName}`
             );
         }
 
@@ -2279,7 +2279,7 @@ async function validateLifeCycleParams(
         if (!dbExists && !isDemoFlow) {
             throw createError(
                 412,
-                `Database ${resourceDetails.database} does not exists on host ${resourceDetails.resourceName}`
+                `Database ${resourceDetails.database} does not exists on host ${resourceDetails.resourceName}\\${resourceDetails}\\${resourceDetails.databaseInstanceName}`
             );
         }
         status = JOBSTATUS.COMPLETED;
@@ -2591,7 +2591,7 @@ async function validateSplitParams(
         if (!dbExists && !isDemoFlow) {
             throw createError(
                 412,
-                `Database ${resourceDetails.database} does not exists on source host ${resourceDetails.resourceName}`
+                `Database ${resourceDetails.database} does not exists on source host ${resourceDetails.resourceName}\\${resourceDetails.databaseInstanceName}`
             );
         }
 
@@ -2783,7 +2783,7 @@ async function checkDatabaseIntegrity(
     if (!databaseDetails) {
         throw createError(
             HttpErrorCodes.NOT_FOUND,
-            `Database ${databaseName} does not exists on source host ${srcDetails.resourceName}`
+            `Database ${databaseName} does not exists on source host ${srcDetails.resourceName}\\${srcDetails.databaseInstanceName}`
         );
     }
 
