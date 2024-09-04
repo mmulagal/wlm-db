dd5d4a142d6dba19a87ea1ff680c790e9f479378
Mulagalapati, Murali <Murali.Mulagalapati@netapp.com>
2024-09-04 20:33:03 +0530

    DBS-3809: /sandboxes for multi instances

--- server/src/operations/workloads/mssql/queries.ts
+++ server/src/operations/workloads/mssql/queries.ts
@@ -294,6 +294,35 @@ const DATABASES_COUNT_V2 = `${SET_NOCOUNT} SELECT COUNT(*) AS totalCount FROM sy
 
 const SERVER_VERSION = `${SET_NOCOUNT} SELECT @@VERSION AS version ${FOR_JSON_PATH}`;
 
+const GET_SANDBOXES = `${SET_NOCOUNT}
+    DROP TABLE IF EXISTS #properties;
+
+    CREATE TABLE #properties (
+        database_name nvarchar(255),
+        name nvarchar(255),
+        value sql_variant
+    );
+
+    INSERT INTO #properties
+    EXEC sp_MSforeachdb '
+        USE [?];
+        SELECT database_name = DB_NAME(), l.name, l.value
+        FROM sys.databases d
+        OUTER APPLY fn_listextendedproperty(default, default, default, default, default, default, default) l
+        WHERE d.name = DB_NAME()
+        AND database_id > 4
+        AND l.name IS NOT NULL
+        AND l.value IS NOT NULL ';
+
+    SELECT database_name, JSON_QUERY(properties) AS sandbox_properties
+    FROM (
+        SELECT database_name, JSON_QUERY((SELECT name, value FROM #properties AS p2 WHERE p2.database_name = p1.database_name AND p2.name IN ('source', 'createdAt', 'tag', 'updatedAt', 'cloned_by', 'accountId') FOR JSON PATH)) AS properties
+        FROM #properties AS p1
+    ) AS grouped_properties
+    GROUP BY database_name, properties
+    ${FOR_JSON_PATH}
+`;
+
 export {
     DATABASES,
     DATABASES_COUNT,
@@ -322,5 +351,6 @@ export {
     INSTANCE_GUID,
     ENTERPRISE_CHECK_QUERY,
     DATABASES_COUNT_V2,
-    SERVER_VERSION
+    SERVER_VERSION,
+    GET_SANDBOXES
 };
