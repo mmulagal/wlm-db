b1b8acc8de15bb8bd7b8a48cf3b91d03da3eae95
U, Srigowri <Srigowri.U@netapp.com>
2024-09-12 15:11:50 -0700

    DBS-3652 - Get Well: data Storage Assessment investigation - added host config data table

---
+++ server/src/lib/database/host_config.ts
@@ -0,0 +1,78 @@
+import getLogger from '../../utils/logger';
+import { prisma } from '../../utils/prisma-utils';
+
+const logger = getLogger();
+
+interface HostConfigData {
+    account_id: string;
+    credentials_id: string;
+    region: string;
+    resource_id: string;
+    database_instance_id: string;
+    timestamp: Date;
+    last_updated?: Date;
+    config_data: object;
+    config_data_type: string;
+}
+async function createHostConfigData(records: HostConfigData[]) {
+    logger.info('Creating host config data', { records });
+
+    return prisma.client.host_config_data.createMany({
+        data: records
+    });
+}
+
+async function listHostConfigData(
+    accountId?: string,
+    region?: string,
+    credentialsId?: string,
+    resourceId?: string,
+    databaseInstanceId?: string,
+    configDataType?: string
+) {
+    logger.info('Listing host config data', {
+        accountId,
+        region,
+        credentialsId,
+        resourceId,
+        databaseInstanceId,
+        configDataType
+    });
+
+    return prisma.client.host_config_data.findMany({
+        where: {
+            ...(accountId && { account_id: accountId }),
+            ...(region && { region }),
+            ...(credentialsId && { credentials_id: credentialsId }),
+            ...(resourceId && { resource_id: resourceId }),
+            ...(databaseInstanceId && { database_instance_id: databaseInstanceId }),
+            ...(configDataType && { config_data_type: configDataType })
+        }
+    });
+}
+
+async function removeHostConfigData(
+    id?: string[],
+    accountId?: string,
+    region?: string,
+    credentialsId?: string,
+    resourceId?: string,
+    databaseInstanceId?: string
+) {
+    logger.info('Removing host config data', { id, accountId, region, credentialsId, resourceId, databaseInstanceId });
+
+    return prisma.client.host_config_data.deleteMany({
+        where: {
+            OR: [
+                ...(id ? [{ id: { in: id } }] : []),
+                ...(region ? [{ region }] : []),
+                ...(accountId ? [{ account_id: accountId }] : []),
+                ...(credentialsId ? [{ credentials_id: credentialsId }] : []),
+                ...(resourceId ? [{ resource_id: resourceId }] : []),
+                ...(databaseInstanceId ? [{ database_instance_id: databaseInstanceId }] : [])
+            ]
+        }
+    });
+}
+
+export { createHostConfigData, listHostConfigData, removeHostConfigData };
---
+++ server/test/lib/database/host-config.test.ts
@@ -0,0 +1,271 @@
+import { createHostConfigData, listHostConfigData, removeHostConfigData } from '../../../src/lib/database/host_config';
+import { ACCOUNT_ID, DEFAULT_AWS_CREDENTIALS_ID } from '../../utils/consts';
+
+describe('Host config data operations', () => {
+    it('should create Host config data records', async () => {
+        const hostConfigDataRecords = [
+            {
+                resource_id: 'i-1234567890abcdef0',
+                account_id: ACCOUNT_ID,
+                credentials_id: DEFAULT_AWS_CREDENTIALS_ID,
+                region: 'us-east-1',
+                database_instance_id: 'sql-server-broker-uuid',
+                timestamp: new Date(),
+                last_updated: new Date(),
+                config_data: {
+                    volumes: [
+                        {
+                            name: 'vol_murali_clone1',
+                            'thin-provision': true,
+                            'space-guarantee': 'none',
+                            autosize: 'off',
+                            'autosize-mode': 'off',
+                            'fractional-reserve': 0,
+                            'snapshot-policy': 'default',
+                            'snapshot-reserve': 5,
+                            'snapshot-autodelete': false,
+                            'tiering-policy': 'none',
+                            'tiering-min-cooling-days': null
+                        },
+                        {
+                            name: 'rranga_clone',
+                            'thin-provision': true,
+                            'space-guarantee': 'none',
+                            autosize: 'off',
+                            'autosize-mode': 'off',
+                            'fractional-reserve': 0,
+                            'snapshot-policy': 'default',
+                            'snapshot-reserve': 5,
+                            'snapshot-autodelete': false,
+                            'tiering-policy': 'auto',
+                            'tiering-min-cooling-days': 31
+                        }
+                    ]
+                },
+                config_data_type: 'CONFIG'
+            },
+            {
+                resource_id: 'i-1234567890abcdef0',
+                account_id: ACCOUNT_ID,
+                credentials_id: DEFAULT_AWS_CREDENTIALS_ID,
+                region: 'us-east-1',
+                database_instance_id: 'sql-server-broker-uuid',
+                timestamp: new Date(),
+                last_updated: new Date(),
+                config_data: {
+                    volumes: [
+                        {
+                            name: 'vol_murali_clone1',
+                            'thin-provision': true,
+                            'space-guarantee': 'none',
+                            autosize: 'off',
+                            'autosize-mode': 'off',
+                            'fractional-reserve': 0,
+                            'snapshot-policy': 'default',
+                            'snapshot-reserve': 5,
+                            'snapshot-autodelete': false,
+                            'tiering-policy': 'none',
+                            'tiering-min-cooling-days': null
+                        },
+                        {
+                            name: 'rranga_clone',
+                            'thin-provision': true,
+                            'space-guarantee': 'none',
+                            autosize: 'off',
+                            'autosize-mode': 'off',
+                            'fractional-reserve': 0,
+                            'snapshot-policy': 'default',
+                            'snapshot-reserve': 5,
+                            'snapshot-autodelete': false,
+                            'tiering-policy': 'auto',
+                            'tiering-min-cooling-days': 31
+                        }
+                    ]
+                },
+                config_data_type: 'PERFORMANCE'
+            }
+        ];
+        const resp = await createHostConfigData(hostConfigDataRecords);
+        expect(resp.count).toEqual(2);
+        await removeHostConfigData(undefined, ACCOUNT_ID);
+    });
+
+    it('should list Host config data records', async () => {
+        const hostConfigDataRecords = [
+            {
+                resource_id: 'i-1234567890abcdef0',
+                account_id: ACCOUNT_ID,
+                credentials_id: DEFAULT_AWS_CREDENTIALS_ID,
+                region: 'us-east-1',
+                database_instance_id: 'sql-server-broker-uuid',
+                timestamp: new Date(),
+                last_updated: new Date(),
+                config_data: {
+                    volumes: [
+                        {
+                            name: 'vol_murali_clone1',
+                            'thin-provision': true,
+                            'space-guarantee': 'none',
+                            autosize: 'off',
+                            'autosize-mode': 'off',
+                            'fractional-reserve': 0,
+                            'snapshot-policy': 'default',
+                            'snapshot-reserve': 5,
+                            'snapshot-autodelete': false,
+                            'tiering-policy': 'none',
+                            'tiering-min-cooling-days': null
+                        },
+                        {
+                            name: 'rranga_clone',
+                            'thin-provision': true,
+                            'space-guarantee': 'none',
+                            autosize: 'off',
+                            'autosize-mode': 'off',
+                            'fractional-reserve': 0,
+                            'snapshot-policy': 'default',
+                            'snapshot-reserve': 5,
+                            'snapshot-autodelete': false,
+                            'tiering-policy': 'auto',
+                            'tiering-min-cooling-days': 31
+                        }
+                    ]
+                },
+                config_data_type: 'CONFIG'
+            },
+            {
+                resource_id: 'i-1234567890abcdef0',
+                account_id: ACCOUNT_ID,
+                credentials_id: DEFAULT_AWS_CREDENTIALS_ID,
+                region: 'us-east-1',
+                database_instance_id: 'sql-server-broker-uuid',
+                timestamp: new Date(),
+                last_updated: new Date(),
+                config_data: {
+                    volumes: [
+                        {
+                            name: 'vol_murali_clone1',
+                            'thin-provision': true,
+                            'space-guarantee': 'none',
+                            autosize: 'off',
+                            'autosize-mode': 'off',
+                            'fractional-reserve': 0,
+                            'snapshot-policy': 'default',
+                            'snapshot-reserve': 5,
+                            'snapshot-autodelete': false,
+                            'tiering-policy': 'none',
+                            'tiering-min-cooling-days': null
+                        },
+                        {
+                            name: 'rranga_clone',
+                            'thin-provision': true,
+                            'space-guarantee': 'none',
+                            autosize: 'off',
+                            'autosize-mode': 'off',
+                            'fractional-reserve': 0,
+                            'snapshot-policy': 'default',
+                            'snapshot-reserve': 5,
+                            'snapshot-autodelete': false,
+                            'tiering-policy': 'auto',
+                            'tiering-min-cooling-days': 31
+                        }
+                    ]
+                },
+                config_data_type: 'PERFORMANCE'
+            }
+        ];
+        await createHostConfigData(hostConfigDataRecords);
+        const resp = await listHostConfigData(ACCOUNT_ID);
+        expect(resp.length).toEqual(2);
+        await removeHostConfigData(undefined, ACCOUNT_ID);
+    });
+
+    it('should remove Host config data records', async () => {
+        const hostConfigDataRecords = [
+            {
+                resource_id: 'i-1234567890abcdef0',
+                account_id: ACCOUNT_ID,
+                credentials_id: DEFAULT_AWS_CREDENTIALS_ID,
+                region: 'us-east-1',
+                database_instance_id: 'sql-server-broker-uuid',
+                timestamp: new Date(),
+                last_updated: new Date(),
+                config_data: {
+                    volumes: [
+                        {
+                            name: 'vol_murali_clone1',
+                            'thin-provision': true,
+                            'space-guarantee': 'none',
+                            autosize: 'off',
+                            'autosize-mode': 'off',
+                            'fractional-reserve': 0,
+                            'snapshot-policy': 'default',
+                            'snapshot-reserve': 5,
+                            'snapshot-autodelete': false,
+                            'tiering-policy': 'none',
+                            'tiering-min-cooling-days': null
+                        },
+                        {
+                            name: 'rranga_clone',
+                            'thin-provision': true,
+                            'space-guarantee': 'none',
+                            autosize: 'off',
+                            'autosize-mode': 'off',
+                            'fractional-reserve': 0,
+                            'snapshot-policy': 'default',
+                            'snapshot-reserve': 5,
+                            'snapshot-autodelete': false,
+                            'tiering-policy': 'auto',
+                            'tiering-min-cooling-days': 31
+                        }
+                    ]
+                },
+                config_data_type: 'CONFIG'
+            },
+            {
+                resource_id: 'i-1234567890abcdef0',
+                account_id: ACCOUNT_ID,
+                credentials_id: DEFAULT_AWS_CREDENTIALS_ID,
+                region: 'us-east-1',
+                database_instance_id: 'sql-server-broker-uuid',
+                timestamp: new Date(),
+                last_updated: new Date(),
+                config_data: {
+                    volumes: [
+                        {
+                            name: 'vol_murali_clone1',
+                            'thin-provision': true,
+                            'space-guarantee': 'none',
+                            autosize: 'off',
+                            'autosize-mode': 'off',
+                            'fractional-reserve': 0,
+                            'snapshot-policy': 'default',
+                            'snapshot-reserve': 5,
+                            'snapshot-autodelete': false,
+                            'tiering-policy': 'none',
+                            'tiering-min-cooling-days': null
+                        },
+                        {
+                            name: 'rranga_clone',
+                            'thin-provision': true,
+                            'space-guarantee': 'none',
+                            autosize: 'off',
+                            'autosize-mode': 'off',
+                            'fractional-reserve': 0,
+                            'snapshot-policy': 'default',
+                            'snapshot-reserve': 5,
+                            'snapshot-autodelete': false,
+                            'tiering-policy': 'auto',
+                            'tiering-min-cooling-days': 31
+                        }
+                    ]
+                },
+                config_data_type: 'PERFORMANCE'
+            }
+        ];
+        await createHostConfigData(hostConfigDataRecords);
+        const resp = await listHostConfigData(ACCOUNT_ID);
+        const configIds = resp.map(config => config.id);
+        const removeResp = await removeHostConfigData(configIds);
+        expect(removeResp.count).toBeGreaterThan(1);
+    });
+});
