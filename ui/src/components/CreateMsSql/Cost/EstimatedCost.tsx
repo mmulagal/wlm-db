0bc6603205784f5646ee017c714b26f680850d08
Sonam Sonam <sonamy@netapp.com>
2024-09-12 15:25:57 +0530

    EBS text change DBS-2983

--- ui/src/components/CreateMsSql/Cost/EstimatedCost.tsx
+++ ui/src/components/CreateMsSql/Cost/EstimatedCost.tsx
@@ -453,7 +453,7 @@ const EstimatedCost = () => {
                                 </Typography>
                                 <div className={styles.secondRow}>
                                     <Typography variant="Regular_14">
-                                        {GENERAL.TYPE}: {GENERAL.EBS}
+                                        {GENERAL.TYPE}: {GENERAL.ELASTIC_BLOCK_STORE}
                                     </Typography>
                                     <div className={styles.sizeRow}>
                                         <Typography variant="Regular_14">
@@ -462,7 +462,7 @@ const EstimatedCost = () => {
                                     </div>
                                     <div className={styles.sizeRow}>
                                         <Typography variant="Regular_14">
-                                            {GENERAL.TYPE}:
+                                            {GENERAL.VOLUME_TYPE}:
                                             {' ' + data?.data?.ebsStorage?.ebsBreakdownByVolumeType?.[0]?.volumeType}
                                         </Typography>
                                     </div>
