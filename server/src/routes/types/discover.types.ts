5b968af8b6e26c8fa5aa21739b9e0bc482ada62a
Himanshu Tyagi <htyagi@netapp.com>
2024-09-16 19:40:09 +0530

    DBS-3889: Add 'file System Storage Type' field to GET /mssql/instances and GET /discovery API Response

--- server/src/routes/types/discover.types.ts
+++ server/src/routes/types/discover.types.ts
@@ -105,7 +105,10 @@ const SqlServerInstanceInfo = Type.Object({
                 svmId: Type.Optional(
                     Type.String({ description: 'ID of Storage Virtual Machine, if underlying storage is FSx ONTAP' })
                 ),
-                protocol: Type.Optional(Type.String({ description: 'Data sharing protocol, iSCSI or SMB' }))
+                protocol: Type.Optional(Type.String({ description: 'Data sharing protocol, iSCSI or SMB' })),
+                fileSystemStorageType: Type.Optional(
+                    Type.String({ description: 'File system storage type, SSD or HDD' })
+                )
             })
         )
     ),
