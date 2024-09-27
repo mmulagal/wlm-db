89996956a54e50633acd0193771ed566ae68158d
mshreyas <mshreyas@netapp.com>
2024-09-27 11:59:21 +0530

    DBS-3880 deployment audit log resource id fix

--- server/src/operations/deployment-operations.ts
+++ server/src/operations/deployment-operations.ts
@@ -639,7 +639,7 @@ async function deployStackOrCreateTemplateURL(
         tags,
         triggeredFrom
     });
-
+    updateLongRunningAuditGroup(undefined, undefined, sqlConfiguration?.sqlServerName);
     const { workloadInstanceType } = ec2Configuration;
     const { sqlServerName, sqlAmiName, sqlCollation } = sqlConfiguration;
     const { databaseSize, fsxVolThroughput, fsxIOPS } = fsxConfiguration;
