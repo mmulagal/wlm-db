75ed672aceb9986ee28cf5885f57102d2155527d
Himanshu Tyagi <htyagi@netapp.com>
2024-10-15 13:55:52 +0530

    DBS-4032: Terraform - Standalone deployment with new SG throws error Invalid value for variable ontap_security_group_id

--- server/resources/mssql/scripts/common/Restart-Computer.ps1
+++ server/resources/mssql/scripts/common/Restart-Computer.ps1
@@ -2,7 +2,7 @@
 param(
     [Parameter(Mandatory=$false)]
     [string]$Count # used in terraform ps scripts to distinguish between the restart commands
-}
+)
 
 Start-Transcript -Path C:\cfn\log\Restart-Computer.ps1.txt -Append
 $ErrorActionPreference = "SilentlyContinue"
