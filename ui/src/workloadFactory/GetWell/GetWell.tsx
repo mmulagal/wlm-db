3e1c3b3ba3c48eacdad7e012e9114f0225b4347f
Yadav, Sonam <Sonam.Sonam@netapp.com>
2024-10-17 14:57:45 +0530

    get well refresh api

--- ui/src/workloadFactory/GetWell/GetWell.tsx
+++ ui/src/workloadFactory/GetWell/GetWell.tsx
@@ -188,7 +188,7 @@ const GetWell = () => {
                         {!optimizePrintState &&
                             (loading ? (
                                 <div className={styles.refreshIconDisable}>
-                                    <Spinner isLarge />
+                                    <RefreshIcon />
                                 </div>
                             ) : (
                                 <Popover
