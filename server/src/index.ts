e2709e3106ffa99b7d1ad3c4b362b4c0649f1652
krithib <krithib@netapp.com>
2024-10-20 12:56:54 +0530

    Print all enviornment variables

--- server/src/index.ts
+++ server/src/index.ts
@@ -72,7 +72,7 @@ import { resetCache } from './utils/cache';
 const logger = getLogger();
 const accessLogger = getLogger('access');
 
-logger.info(`Env variable ${process.env.REDIS_ENDPOINT}.`);
+logger.info(`Env variable ${process.env}.`);
 logger.info(`Redis URL ${REDIS_URL}.`);
 
 const { verifyToken, authorizeJwt } = jwtOperation;
