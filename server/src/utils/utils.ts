8a393f772b7e5cefbe9ff07296765d9dce6172ca
krithib <krithib@netapp.com>
2024-10-22 19:18:18 +0530

    DBS-4107: Correcting redis connection

--- server/src/utils/utils.ts
+++ server/src/utils/utils.ts
@@ -34,9 +34,9 @@ import {
     HOURS_IN_MONTH,
     DEFAULT_MSSQL_INSTANCE_NAME,
     DEFAULT_INSTANCE_NAME,
-    REDIS_URL,
     SECRETS,
-    DatabaseTypes
+    DatabaseTypes,
+    REDIS_URL
 } from './consts';
 
 import getLogger, { hideSecretsValues } from './logger';
@@ -661,11 +661,17 @@ const retryWithDelay = async (fn: any, retries = 3, interval = 5000, finalErr = 
 
 function getRedisDetails() {
     logger.info('in getRedisDetails');
+    let host = '127.0.0.1';
+    let port = '6379';
+    try {
+        [host, port] = REDIS_URL.split(':');
+    } catch (e: any) {
+        logger.error(`Unable to fetch redis host and port from ${REDIS_URL}. Error: ${e}.`);
+    }
     return {
-        connection: {
-            url: REDIS_URL,
-            password: SECRETS.REDIS_PASSWORD
-        }
+        host,
+        port: Number(port),
+        password: SECRETS.REDIS_PASSWORD
     };
 }
 
