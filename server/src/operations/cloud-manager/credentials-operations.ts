1f9895cdde9c69d0813ef09cfa161057d46fd1ea
mshreyas <mshreyas@netapp.com>
2024-10-22 17:59:55 +0530

    update

--- server/src/operations/cloud-manager/credentials-operations.ts
+++ server/src/operations/cloud-manager/credentials-operations.ts
@@ -1,6 +1,6 @@
 import createError from 'http-errors';
 import { isEmpty } from 'lodash-es';
-import { getAllWfCredentials } from '../../lib/cloud-manager/credentials';
+import { getAllWfCredentials, getWfCredentialDetails, wfCredentials } from '../../lib/cloud-manager/credentials';
 import { CredentialsResponseType } from '../../routes/types/credentials.types';
 import getLogger from '../../utils/logger';
 
@@ -64,29 +64,19 @@ async function getCredentialsDetails(credentialsId: string, accountId?: string) 
         throw new Error('Credentials id is invalid');
     }
     try {
-        const metadata = { arn: '' };
+        const {
+            credentials: { accessKeyId, secretAccessKey, sessionToken },
+            metadata
+        } = (await getWfCredentialDetails(credentialsId, accountId)) as wfCredentials;
+
         return {
             credentials: {
-                accessKey: 'AKIAWYGBM3V5ULQD2PX5',
-                secretKey: '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',
-                sessionId: ''
+                accessKey: accessKeyId,
+                secretKey: secretAccessKey,
+                sessionId: sessionToken
             },
             metadata
         };
-
-        // const {
-        //     credentials: { accessKeyId, secretAccessKey, sessionToken },
-        //     metadata
-        // } = (await getWfCredentialDetails(credentialsId, accountId)) as wfCredentials;
-
-        // return {
-        //     credentials: {
-        //         accessKey: accessKeyId,
-        //         secretKey: secretAccessKey,
-        //         sessionId: sessionToken
-        //     },
-        //     metadata
-        // };
     } catch (error) {
         const errMsg = `Failed to fetch credentials. ${error}`;
         logger.error(errMsg);
