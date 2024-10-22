eacc6639e6faa7dc2e41a3c6a29d5bf01cd1deb1
krithib <krithib@netapp.com>
2024-10-22 13:00:12 +0530

    DBS-4111: Storage tier assessment wrongly flagging instance considering other volumes of the system

--- server/src/operations/drift-assessment.ts
+++ server/src/operations/drift-assessment.ts
@@ -279,6 +279,7 @@ async function initiateStorageAssessmentCollection(
             ?.map(i => i?.volumeRecords)
             .flat() || [];
     instanceRecord.mappedVolumesUuids = volumeRecords.map(volume => volume.uuid as string);
+    instanceRecord.mappedVolumeNames = volumeRecords.map(volume => volume.name as string);
 
     instanceRecord.mappedLunNames =
         Object.values(instanceVolumeMapping)
