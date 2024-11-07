48e5b11dbd2078b10c13396959900472acf5ea33
Himanshu Tyagi <htyagi@netapp.com>
2024-11-07 15:43:35 +0530

    DBS-4216: Add Amazon Linux AMI Filtering for pgsql deployment

--- server/src/routes/types/aws.types.ts
+++ server/src/routes/types/aws.types.ts
@@ -79,7 +79,7 @@ const VpcListResponse = Type.Object({
 });
 
 // AMI Request and Response
-const OS_TYPES = Type.Union([Type.Literal('windows')]);
+const OS_TYPES = Type.Union([Type.Literal('windows'), Type.Literal('amazon-linux')]);
 const DB_TYPES = Type.Union([Type.Literal('sql')]);
 
 const AmiQueryString = Type.Object({
