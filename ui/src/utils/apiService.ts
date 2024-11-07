c3efa8051ef28641d8f2b1f97aac17fa3b158923
U, Srigowri <Srigowri.U@netapp.com>
2024-11-07 14:24:46 -0800

    DBS-3983 - updated UI endpoints

--- ui/src/utils/apiService.ts
+++ ui/src/utils/apiService.ts
@@ -854,12 +854,12 @@ export const getWellApi = createApi({
         return {
             getMssqlAssessmentData: builder.mutation({
                 query: ({ credentialId, regionId, databaseHostId, instanceId }) => ({
-                    url: `v1/mssql/credentials/${credentialId}/regions/${regionId}/database-hosts/${databaseHostId}/database-instances/${instanceId}/drift-assessment?fields=storage,compute`
+                    url: `v1/mssql/credentials/${credentialId}/regions/${regionId}/database-hosts/${databaseHostId}/database-instances/${instanceId}/continuous-assessment?fields=storage,compute`
                 })
             }),
             optimizeStorageConfig: builder.mutation({
                 query: ({ credentialId, regionId, databaseHostId, instanceId, payload }) => ({
-                    url: `v1/mssql/credentials/${credentialId}/regions/${regionId}/database-hosts/${databaseHostId}/database-instances/${instanceId}/drift-assessment/optimize`,
+                    url: `v1/mssql/credentials/${credentialId}/regions/${regionId}/database-hosts/${databaseHostId}/database-instances/${instanceId}/continuous-assessment/optimize/storage`,
                     method: 'POST',
                     body: payload
                 })
