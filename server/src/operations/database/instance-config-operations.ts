0d2e2f8e76d788aca7916b730577ea6661ca202b
krithib <krithib@netapp.com>
2024-11-11 18:27:30 +0530

    DBS-4078: Review comments

--- server/src/operations/database/instance-config-operations.ts
+++ server/src/operations/database/instance-config-operations.ts
@@ -9,16 +9,25 @@ const logger = getLogger();
 async function purgeOlderAssessmentRecords() {
     logger.info('Purging older assessment records');
     const instanceAssessmentRecords = await listDatabaseInstanceConfigData();
-    instanceAssessmentRecords.forEach(async instanceAssessmentRecord => {
-        await removeAllButLatestDatabaseInstanceConfigData(
-            instanceAssessmentRecord.account_id,
-            instanceAssessmentRecord.region,
-            instanceAssessmentRecord.credentials_id,
-            instanceAssessmentRecord.resource_id,
-            instanceAssessmentRecord.database_instance_id,
-            instanceAssessmentRecord.creation_time
-        );
-    });
+    instanceAssessmentRecords.forEach(
+        async ({
+            account_id: accountId,
+            region,
+            credentials_id: credentialsId,
+            resource_id: resourceId,
+            database_instance_id: databaseInstanceId,
+            creation_time: creationTime
+        }) => {
+            await removeAllButLatestDatabaseInstanceConfigData(
+                accountId,
+                region,
+                credentialsId,
+                resourceId,
+                databaseInstanceId,
+                creationTime
+            );
+        }
+    );
 }
 
 export { purgeOlderAssessmentRecords };
