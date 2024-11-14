0d9d4224586d0c1685a3dbaa5c1173d5c3aec32e
U, Srigowri <Srigowri.U@netapp.com>
2024-11-13 21:55:52 -0800

    DBS-4056 - describe address scope

--- server/test/lib/aws/auto-scaling.test.ts
+++ server/test/lib/aws/auto-scaling.test.ts
@@ -5,6 +5,7 @@ import '../../simulator/scopes/cloud-manager/cloud-manager-tenancy-scope';
 import '../../simulator/scopes/cloud-manager/workload-factory-auth-scope';
 import '../../simulator/scopes/aws/ec2-scope';
 import '../../simulator/scopes/opentelemetry-scope';
+import '../../simulator/scopes/aws/auto-scaling-scope';
 import { ACCOUNT_ID, DEFAULT_AWS_REGION } from '../../utils/consts';
 
 describe('Auto scaling Lib', () => {
