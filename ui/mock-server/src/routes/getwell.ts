4aeb3a54b3e004e32ecd4012d77a45c5eff6aeb8
Ayush <payush@netapp.com>
2024-11-15 09:57:48 +0530

    storage sizing optimize API integration

--- ui/mock-server/src/routes/getwell.ts
+++ ui/mock-server/src/routes/getwell.ts
@@ -3,22 +3,40 @@ import GetWellJson from '../data/getWell.json';
 
 const router = require('express').Router();
 
-router.get(`${BASE_URL}/v1/mssql/credentials/:credentialsId/regions/:region/database-hosts/:databaseHostId/database-instances/:databaseInstanceId/assessment`, async (req: {}, res: any) => {
-    setTimeout(() => {
-        generateResponse(res, 200, GetWellJson);
-    }, 20);
-});
+router.get(
+    `${BASE_URL}/v1/mssql/credentials/:credentialsId/regions/:region/database-hosts/:databaseHostId/database-instances/:databaseInstanceId/assessment`,
+    async (req: {}, res: any) => {
+        setTimeout(() => {
+            generateResponse(res, 200, GetWellJson);
+        }, 20);
+    }
+);
 
-router.post(`${BASE_URL}/v1/mssql/credentials/:credentialsId/regions/:region/database-hosts/:databaseHostId/database-instances/:databaseInstanceId/optimize/storage-configuration`, async (req: {}, res: any) => {
-    setTimeout(() => {
-        generateResponse(res, 202, { jobId: '1234' });
-    }, 2000);
-});
+router.post(
+    `${BASE_URL}/v1/mssql/credentials/:credentialsId/regions/:region/database-hosts/:databaseHostId/database-instances/:databaseInstanceId/optimize/storage-configuration`,
+    async (req: {}, res: any) => {
+        setTimeout(() => {
+            generateResponse(res, 202, { jobId: '1234' });
+        }, 2000);
+    }
+);
 
-router.post(`${BASE_URL}/v1/mssql/credentials/:credentialsId/regions/:region/database-hosts/:databaseHostId/database-instances/:databaseInstanceId/optimize/compute`, async (req: {}, res: any) => {
-    setTimeout(() => {
-        generateResponse(res, 202, { jobId: '1234' });
-    }, 2000);
-});
+router.post(
+    `${BASE_URL}/v1/mssql/credentials/:credentialsId/regions/:region/database-hosts/:databaseHostId/database-instances/:databaseInstanceId/optimize/storage-sizing`,
+    async (req: {}, res: any) => {
+        setTimeout(() => {
+            generateResponse(res, 202, { jobId: '1234' });
+        }, 2000);
+    }
+);
+
+router.post(
+    `${BASE_URL}/v1/mssql/credentials/:credentialsId/regions/:region/database-hosts/:databaseHostId/database-instances/:databaseInstanceId/optimize/compute`,
+    async (req: {}, res: any) => {
+        setTimeout(() => {
+            generateResponse(res, 202, { jobId: '1234' });
+        }, 2000);
+    }
+);
 
 export default router;
--- ui/src/workloadFactory/GetWell/StorageCardComponent/StorageCardComponent.tsx
+++ ui/src/workloadFactory/GetWell/StorageCardComponent/StorageCardComponent.tsx
@@ -20,7 +20,8 @@ import { setSelectedHeaderTab } from '../../../store/workloadFactory/inventoryV2
 import {
     useLazyGetSubTaskListQuery,
     useOptimizeComputeConfigMutation,
-    useOptimizeStorageConfigMutation
+    useOptimizeStorageConfigMutation,
+    useOptimizeStorageSizingMutation
 } from '../../../utils/apiService';
 import TooltipComponent from '../../../common/TooltipComponent/TooltipComponent';
 import { ReactComponent as TooltipIcon } from '../../../assets/tooltipGrey.svg';
@@ -37,6 +38,7 @@ const StorageCardComponent = ({ cardData, optimizePrintState, type }: any) => {
     const optimizingData = useAppSelector(state => state.getWellOptimize.optimizingData);
     const [optimizeStorageConfig] = useOptimizeStorageConfigMutation();
     const [optimizeComputeConfig] = useOptimizeComputeConfigMutation();
+    const [optimizeStorageSizing] = useOptimizeStorageSizingMutation();
     const [getJobDetailApi] = useLazyGetSubTaskListQuery();
 
     const [disableText, setDisableText] = useState(false);
@@ -200,9 +202,15 @@ const StorageCardComponent = ({ cardData, optimizePrintState, type }: any) => {
     const callOptimizeApi = (type: any) => {
         let payload = {};
         let apiCall = null;
+        const state = store.getState();
         if (type === GENERAL.COMPUTE_RIGHTSIZING) {
             apiCall = optimizeComputeConfig;
-            const state = store.getState();
+            const { selectedRecommendedInstance } = state.getWellOptimize;
+            payload = {
+                instanceType: selectedRecommendedInstance?.value
+            };
+        } else if(type === 'Storage tier' || type === 'Log drive size' || type === 'File system headroom' || type === 'TempDB drive size') {
+            apiCall = optimizeStorageSizing;
             const { selectedRecommendedInstance } = state.getWellOptimize;
             payload = {
                 instanceType: selectedRecommendedInstance?.value
@@ -291,10 +299,7 @@ const StorageCardComponent = ({ cardData, optimizePrintState, type }: any) => {
                 }}
                 customClass={styles.colorSet}
                 hidePrimaryButton={
-                    type === 'Storage tier' ||
-                    type === 'User data files (.mdf) placement' ||
-                    type === 'Log files (.ldf) placement' ||
-                    type === 'TempDB placement'
+                    type === 'User data files (.mdf) placement'
                 }
             />
         );
@@ -365,7 +370,7 @@ const StorageCardComponent = ({ cardData, optimizePrintState, type }: any) => {
                             height="30px"
                         >
                             <div>
-                                <DsButton variant="secondary" isDisabled={true}>
+                                <DsButton variant="secondary">
                                     Optimize
                                 </DsButton>
                             </div>
@@ -381,7 +386,7 @@ const StorageCardComponent = ({ cardData, optimizePrintState, type }: any) => {
                         height="50px"
                     >
                         <div>
-                            <DsButton variant="secondary" isDisabled={true}>
+                            <DsButton variant="secondary">
                                 Optimize
                             </DsButton>
                         </div>
