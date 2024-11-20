c80fc1d72f4980570e50af8d07b1cc9632972b65
Ayush <payush@netapp.com>
2024-11-20 10:48:27 +0530

    DBS-4294: retry for rate exceeded error in pgsql deploy

--- ui/src/utils/apiService.ts
+++ ui/src/utils/apiService.ts
@@ -86,7 +86,9 @@ const dynamicBaseQuery: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryE
         const result: any = await rawBaseQuery(adjustedArgs, api, extraOptions);
         // For deploy API and discover API if it gets rate exceeded than retry that API
         if (
-            (api.endpoint === 'deploySqlTemplate' || api.endpoint === 'discoverHosts') &&
+            (api.endpoint === 'deploySqlTemplate' ||
+                api.endpoint === 'discoverHosts' ||
+                api.endpoint === 'deployPgsqlTemplate') &&
             result.error?.data &&
             result.error.data?.message.toLowerCase().includes(API_ERRORS.RATE_EXCEEDED)
         ) {
@@ -1000,5 +1002,10 @@ export const {
     useGetManualViewCalculationsMutation
 } = exploreSavingsApi;
 
-export const { useGetMssqlAssessmentDataMutation, useOptimizeStorageConfigMutation, useOptimizeComputeConfigMutation, useOptimizeStorageSizingMutation, useOptimizeOperatingSystemMutation } =
-    getWellApi;
+export const {
+    useGetMssqlAssessmentDataMutation,
+    useOptimizeStorageConfigMutation,
+    useOptimizeComputeConfigMutation,
+    useOptimizeStorageSizingMutation,
+    useOptimizeOperatingSystemMutation
+} = getWellApi;
