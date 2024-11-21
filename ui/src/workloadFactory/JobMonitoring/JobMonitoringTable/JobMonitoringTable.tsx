d784952904f671276a275d5504c6ba8343835a4a
rpanwar@netapp.com <rpanwar@netapp.com>
2024-11-21 10:50:00 +0530

    DBS-4288 unblocking subjobs for assessment

--- ui/src/workloadFactory/JobMonitoring/JobMonitoringTable/JobMonitoringTable.tsx
+++ ui/src/workloadFactory/JobMonitoring/JobMonitoringTable/JobMonitoringTable.tsx
@@ -263,28 +263,23 @@ const JobMonitoringTable = () => {
                 return (
                     <>
                         <div className={`${styles.statusbar} ${styles[statusType]}`}>&nbsp;</div>
-                        {rowData?.type !== JOB_MONITORING_TYPE.ASSESSMENT ? (
-                            <div
-                                className={
-                                    isIntegrityCheckJob ? `${styles.arrow} ${styles['arrow-disabled']}` : styles.arrow
-                                }
-                            >
-                                <ArrowIcon
-                                    className={currentRowState?.isExpanded ? styles['arrow-down'] : ''}
-                                    onClick={(e: any) => {
-                                        e.stopPropagation();
-                                        if (!isIntegrityCheckJob) {
-                                            getSubJobsData(rowData?.id); // calling sub jobs api on expand click
-                                            expandTableRow(updateRowState, rowData, currentRowState, rowsState);
-                                        }
-                                    }}
-                                />
-                            </div>
-                        ) : (
-                            <div className={`${styles.arrow} ${styles['arrow-disabled']}`}>
-                                <ArrowIcon className={styles['arrow-disable']} />
-                            </div>
-                        )}
+
+                        <div
+                            className={
+                                isIntegrityCheckJob ? `${styles.arrow} ${styles['arrow-disabled']}` : styles.arrow
+                            }
+                        >
+                            <ArrowIcon
+                                className={currentRowState?.isExpanded ? styles['arrow-down'] : ''}
+                                onClick={(e: any) => {
+                                    e.stopPropagation();
+                                    if (!isIntegrityCheckJob) {
+                                        getSubJobsData(rowData?.id); // calling sub jobs api on expand click
+                                        expandTableRow(updateRowState, rowData, currentRowState, rowsState);
+                                    }
+                                }}
+                            />
+                        </div>
                     </>
                 );
             }
@@ -410,7 +405,7 @@ const JobMonitoringTable = () => {
                 );
             }
         },
-        
+
         lastColDetails()
     ];
 
