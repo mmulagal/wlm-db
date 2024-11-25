226a44048b41bb91521e5a535765686763d8aff3
U, Srigowri <Srigowri.U@netapp.com>
2024-11-24 20:45:58 -0800

    DBS-4379 - Invalid permission error on automate role - creds ID in EC2 client initialization

--- server/src/utils/consts.ts
+++ server/src/utils/consts.ts
@@ -1482,7 +1482,9 @@ const PERMISSIONS_TO_IGNORE_FOR_DEPLOYMENT = [
     'compute-optimizer:GetEffectiveRecommendationPreferences',
     'compute-optimizer:GetEC2InstanceRecommendations',
     'autoscaling:DescribeAutoScalingGroups',
-    'autoscaling:DescribeAutoScalingInstances'
+    'autoscaling:DescribeAutoScalingInstances',
+    'ec2:DescribeAddresses',
+    'ec2:ModifyInstanceAttribute'
 ];
 const DEMO_AWS_ACCOUNT_ID = randomize('0', 12);
 const DEMO_DEFAULT_REGION = 'us-east-1';
