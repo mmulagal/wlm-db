a2c79fba1c05f33cd5cf882a4e127068f5281280
rpanwar@netapp.com <rpanwar@netapp.com>
2024-11-26 16:18:08 +0530

    DBS-4370 Added region and cred for assessment subjobs

--- ui/src/workloadFactory/JobMonitoring/SubJobTable/SubJobTable.tsx
+++ ui/src/workloadFactory/JobMonitoring/SubJobTable/SubJobTable.tsx
@@ -8,7 +8,7 @@ import { ReactComponent as ErrorIcon } from '../../../assets/error-icon.svg';
 import { ReactComponent as NoDataIcon } from '../../../assets/ic_file.svg';
 import TaskTable from '../TaskTable/TaskTable';
 import CommonStyles from '../../../utils/CommonStyles.module.scss';
-import { CREATE_RESOURCE, JOB_MONITORING_STATUS, WLF_TABS } from '../../../utils/consts';
+import { CREATE_RESOURCE, JOB_MONITORING_STATUS, JOB_MONITORING_TYPE, WLF_TABS } from '../../../utils/consts';
 import {
     expandTableRow,
     formatDateWithTime,
@@ -51,7 +51,7 @@ const SubJobTable = ({ jobId, statusType }: any) => {
         return <TaskTable taskList={rowData?.subJobs || []} />;
     }, []);
 
-    const navigateToContinuosOptimization = (message: string) => {
+    const navigateToContinuosOptimization = (message: string, rowData: any) => {
         const splitMessage = message.split(';');
 
         // Extract the JSON part of the split message
@@ -68,8 +68,14 @@ const SubJobTable = ({ jobId, statusType }: any) => {
 
         dispatch(setSelectedHeaderTab(WLF_TABS.OPTIMIZE));
         dispatch(selectedTabSelection(WLF_TABS.OPTIMIZE));
-        dispatch(setCredIdFromJM(subJobsData?.credentialsId));
-        dispatch(setRegionFromJM(subJobsData?.region?.code));
+        if (subJobsData?.type === JOB_MONITORING_TYPE.ASSESSMENT) {
+            dispatch(setCredIdFromJM(rowData?.credentialsId));
+            dispatch(setRegionFromJM(rowData?.region?.code));
+        } else {
+            dispatch(setCredIdFromJM(subJobsData?.credentialsId));
+            dispatch(setRegionFromJM(subJobsData?.region?.code));
+        }
+
         dispatch(setLandingFrom(WLF_TABS.JOB_MONITORING));
         dispatch(setGwHostname(subJobsData?.resourceName));
         dispatch(setGwResourceId(resourceId));
@@ -117,7 +123,7 @@ const SubJobTable = ({ jobId, statusType }: any) => {
             isSortable: true,
             width: '676px',
             isSticky: true,
-            renderCell: (cellData: any) => {
+            renderCell: (cellData: any, rowData: any) => {
                 if (cellData.includes('databaseInstanceId') && cellData.includes('resourceId')) {
                     const splitMessage = cellData.split(';');
 
@@ -135,7 +141,7 @@ const SubJobTable = ({ jobId, statusType }: any) => {
                                 <Button
                                     variant="link"
                                     onClick={() => {
-                                        navigateToContinuosOptimization(cellData);
+                                        navigateToContinuosOptimization(cellData, rowData);
                                     }}
                                 >
                                     instance optimization dashboard
