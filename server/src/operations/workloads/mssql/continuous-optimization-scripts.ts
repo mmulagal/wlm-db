c93c4a54c1c611301cdbb456c90ad58940823c48
krithib <krithib@netapp.com>
2024-11-27 22:05:00 +0530

    DBS-4204: Access paths correction for physical and virtual drives

--- server/src/operations/workloads/mssql/continuous-optimization-scripts.ts
+++ server/src/operations/workloads/mssql/continuous-optimization-scripts.ts
@@ -256,12 +256,8 @@ const INSTANCE_DRIVE_DETAILS_TEMPLATE = (instance: string, sqlAuthEnabled: boole
         if($netappDataDrives -contains $dataDrive.dataDriveLetter) {
             $logDrive = $instanceAllLogDrivesSizes | Where-Object { $_.databaseName -eq $dataDrive.databaseName }
             if (($logDrive) -and ($netappLogDrives -contains $logDrive.logDriveLetter)) {
-                $dataAccessPath = ($dataDrive.dataDrivePath.Split("\\") | Select-Object -First 2) -join "\\"
-                $dataDrive | Add-Member -MemberType NoteProperty -Name "dataAccessPath" -Value $dataAccessPath
                 $dataDrive | Add-Member -MemberType NoteProperty -Name "logDriveLetter" -Value $logDrive.logDriveLetter 
                 $dataDrive | Add-Member -MemberType NoteProperty -Name "logDrivePath" -Value $logDrive.logDrivePath
-                $logAccessPath = ($logDrive.logDrivePath.Split("\\") | Select-Object -First 2) -join "\\"
-                $dataDrive | Add-Member -MemberType NoteProperty -Name "logAccessPath" -Value $logAccessPath
                 $dataDrive | Add-Member -MemberType NoteProperty -Name "logDriveTotalSizeMB" -Value $logDrive.logDriveTotalSizeMB
                 }
             } 
@@ -486,8 +482,14 @@ const STORAGE_CONFIGURATION_ASSESSMENT = (instanceRecord: WorkloadInstance) =>
                 $drive | Add-Member -MemberType NoteProperty -Name "svmName" -Value $logVolumeLunDetails.svmName
                 $drive | Add-Member -MemberType NoteProperty -Name "diskNumber" -Value $logVolumeLunDetails.diskNumber
                 $drive | Add-Member -MemberType NoteProperty -Name "diskSerialNumber" -Value $logVolumeLunDetails.lunSerialNumber
+                if($logVolumeLunDetails.accessPaths -and $logVolumeLunDetails.accessPaths.Count -gt 0) {
+                    $drive | Add-Member -MemberType NoteProperty -Name "logAccessPath" -Value $logVolumeLunDetails.accessPaths[0]
+                    }
                 }
-                
+            $dataVolumeLunDetails = $responseObject.data | Where-Object { $_.name -eq $drive.databaseName }
+            if($dataVolumeLunDetails -and $dataVolumeLunDetails.accessPaths -and $dataVolumeLunDetails.accessPaths.Count -gt 0) {
+                $drive | Add-Member -MemberType NoteProperty -Name "dataAccessPath" -Value $dataVolumeLunDetails.accessPaths[0]         
+                }   
             }
         
         foreach ($drive in $defaultTempDBDriveSize) {
