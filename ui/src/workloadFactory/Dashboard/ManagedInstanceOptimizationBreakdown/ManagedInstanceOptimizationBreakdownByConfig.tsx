322029819e61f9c144c1e8843b1cae6776adf94d
rpanwar@netapp.com <rpanwar@netapp.com>
2024-12-02 18:28:18 +0530

    DBS-4462 optimization breakdown by configs

--- ui/src/workloadFactory/Dashboard/ManagedInstanceOptimizationBreakdown/ManagedInstanceOptimizationBreakdownByConfig.tsx
+++ ui/src/workloadFactory/Dashboard/ManagedInstanceOptimizationBreakdown/ManagedInstanceOptimizationBreakdownByConfig.tsx
@@ -1,5 +1,7 @@
-import { DsTypography } from '@netapp/design-system';
+import { DsButton, DsTypography } from '@netapp/design-system';
 import styles from './ManagedInstanceOptimizationBreakdownByConfig.module.scss';
+import BarComponent from '../BarComponent/BarComponent';
+import SeparatorComponent from '../../../common/SeparatorComponent/SeparatorComponent';
 
 const ManagedInstanceOptimizationBreakdownByConfig = () => {
     return (
@@ -10,6 +12,208 @@ const ManagedInstanceOptimizationBreakdownByConfig = () => {
                 </DsTypography>
 
                 {/* {loading && <FlashingDotsLoader />} */}
+            </div>
+
+            <div className={styles.mainSection}>
+                <div className={`${styles.tile} ${styles.firstTile}`}>
+                    <BarComponent
+                        color="#5E8DCD"
+                        headingText="Storage tier"
+                        percentage={55}
+                        beforeOutOf={65}
+                        afterOutOf={120}
+                        bottomText="Optimized instances:"
+                        width="360px"
+                    />
+
+                    <SeparatorComponent variant="vertical" height="60px" />
+
+                    <div className={styles.buttonContainer}>
+                        <DsButton variant="secondary" isThin={true} onClick={() => {}}>
+                            Optimize
+                        </DsButton>
+                    </div>
+                </div>
+
+                <div className={styles.tile}>
+                    <BarComponent
+                        color="#5E8DCD"
+                        headingText="File system headroom"
+                        percentage={55}
+                        beforeOutOf={65}
+                        afterOutOf={120}
+                        bottomText="Optimized instances:"
+                        width="360px"
+                    />
+
+                    <SeparatorComponent variant="vertical" height="60px" />
+
+                    <div className={styles.buttonContainer}>
+                        <DsButton variant="secondary" isThin={true} onClick={() => {}}>
+                            Optimize
+                        </DsButton>
+                    </div>
+                </div>
+
+                <div className={styles.tile}>
+                    <BarComponent
+                        color="#5E8DCD"
+                        headingText="Log drive size "
+                        percentage={55}
+                        beforeOutOf={65}
+                        afterOutOf={120}
+                        bottomText="Optimized instances:"
+                        width="360px"
+                    />
+
+                    <SeparatorComponent variant="vertical" height="60px" />
+
+                    <div className={styles.buttonContainer}>
+                        <DsButton variant="secondary" isThin={true} onClick={() => {}}>
+                            Optimize
+                        </DsButton>
+                    </div>
+                </div>
+
+                <div className={styles.tile}>
+                    <BarComponent
+                        color="#5E8DCD"
+                        headingText="TempDB drive size"
+                        percentage={55}
+                        beforeOutOf={65}
+                        afterOutOf={120}
+                        bottomText="Optimized instances:"
+                        width="360px"
+                    />
+
+                    <SeparatorComponent variant="vertical" height="60px" />
+
+                    <div className={styles.buttonContainer}>
+                        <DsButton variant="secondary" isThin={true} onClick={() => {}}>
+                            Optimize
+                        </DsButton>
+                    </div>
+                </div>
+
+                <div className={styles.tile}>
+                    <BarComponent
+                        color="#5E8DCD"
+                        headingText="User data files (.mdf) "
+                        percentage={55}
+                        beforeOutOf={65}
+                        afterOutOf={120}
+                        bottomText="Optimized instances:"
+                        width="360px"
+                    />
+
+                    <SeparatorComponent variant="vertical" height="60px" />
+
+                    <div className={styles.buttonContainer}>
+                        <DsButton variant="secondary" isThin={true} onClick={() => {}}>
+                            Optimize
+                        </DsButton>
+                    </div>
+                </div>
+
+                <div className={styles.tile}>
+                    <BarComponent
+                        color="#5E8DCD"
+                        headingText="Log files (.ldf) "
+                        percentage={55}
+                        beforeOutOf={65}
+                        afterOutOf={120}
+                        bottomText="Optimized instances:"
+                        width="360px"
+                    />
+
+                    <SeparatorComponent variant="vertical" height="60px" />
+
+                    <div className={styles.buttonContainer}>
+                        <DsButton variant="secondary" isThin={true} onClick={() => {}}>
+                            Optimize
+                        </DsButton>
+                    </div>
+                </div>
+
+                <div className={styles.tile}>
+                    <BarComponent
+                        color="#5E8DCD"
+                        headingText="TempDB placement"
+                        percentage={55}
+                        beforeOutOf={65}
+                        afterOutOf={120}
+                        bottomText="Optimized instances:"
+                        width="360px"
+                    />
+
+                    <SeparatorComponent variant="vertical" height="60px" />
+
+                    <div className={styles.buttonContainer}>
+                        <DsButton variant="secondary" isThin={true} onClick={() => {}}>
+                            Optimize
+                        </DsButton>
+                    </div>
+                </div>
+
+                <div className={styles.tile}>
+                    <BarComponent
+                        color="#5E8DCD"
+                        headingText="ONTAP configuration"
+                        percentage={55}
+                        beforeOutOf={65}
+                        afterOutOf={120}
+                        bottomText="Optimized instances:"
+                        width="360px"
+                    />
+
+                    <SeparatorComponent variant="vertical" height="60px" />
+
+                    <div className={styles.buttonContainer}>
+                        <DsButton variant="secondary" isThin={true} onClick={() => {}}>
+                            Optimize
+                        </DsButton>
+                    </div>
+                </div>
+
+                <div className={styles.tile}>
+                    <BarComponent
+                        color="#5E8DCD"
+                        headingText="Operating system"
+                        percentage={55}
+                        beforeOutOf={65}
+                        afterOutOf={100}
+                        bottomText="Optimized instances:"
+                        width="360px"
+                    />
+
+                    <SeparatorComponent variant="vertical" height="60px" />
+
+                    <div className={styles.buttonContainer}>
+                        <DsButton variant="secondary" isThin={true} onClick={() => {}}>
+                            Optimize
+                        </DsButton>
+                    </div>
+                </div>
+
+                <div className={styles.tile}>
+                    <BarComponent
+                        color="#5E8DCD"
+                        headingText="Compute rightsizing"
+                        percentage={55}
+                        beforeOutOf={65}
+                        afterOutOf={100}
+                        bottomText="Optimized instances:"
+                        width="360px"
+                    />
+
+                    <SeparatorComponent variant="vertical" height="60px" />
+
+                    <div className={styles.buttonContainer}>
+                        <DsButton variant="secondary" isThin={true} onClick={() => {}}>
+                            Optimize
+                        </DsButton>
+                    </div>
+                </div>
             </div>
         </div>
     );
