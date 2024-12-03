648571e6dbec06cdabb8ace31c468504b00d8906
rpanwar@netapp.com <rpanwar@netapp.com>
2024-12-03 09:28:07 +0530

    DBS-4456 Explore savings button

--- ui/src/workloadFactory/Dashboard/PotentialSavings/PotentialSavings.tsx
+++ ui/src/workloadFactory/Dashboard/PotentialSavings/PotentialSavings.tsx
@@ -1,7 +1,18 @@
-import { DsTypography } from '@netapp/design-system';
+import { DsButton, DsTypography } from '@netapp/design-system';
 import styles from './PotentialSavings.module.scss';
+import { useDispatch } from 'react-redux';
+import { setSelectedHeaderTab } from '../../../store/workloadFactory/inventoryV2Slice';
+import { handleURL } from '../../../utils/utilityFunctions';
+import { useAppSelector } from '../../../store/storeHooks';
+import { WLF_TABS } from '../../../utils/consts';
 
 const PotentialSavings = () => {
+    const dispatch = useDispatch();
+    const { isWorkloadFactory } = useAppSelector(state => state?.auth);
+    const handleClick = (value: string) => {
+        dispatch(setSelectedHeaderTab(value));
+        handleURL(value, isWorkloadFactory);
+    };
     return (
         <div className={styles.potentialSavings}>
             <div className={styles.headSection}>
@@ -10,6 +21,13 @@ const PotentialSavings = () => {
                 </DsTypography>
 
                 {/* {loading && <FlashingDotsLoader />} */}
+
+                <div className={styles.rightSection}>
+                    {/* {loading && <FlashingDotsLoader />} */}
+                    <DsButton variant="secondary" isThin={true} onClick={() => handleClick(WLF_TABS.EXPLORE_SAVINGS)}>
+                        Explore savings
+                    </DsButton>
+                </div>
             </div>
         </div>
     );
