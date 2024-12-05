bbed0ef8e3f3ecaa151740f10958be73529f2d0f
rpanwar@netapp.com <rpanwar@netapp.com>
2024-12-05 15:00:10 +0530

    DBS-4398 Eslint issue build error fix

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
     base: './',
     plugins: [
         react(),
-        // eslint(),
+        eslint(),
         cssInjectedByJsPlugin(),
         commonjs(),
         viteTsconfigPaths(),
