eec346343c2ec9ab8766e575f40cd304b15c4f18
rpanwar@netapp.com <rpanwar@netapp.com>
2024-12-08 10:26:58 +0530

    DBS-4464 Potential Savings UI

--- ui/src/workloadFactory/Dashboard/PotentialSavings/PotentialSavings.tsx
+++ ui/src/workloadFactory/Dashboard/PotentialSavings/PotentialSavings.tsx
@@ -1,10 +1,14 @@
-import { DsButton, DsTypography } from '@netapp/design-system';
+import { DsButton, DsFlashingDotsLoader, DsTypography } from '@netapp/design-system';
 import styles from './PotentialSavings.module.scss';
 import { useDispatch } from 'react-redux';
 import { setSelectedHeaderTab } from '../../../store/workloadFactory/inventoryV2Slice';
-import { handleURL } from '../../../utils/utilityFunctions';
+import { formatNumberWithCustomComma, handleURL } from '../../../utils/utilityFunctions';
 import { useAppSelector } from '../../../store/storeHooks';
 import { WLF_TABS } from '../../../utils/consts';
+import ComparisonChartStack from '../../../ui-components/Charts/ComparionChartStack';
+import SeparatorComponent from '../../../common/SeparatorComponent/SeparatorComponent';
+import useResize from '../../../common/hooks/useResize';
+import { ReactComponent as PotentialSavingsImage } from '../../../assets/potential_savings.svg';
 
 const PotentialSavings = () => {
     const dispatch = useDispatch();
@@ -13,11 +17,17 @@ const PotentialSavings = () => {
         dispatch(setSelectedHeaderTab(value));
         handleURL(value, isWorkloadFactory);
     };
+    const loading = false;
+    const windowSize = useResize();
+    const noData = false;
     return (
         <div className={styles.potentialSavings}>
             <div className={styles.headSection}>
                 <DsTypography variant="Regular_16" className={styles.title}>
-                    Potential savings (Elastic Block Store (EBS) & FSx for Windows File Server)
+                    Potential savings
+                    {windowSize.width >= 1700 && (
+                        <span>&nbsp;(Elastic Block Store (EBS) & FSx for Windows File Server)</span>
+                    )}
                 </DsTypography>
 
                 {/* {loading && <FlashingDotsLoader />} */}
@@ -27,6 +37,66 @@ const PotentialSavings = () => {
                     <DsButton variant="secondary" isThin={true} onClick={() => handleClick(WLF_TABS.EXPLORE_SAVINGS)}>
                         Explore savings
                     </DsButton>
+                </div>
+            </div>
+
+            {windowSize.width < 1700 && (
+                <DsTypography style={{ margin: '20px 0 20px 40px' }} variant="Regular_13">
+                    (Elastic Block Store (EBS) & FSx for Windows File Server)
+                </DsTypography>
+            )}
+
+            <div className={styles.mainSection}>
+                <div className={styles.valueSection}>
+                    <div className={styles.subContent}>
+                        <div className={styles.loaderText}>
+                            <DsTypography variant="Regular_24" style={{ lineHeight: 'unset' }}>
+                                80
+                            </DsTypography>
+                            {loading && <DsFlashingDotsLoader />}
+                        </div>
+
+                        <DsTypography variant="Regular_14">EBS & FSxW instances</DsTypography>
+                    </div>
+
+                    <SeparatorComponent variant="horizontal" height="" />
+
+                    <div className={styles.subContent}>
+                        <div className={styles.loaderText}>
+                            <DsTypography variant="Regular_24" style={{ lineHeight: 'unset' }}>
+                                $9,200
+                            </DsTypography>
+                            {loading && <DsFlashingDotsLoader />}
+                        </div>
+
+                        <DsTypography variant="Regular_14">Potential savings</DsTypography>
+                    </div>
+
+                    <SeparatorComponent variant="horizontal" height="" />
+
+                    <div className={styles.subContent}>
+                        <div className={styles.loaderText}>
+                            <DsTypography variant="Regular_24" style={{ lineHeight: 'unset' }}>
+                                72%
+                            </DsTypography>
+                            {loading && <DsFlashingDotsLoader />}
+                        </div>
+                        <DsTypography variant="Regular_14">Savings percentage</DsTypography>
+                    </div>
+                </div>
+                <div className={styles.chartSection}>
+                    {noData && <PotentialSavingsImage />}
+                    {!noData && (
+                        <ComparisonChartStack
+                            data={[[3750], [6475, 6475]]}
+                            yTickFormatter={yValue => '$' + formatNumberWithCustomComma(Number(yValue), true)}
+                            height={300}
+                            colors={['chart-2', 'chart-3', 'chart-2']}
+                            categories={['FSx for ONTAP', 'Amazon Elastic Block Store', 'Amazon Elastic Block Store2']}
+                            tooltipHeading={['EBS', 'FSxW']}
+                            tooltipText={['Amazon Elastic Block Store', 'FSx for Windows']}
+                        />
+                    )}
                 </div>
             </div>
         </div>
