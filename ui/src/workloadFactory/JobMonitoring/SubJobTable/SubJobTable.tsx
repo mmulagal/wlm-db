37bde2e8ef05e14bcfaff228c2ba9e176b5f2f0c
rpanwar@netapp.com <rpanwar@netapp.com>
2025-01-02 15:24:32 +0530

    DBS-4676 removed duplicate warning

--- ui/src/workloadFactory/JobMonitoring/SubJobTable/SubJobTable.tsx
+++ ui/src/workloadFactory/JobMonitoring/SubJobTable/SubJobTable.tsx
@@ -215,23 +215,6 @@ const SubJobTable = ({ jobId, statusType }: any) => {
                                     <Warning />
                                 ))}
                             {cellData === JOB_MONITORING_STATUS.IN_PROGRESS && <InProgress />}
-                            {cellData === JOB_MONITORING_STATUS.WARNING &&
-                                (rowData?.error ? (
-                                    <Popover
-                                        popoverClass={CommonStyles['popover']}
-                                        children={
-                                            <Typography variant="Regular_14" style={{ wordBreak: 'break-word' }}>
-                                                {rowData?.error}
-                                            </Typography>
-                                        }
-                                        trigger="hover"
-                                        delayHide={200}
-                                        interactive={true}
-                                        container={<Warning className={styles.statusIcon} />}
-                                    />
-                                ) : (
-                                    <Warning />
-                                ))}
                         </div>
                         <div>{jobMonitoringStatusMapping(cellData)}</div>
                     </div>
