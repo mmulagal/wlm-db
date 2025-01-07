93403936a0e46781478fb750ee5c4d1621553273
rpanwar@netapp.com <rpanwar@netapp.com>
2025-01-07 12:26:32 +0530

    DBS-4773 ES tooltip not visible fix

--- ui/src/workloadFactory/Sandbox/SandboxDistributionDate/SandboxChart/SandboxChart.tsx
+++ ui/src/workloadFactory/Sandbox/SandboxDistributionDate/SandboxChart/SandboxChart.tsx
@@ -70,10 +70,10 @@ const SandboxChart = () => {
                 {loading && <DsFlashingDotsLoader />}
             </div>
             {/* @ts-ignore */}
-            {aggregatedSandboxList.length === 0 && <div className={styles.emptyCircle}></div>}
-            {isNA && <div className={styles.emptyCircle}></div>}
 
-            {!isNA && <canvas ref={ref} id="chart-area" width={184} height={184}></canvas>}
+            {(isNA || aggregatedSandboxList.length === 0) && <div className={styles.emptyCircle}></div>}
+
+            {aggregatedSandboxList.length !== 0 && <canvas ref={ref} id="chart-area" width={184} height={184}></canvas>}
         </div>
     );
 };
