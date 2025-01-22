e25c1828a1d89a2ff5671d0a7df6c00e962fc265
Yadav, Sonam <Sonam.Sonam@netapp.com>
2025-01-22 16:10:27 +0530

    bug fix DBS-4878

--- ui/src/workloadFactory/GetWell/StorageCardComponent/StorageCardComponent.tsx
+++ ui/src/workloadFactory/GetWell/StorageCardComponent/StorageCardComponent.tsx
@@ -98,19 +98,22 @@ const StorageCardComponent = ({ cardData, optimizePrintState, type }: any) => {
         if (
             cardData?.id === 'headroom' &&
             (cardData?.block_two?.value === GETWELL_STATUS.OVER_PROVISIONED ||
-                cardData?.sizingViolations?.overProvisionedDrives?.length)
+                (cardData?.sizingViolations?.overProvisionedDrives?.length &&
+                    !cardData?.sizingViolations?.underProvisionedDrives?.length))
         ) {
             return GENERAL.HEADROOM_OVER_PROVISIONED_ERROR;
         } else if (
             cardData?.id === 'log-drive-size' &&
             (cardData?.block_two?.value === GETWELL_STATUS.OVER_PROVISIONED ||
-                cardData?.sizingViolations?.overProvisionedDrives?.length)
+                (cardData?.sizingViolations?.overProvisionedDrives?.length &&
+                    !cardData?.sizingViolations?.underProvisionedDrives?.length))
         ) {
             return GENERAL.LOG_DRIVE_OVER_PROVISIONED_ERROR;
         } else if (
             cardData?.id === 'tempdb-drive-size' &&
             (cardData?.block_two?.value === GETWELL_STATUS.OVER_PROVISIONED ||
-                cardData?.sizingViolations?.overProvisionedDrives?.length)
+                (cardData?.sizingViolations?.overProvisionedDrives?.length &&
+                    !cardData?.sizingViolations?.underProvisionedDrives?.length))
         ) {
             return GENERAL.TEMPDB_DRIVE_OVER_PROVISIONED_ERROR;
         } else if (
