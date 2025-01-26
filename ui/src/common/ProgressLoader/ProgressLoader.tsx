d7cb25d7e1bd1f2c5cce0efa626ad2f1ef716685
rpanwar@netapp.com <rpanwar@netapp.com>
2025-01-26 19:13:00 +0530

    DBS-4952 Progress Loader widget

---
+++ ui/src/common/ProgressLoader/ProgressLoader.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styles from './ProgressLoader.module.scss';
+import classNames from 'classnames';
+
+type ProgressLoaderSize = 'large' | 'small';
+
+const ProgressLoader = ({
+    percent,
+    thumbClassName,
+    thumbStyle,
+    className,
+    style,
+    indeterminate,
+    size = 'large'
+}: {
+    percent?: number;
+    thumbClassName?: string;
+    thumbStyle?: any;
+    style?: any;
+    indeterminate?: boolean;
+    size?: ProgressLoaderSize;
+    className?: string;
+}) => {
+    return (
+        <div
+            className={classNames(styles.track, className, {
+                [styles['indeterminate']]: indeterminate,
+                [styles['small']]: size === 'small'
+            })}
+            style={style}
+        >
+            <div
+                className={classNames(styles.thumb, thumbClassName)}
+                style={{ ...thumbStyle, width: indeterminate ? undefined : `${percent}%` }}
+            />
+        </div>
+    );
+};
+
+export default ProgressLoader;
