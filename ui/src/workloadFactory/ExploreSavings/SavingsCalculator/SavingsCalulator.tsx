7e9e8516073db59b4b2ea9efcce5798cd4ab4d88
rpanwar@netapp.com <rpanwar@netapp.com>
2025-01-29 11:16:53 +0530

    DBS-4999 timestamp added in downloaded Pdf

--- ui/src/workloadFactory/ExploreSavings/SavingsCalculator/SavingsCalulator.tsx
+++ ui/src/workloadFactory/ExploreSavings/SavingsCalculator/SavingsCalulator.tsx
@@ -107,7 +107,7 @@ const SavingsCalculator = ({ statusCheck }: any) => {
         setTimeout(() => {
             const elem = document.getElementById('export-pdf') as HTMLElement;
             var options = {
-                filename: `SavingsCalculator.pdf`,
+                filename: `SavingsCalculator-${Date.now()}.pdf`,
                 compression: 'MEDIUM'
             };
             //@ts-ignore
