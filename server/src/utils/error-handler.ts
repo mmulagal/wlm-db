281436063d27e96a7d5c2f3049807718a1d3ab93
bhaskarsen98 <bsen@netapp.com>
2025-01-30 17:40:09 +0530

    DBS-4786: addr review comm

--- server/src/utils/error-handler.ts
+++ server/src/utils/error-handler.ts
@@ -100,6 +100,15 @@ function handleValidationError(
             },
             ''
         );
+
+        /**
+         * In case of multiple error on same JSON node, fastify throws multiple errors for same node. next line should remove dupicates.
+         * The issue can be seen in cases where a JSON node is validated against an enum.
+         * For example: the error message:
+         * "body/assessments/0/configurationName must be equal to constant, body/assessments/0/configurationName must be equal to constant,  body/assessments/0/configurationName must match a schema in anyOf: prop1, prop2"
+         * Will be reduced to :
+         * "body/assessments/0/configurationName must be equal to constant, body/assessments/0/configurationName must match a schema in anyOf: prop1, prop2"
+         */
         message = message
             .split(',')
             .map(err => err.trim())
