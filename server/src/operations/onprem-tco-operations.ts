538698dd4b54ac31c20fcba33f8207663facfca2
mshreyas <mshreyas@netapp.com>
2025-02-05 16:10:47 +0530

    DBS-5055 bug fix

--- server/src/operations/onprem-tco-operations.ts
+++ server/src/operations/onprem-tco-operations.ts
@@ -19,7 +19,7 @@ import {
     MSSQL,
     WLMDB
 } from '../utils/consts';
-import { convertGiBToBytes, getArtifactsRegionBucketName, isDemo, sizeInGigaBytes } from '../utils/utils';
+import { convertGiBToBytes, getArtifactsRegionBucketName, sizeInGigaBytes } from '../utils/utils';
 import getLogger from '../utils/logger';
 import { registerJob } from './database/job-operations';
 import { updateJob } from '../lib/database/job';
@@ -73,8 +73,6 @@ import {
 const { getPreSignedUrl } = preSignedUrl;
 
 const logger = getLogger();
-
-const isDemoFlow = isDemo();
 
 const ENTERPRISE_EDITION = 'Enterprise Edition';
 const STANDARD_EDITION = 'Standard Edition';
@@ -1081,6 +1079,8 @@ function getOnpremLicenseRecommendations(sqlInstancesDetails: SqlInstanceDetails
         ({ isUsingAnyEnterpriseFeature }) => isUsingAnyEnterpriseFeature
     );
     const recommendedLicenseEdition = isUsingEnterpriseFeature ? ENTERPRISE_EDITION : STANDARD_EDITION;
+    const test = isNonFreeEnterpriseEdition(currentLicenseEdition);
+    logger.info(test);
     const finding =
         isNonFreeEnterpriseEdition(currentLicenseEdition) && !isUsingEnterpriseFeature
             ? FINDING.NOT_OPTIMIZED
@@ -1216,7 +1216,7 @@ async function getOnPremResourceExploreSavings(
 
     const { windowsSystemName: resourceName } = hostConfig as unknown as WindowsConfig;
 
-    if ((sqlInstanceData || snapShotInfo) && !isDemoFlow) {
+    if (sqlInstanceData || snapShotInfo) {
         try {
             const updatedSqlDetailsBasedOnRequest = rawSqlInstanceDetails.map(detail => {
                 try {
