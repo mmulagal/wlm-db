05281ee9245aff96c7150c3207a5a3e851b10f4d
rpanwar@netapp.com <rpanwar@netapp.com>
2025-02-08 10:51:16 +0530

    Added sample table for Hosts

--- ui/src/workloadFactory/InventoryV2/InventoryTablesComponent/InventoryTablesComponent.tsx
+++ ui/src/workloadFactory/InventoryV2/InventoryTablesComponent/InventoryTablesComponent.tsx
@@ -1,11 +1,12 @@
 import { useAppSelector } from '../../../store/storeHooks';
+import HostsTable from './HostsTable/HostsTable';
 import styles from './InventoryTablesComponent.module.scss';
 
 const InventoryTablesComponent = () => {
     const { selectedInventoryTab } = useAppSelector(state => state.inventoryV2);
     return (
         <div className={styles['inventory-tables-component']}>
-            {selectedInventoryTab === 'Hosts' && <div>Render host table here </div>}
+            {selectedInventoryTab === 'Hosts' && <HostsTable />}
             {selectedInventoryTab === 'Instances' && <div>Render Instances table here </div>}
             {selectedInventoryTab === 'Databases' && <div>Render Databases table here </div>}
         </div>
