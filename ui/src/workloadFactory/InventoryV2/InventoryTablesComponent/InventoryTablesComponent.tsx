eadf3dd8dba760dd4c85c42171aaa12866bfffb5
Yadav, Sonam <Sonam.Sonam@netapp.com>
2025-02-10 10:40:24 +0530

    new inventory updates

--- ui/src/workloadFactory/InventoryV2/InventoryTablesComponent/InventoryTablesComponent.tsx
+++ ui/src/workloadFactory/InventoryV2/InventoryTablesComponent/InventoryTablesComponent.tsx
@@ -1,13 +1,14 @@
 import { useAppSelector } from '../../../store/storeHooks';
+import HostTable from '../HostTable/HostTable';
 import InstancesTable from '../InstancesTable/InstancesTable';
-import HostsTable from './HostsTable/HostsTable';
+// import HostsTable from './HostsTable/HostsTable';
 import styles from './InventoryTablesComponent.module.scss';
 
 const InventoryTablesComponent = () => {
     const { selectedInventoryTab } = useAppSelector(state => state.inventoryV2);
     return (
         <div className={styles['inventory-tables-component']}>
-            {selectedInventoryTab === 'Hosts' && <HostsTable />}
+            {selectedInventoryTab === 'Hosts' && <HostTable />}
             {selectedInventoryTab === 'Instances' && <InstancesTable />}
             {selectedInventoryTab === 'Databases' && <div>Render Databases table here </div>}
         </div>
