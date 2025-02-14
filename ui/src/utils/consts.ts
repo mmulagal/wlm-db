b9bed00ca3993c54dee3f04b42085805d31c5e76
rpanwar@netapp.com <rpanwar@netapp.com>
2025-02-14 20:23:53 +0530

    DBS-5145 inner pages implementation

--- ui/src/utils/consts.ts
+++ ui/src/utils/consts.ts
@@ -348,6 +348,7 @@ export const WLF_TABS = {
     DASHBOARD: 'Dashboard',
     DASHBOARD_INNER_PAGE: 'DashboardInnerPage',
     OPTIMIZE_INNER_PAGE: 'OptimizeInnerPage',
+    OPTIMIZE_ONTAP_INNER_PAGE: 'OptimizeOntapInnerPage',
     INVENTORY: 'Inventory',
     OVERVIEW: 'Overview',
     SANDBOXES: 'Sandboxes',
