86f73be716853d89ef497f0217459ac7d8c154a7
Mulagalapati, Murali <Murali.Mulagalapati@netapp.com>
2025-02-18 16:19:58 +0530

    DBS-5166: trying to make the code visible to semgrep

--- server/src/utils/prisma-utils.ts
+++ server/src/utils/prisma-utils.ts
@@ -25,10 +25,9 @@ function sanitizeInput(input: string): string {
 async function execute(command: string, timeout?: number, cwd?: string) {
     logger.info('Executing command:', { command, timeout, cwd });
 
-    command = sanitizeInput(command);
     return new Promise(resolve => {
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        exec(command, { cwd, timeout }, (error: any, stdout: any, stderr: any) => {
+        exec(sanitizeInput(command), { cwd, timeout }, (error: any, stdout: any, stderr: any) => {
             if (error) {
                 logger.error('Failed to execute shell commands', error);
             }
