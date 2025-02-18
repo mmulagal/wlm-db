353feb458f29d4d3544d0de7cd6db2ba6429e98a
Mulagalapati, Murali <Murali.Mulagalapati@netapp.com>
2025-02-18 21:37:57 +0530

    Revert "change exec to spawn"
    This reverts commit c8f5397f4c7288e473cea1c5097934b4d279c367.

--- server/src/utils/prisma-utils.ts
+++ server/src/utils/prisma-utils.ts
@@ -1,4 +1,4 @@
-import { spawn } from 'child_process';
+import { exec } from 'child_process';
 import getLogger from './logger';
 
 let PrismaClient: typeof import('@prisma/client').PrismaClient;
@@ -24,42 +24,23 @@ function sanitizeInput(input: string): string {
 
 // Function to validate user input
 function validateInput(input: string): boolean {
-    return /^[a-zA-Z0-9_/.\s]+$/.test(input);
+    return /^[a-zA-Z0-9/\s]+$/.test(input);
 }
 
-async function execute(command: string, args: string[], timeout?: number, cwd?: string) {
-    logger.info('Executing command:', { command, args, timeout, cwd });
+async function execute(command: string, timeout?: number, cwd?: string) {
+    logger.info('Executing command:', { command, timeout, cwd });
 
-    if (!validateInput(command) || !args.every(validateInput)) {
+    if (!validateInput(command)) {
         throw new Error('Invalid command input');
     }
 
-    return new Promise((resolve, reject) => {
-        const child = spawn(command, args.map(sanitizeInput), { cwd, timeout });
-
-        let stdout = '';
-        let stderr = '';
-
-        child.stdout.on('data', data => {
-            stdout += data;
-        });
-
-        child.stderr.on('data', data => {
-            stderr += data;
-        });
-
-        child.on('close', code => {
-            if (code !== 0) {
-                logger.error('Failed to execute shell commands', { code, stderr });
-                reject(new Error(`Command to spawn failed with exit code ${code}`));
-                return;
+    return new Promise(resolve => {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        exec(sanitizeInput(command), { cwd, timeout }, (error: any, stdout: any, stderr: any) => {
+            if (error) {
+                logger.error('Failed to execute shell commands', error);
             }
-            resolve(stdout);
-        });
-
-        child.on('error', error => {
-            logger.error('Spawn failed to execute shell commands', error);
-            reject(error);
+            resolve(stdout || stderr);
         });
     });
 }
