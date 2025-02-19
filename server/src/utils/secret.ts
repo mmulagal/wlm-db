039f70edfc9083938b83adb944ae3167307c10e9
Mulagalapati, Murali <Murali.Mulagalapati@netapp.com>
2025-02-19 14:02:20 +0530

    Remove logging of secrets in demo

--- server/src/utils/secret.ts
+++ server/src/utils/secret.ts
@@ -44,6 +44,6 @@ export default async function initiateSecrets() {
         })
     );
     if (isDemo()) {
-        logger.info('Secrets initiated in demo:', SECRETS);
+        logger.info('Secrets initiated in demo');
     }
 }
