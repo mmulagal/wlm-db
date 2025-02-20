7231018f31ff20342538e3a7286352ac171801c5
apekshac <Apeksha.C@netapp.com>
2025-02-20 16:31:55 +0530

    DBS-5194 added the read me file and output files

---
+++ server/resources/pgsql/terraform/README.md
@@ -0,0 +1,160 @@
+# Workload Factory for Databases Terraform Deployment
+
+# Deploy a SQL Server on EC2 with FSx for ONTAP
+
+This Terraform automates the deployment of a PostgreSQL Server on Amazon EC2, utilizing FSx for ONTAP for storage. The deployment is configured as a standalone instance with Single-AZ FSx for ONTAP filesystem.
+
+## Table of Contents
+
+1. [Modules](#modules)
+2. [Deployment Models](#deployment-models)
+3. [Prerequisites](#prerequisites)
+4. [Installation](#installation)
+    - [Windows](#windows)
+    - [Mac](#mac)
+    - [Linux](#linux)
+5. [Variables](#variables)
+6. [Initial setup and usage](#initial-setup-and-usage)
+    - [Step 1: Initialize Terraform](#step-1-initialize-terraform)
+    - [Step 2: Update `terraform.tfvars` file](#step-2-update-terraformtfvars-file)
+    - [Step 3: Plan the deployment](#step-3-plan-the-deployment)
+    - [Step 4: Apply the changes](#step-4-apply-the-changes)
+    - [Step 5: Destroy the deployment](#step-5-destroy-the-deployment)
+
+## Modules
+
+-   **VPC endpoints**: Creates VPC Endpoints for private network deployments.
+-   **Validation node**: Creates a validation EC2 instance to perform a set of validations for the deployment.
+-   **FSx for ONTAP volumes**: Configures two LUNs on FSxN volumes for SQL Data, SQL Log.
+-   **EC2 instances**: Deploys EC2 instances with PostgreSQL.
+
+## Deployment Models
+
+1. **Standalone Instance**:
+    - Single-AZ deployment.
+    - Creates a single validation EC2 instance.
+
+## Prerequisites
+
+-   Terraform v1.9.5 or later
+-   AWS account
+-   AWS CLI installed and configured with your account
+    -   By default, this project will use the default profile.
+    -   To use a specific profile, mention the profile name in the `tfvars` file as `aws_profile`.
+
+## Installation
+
+### Windows
+
+**Terraform**
+
+1. Download the appropriate package for your system from the [Terraform downloads page](https://www.terraform.io/downloads.html).
+2. Unzip the downloaded file to a location where you want to store the Terraform binary. For example, you might create a directory named `terraform` in your `C:\` drive.
+3. Add the directory containing the Terraform binary to your system's PATH environment variable. You can do this by searching for "Environment Variables" in your computer's settings.
+
+**AWS CLI**
+
+1. Download the AWS CLI MSI installer for Windows from the [AWS CLI installation page](https://docs.aws.amazon.com/cli/v1/userguide/install-windows.html).
+2. Run the downloaded MSI installer and follow the on-screen instructions.
+
+### Mac
+
+**Terraform**
+
+1. If you have Homebrew installed, you can install Terraform by running:
+    ```sh
+    brew install terraform
+    ```
+2. If you don't have Homebrew, you can download the appropriate package for your system from the [Terraform downloads page](https://www.terraform.io/downloads.html) and move the Terraform binary to `/usr/local/bin/`.
+
+**AWS CLI**
+
+1. If you have Homebrew installed, you can install AWS CLI by running:
+    ```sh
+    brew install awscli
+    ```
+2. If you don't have Homebrew, you can download the AWS CLI package from the [AWS CLI installation page](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-mac.html) and follow the instructions to install it.
+
+### Linux
+
+**Terraform**
+
+1. Download the appropriate package for your system from the [Terraform downloads page](https://www.terraform.io/downloads.html).
+2. Unzip the downloaded file to a directory of your choice.
+3. Move the Terraform binary to `/usr/local/bin/`:
+    ```sh
+    sudo mv terraform /usr/local/bin/
+    ```
+
+**AWS CLI**
+
+1. Download the AWS CLI package from the [AWS CLI installation page](https://docs.aws.amazon.com/cli/latest/userguide/install-cliv2-linux.html).
+2. Unzip the package:
+    ```sh
+    unzip awscliv2.zip
+    ```
+3. Run the install script:
+
+    ```sh
+    sudo ./aws/install
+    ```
+
+    ```
+
+    ```
+
+## Variables
+
+If any validation errors are thrown while running `terraform plan`, please fill in the necessary values to continue the deployment.
+
+## Initial setup and usage
+
+### Step 1: Initialize Terraform
+
+Before you can use Terraform, you need to initialize your working directory. This step downloads the necessary provider plugins and sets up the backend configuration.
+
+```sh
+terraform init
+```
+
+### Step 2: Update `terraform.tfvars` file
+
+Update a file named `terraform.tfvars` in your project directory if any changes required. This file will contain the values for the variables required by your Terraform configuration. Fill in the necessary variables as described in the [Variables](#variables) section.
+
+Example `terraform.tfvars` file:
+
+```hcl
+aws_profile = "default"
+region      = "us-west-2"
+instance_type = "t2.medium"
+```
+
+### Step 3: Plan the deployment
+
+The `terraform plan` command creates an execution plan, showing you what actions Terraform will take to achieve the desired state defined in your configuration files. This step helps you verify that the changes are as expected before applying them.
+
+```sh
+terraform plan
+```
+
+### Step 4: Apply the changes
+
+The `terraform apply` command executes the actions proposed in the plan to create, update, or delete resources in your infrastructure. Review the plan output carefully before confirming the apply operation.
+
+```sh
+terraform apply
+```
+
+### Step 5: Destroy the deployment
+
+When you no longer need the infrastructure, you can use the `terraform destroy` command to delete all the resources created by Terraform. Use this command with caution, as it will remove all the infrastructure defined in your configuration.
+
+```sh
+terraform destroy
+```
+
+**Note:** Please use the `terraform destroy` command carefully, as it will delete all the infrastructure created via Terraform.
+
+```
+
+```
---
+++ server/resources/pgsql/terraform/outputs.tf
@@ -0,0 +1,172 @@
+output "credentials_ssm_parameter_name" {
+  value = aws_ssm_parameter.credentials_ssm_parameter.name
+}
+
+output "credentials_ssm_parameter_description" {
+  value = aws_ssm_parameter.credentials_ssm_parameter.description
+}
+
+output "credentials_ssm_parameter_value" {
+  value     = aws_ssm_parameter.credentials_ssm_parameter.value
+  sensitive = true
+}
+
+output "credentials_ssm_parameter_tags" {
+  value = aws_ssm_parameter.credentials_ssm_parameter.tags
+}
+
+output "role_name" {
+  value = aws_iam_role.ec2_iam_role.name
+}
+
+output "role_id" {
+  value = aws_iam_role.ec2_iam_role.id
+}
+
+# Output policy name and policy ID
+output "policy_name" {
+  value = aws_iam_role_policy.ec2_iam_role_policy.name
+}
+
+output "policy_id" {
+  value = aws_iam_role_policy.ec2_iam_role_policy.id
+}
+
+
+# output of sql node
+output "standalone_sql_node_instance_id" {
+  description = "The ID of the standalone SQL Node instance"
+  value       = length(module.standalone_sql_node) > 0 ? module.standalone_sql_node[0].sql_node_instance_id : null
+}
+
+output "standalone_sql_node_private_ip" {
+  description = "The private IP of the standalone SQL Node instance"
+  value       = length(module.standalone_sql_node) > 0 ? module.standalone_sql_node[0].sql_node_private_ip : null
+}
+
+output "standalone_sql_node_instance_name" {
+  description = "The name of the standalone SQL Node instance"
+  value       = length(module.standalone_sql_node) > 0 ? module.standalone_sql_node[0].sql_node_instance_name : null
+}
+
+# output of fsx node
+# For standalone
+output "fsx_fs_standalone_logical_id" {
+  description = "Logical ID of the FSx for ONTAP file system"
+  value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_fs_logical_id : null
+}
+
+output "fsx_svm_standalone_logical_id" {
+  description = "Logical ID of the storage virtual machine"
+  value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_svm_logical_id : null
+}
+
+output "fsx_svm_standalone_arn" {
+  description = "ARN of the storage virtual machine"
+  value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_svm_arn : null
+}
+
+output "fsx_svm_standalone_id" {
+  description = "System generated ID of the storage virtual machine"
+  value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_svm_id : null
+}
+
+output "fsx_svm_standalone_uuid" {
+  description = "System generated UUID of the storage virtual machine"
+  value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_svm_uuid : null
+}
+
+output "fsx_fs_standalone_resource_arn" {
+  description = "ARN of the FSx for ONTAP file system"
+  value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_fs_resource_arn : null
+}
+
+output "fsx_data_volume_standalone_logical_id" {
+  description = "Logical ID of the FSx for ONTAP data volume"
+  value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_data_volume_logical_id : null
+}
+
+output "fsx_data_volume_standalone_arn" {
+  description = "ARN of the FSx for ONTAP data volume"
+  value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_data_volume_arn : null
+}
+
+output "fsx_data_volume_standalone_id" {
+  description = "System generated ID of volume"
+  value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_data_volume_id : null
+}
+
+output "fsx_data_volume_standalone_uuid" {
+  description = "System generated UUID of volume"
+  value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_data_volume_uuid : null
+}
+
+output "fsx_log_volume_standalone_logical_id" {
+  description = "Logical ID of the FSx for ONTAP log volume"
+  value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_log_volume_logical_id : null
+}
+
+output "fsx_log_volume_standalone_arn" {
+  description = "ARN of the FSx for ONTAP log volume"
+  value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_log_volume_arn : null
+}
+
+output "fsx_log_volume_standalone_id" {
+  description = "System generated ID of log volume"
+  value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_log_volume_id : null
+}
+
+output "fsx_log_volume_standalone_uuid" {
+  description = "System generated UUID of log volume"
+  value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_log_volume_uuid : null
+}
+
+# output "fsx_cluster_volume_standalone_logical_id" {
+#   description = "Logical ID of the FSx for ONTAP cluster volume"
+#   value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_cluster_volume_logical_id : "Not Created"
+# }
+
+# output "fsx_cluster_volume_standalone_arn" {
+#   description = "ARN of the FSx for ONTAP cluster volume"
+#   value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_cluster_volume_arn : "Not Created"
+# }
+
+# output "fsx_cluster_volume_standalone_id" {
+#   description = "System generated ID of cluster volume"
+#   value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_cluster_volume_id : "Not Created"
+# }
+
+# output "fsx_cluster_volume_standalone_uuid" {
+#   description = "System generated UUID of cluster volume"
+#   value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_cluster_volume_uuid : "Not Created"
+# }
+
+# output "fsx_temp_dp_volume_standalone_logical_id" {
+#   description = "Logical ID of the FSx for ONTAP temp db volume"
+#   value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_temp_dp_volume_logical_id : null
+# }
+
+# output "fsx_temp_dp_volume_standalone_arn" {
+#   description = "ARN of the FSx for ONTAP temp db volume"
+#   value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_temp_dp_volume_arn : null
+# }
+
+# output "fsx_temp_dp_volume_standalone_id" {
+#   description = "System generated ID of temp db volume"
+#   value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_temp_dp_volume_id : null
+# }
+
+# output "fsx_temp_dp_volume_standalone_uuid" {
+#   description = "System generated UUID of temp dp volume"
+#   value       = length(module.fsxn_standalone) > 0 ? module.fsxn_standalone[0].fsx_temp_dp_volume_uuid : null
+# }
+
+output "absolute_path" {
+  value = abspath(path.root)
+}
+
+output "operating_system" {
+  value = local.is_windows ? "Windows" : "Linux"
+}
+
+
