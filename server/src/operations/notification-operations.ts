b8bb0327f6ef815a28e7e54a070f851f99b5fbd1
wahidx <wahidh@netapp.com>
2025-02-27 15:27:20 +0530

    using isDemo()

--- server/src/operations/notification-operations.ts
+++ server/src/operations/notification-operations.ts
@@ -4,7 +4,7 @@ import { EmailResponseType } from '../routes/types/notification.types';
 import { sendEmail } from './aws/ses-operations';
 import getLogger from '../utils/logger';
 import { EMAIL_RATE_LIMIT_TYPE, EMAIL_TYPES, HttpErrorCodes, MAX_EMAIL_ATTACHMENT_SIZE } from '../utils/consts';
-import { isRateLimited, isValidEmail } from '../utils/utils';
+import { isDemo, isRateLimited, isValidEmail } from '../utils/utils';
 
 const logger = getLogger();
 
@@ -23,7 +23,7 @@ export default async function processEmailRequest(
     const cacheKey = accountId + userEmail;
 
     if (
-        !(process.env.NODE_ENV === 'demo' || process.env.NODE_ENV === 'simulator') &&
+        !isDemo() &&
         isRateLimited(EMAIL_RATE_LIMIT_TYPE, cacheKey, config.get('notification.max-emails-per-day'), '1d')
     ) {
         throw createError(HttpErrorCodes.TOO_MANY_REQUESTS, 'Too many requests');
@@ -66,7 +66,7 @@ async function sendSavingsCalculationEmail(
     logger.info('Sending savings calculation email', { accountId, fileName, userEmail, storageType });
     const successMsg = { message: 'Email sent successfully' };
 
-    if (process.env.NODE_ENV === 'demo') {
+    if (isDemo()) {
         return successMsg;
     }
 
