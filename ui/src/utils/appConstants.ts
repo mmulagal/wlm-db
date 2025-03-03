827eb6c803ee8dedd2a5bb5c53753b8a58e0c61e
Yadav, Sonam <Sonam.Sonam@netapp.com>
2025-03-03 10:36:36 +0530

    manage flow

--- ui/src/utils/appConstants.ts
+++ ui/src/utils/appConstants.ts
@@ -1154,6 +1154,7 @@ export const GENERAL = {
         'Explore Savings for FSx for Windows File Server is not supported for Always on availability group deployment model.',
     NON_SSD_FSXW_MSG:
         'Explore Savings for FSx for Windows File Server is only supported for SSD file system storage type.',
+    MANAGED_SUPPORT_FOR_EBS_FSXW: 'Manage instance is not supported for EBS and FSx for Windows File Server storage type.',
     MIXED_STORAGE_ES_MSG: 'Explore Savings for mixed storage type is not supported.',
     MANAGE_INSTANCE_SUCCESS_MSG: ['The SQL Server instance ', ' has been successfully managed.'],
     MANAGE_INSTANCE_FAILED_MSG: [
