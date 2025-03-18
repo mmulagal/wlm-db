86d7bd50fd1812e52fe5345d20210c88beaa0dde
rpanwar@netapp.com <rpanwar@netapp.com>
2025-03-18 13:51:09 +0530

    DBS-5396 DBS-5398 defects fix

--- ui/src/workloadFactory/InventoryV2/InventoryV2.tsx
+++ ui/src/workloadFactory/InventoryV2/InventoryV2.tsx
@@ -6,7 +6,7 @@ import InventoryTab from './InventoryTab/InventoryTab';
 import InventoryTablesComponent from './InventoryTablesComponent/InventoryTablesComponent';
 import { INVENTORY_ACTIONS, INVENTORY_STATUS, PROTECTION_TEXT_STATUS } from '../../utils/consts';
 import { GENERAL } from '../../utils/appConstants';
-import { formatSizeTwoPrecision } from '../../utils/utilityFunctions';
+import { categorizeStorageSize, formatSize, formatSizeTwoPrecision } from '../../utils/utilityFunctions';
 import {
     getDiscoveredHostDeploymentV2,
     getOptimizationStatus,
@@ -178,6 +178,7 @@ const InventoryV2 = () => {
                                 credentialName: perHost?.credentialName,
                                 accountId: perHost?.accountId,
                                 regionName: perHost?.regionName,
+                                sizeRange: categorizeStorageSize(formatSize(perDatabase?.size)),
                                 resourceId: perHost?.resourceId,
                                 ec2InstanceId: perHost?.ec2InstanceId
                             };
