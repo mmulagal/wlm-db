fc700dc46b560067814b9a72bab9c6364e64c22a
Yadav, Sonam <Sonam.Sonam@netapp.com>
2025-03-21 09:52:55 +0530

    DBS-5442: snapshot policy payload change in UI

--- ui/src/workloadFactory/GetWell/OptimizeInnerPage/OptimizeInnerPage.tsx
+++ ui/src/workloadFactory/GetWell/OptimizeInnerPage/OptimizeInnerPage.tsx
@@ -285,9 +285,9 @@ const OptimizeInnerPage = () => {
                                 uuid: selectedSnapshot?.data?.uuid,
                                 name: selectedSnapshot?.data?.name
                             },
-                            volumes: selectedRowsForOptimizeInnerPage.map(({ volumeName, id }: any) => ({
+                            volumes: selectedRowsForOptimizeInnerPage.map(({ volumeName, ontapVolumeUuid }: any) => ({
                                 ontapVolumeName: volumeName,
-                                ontapVolumeUuid: id
+                                ontapVolumeUuid: ontapVolumeUuid
                             }))
                         }
                     ]
@@ -303,7 +303,7 @@ const OptimizeInnerPage = () => {
                             },
                             volumes: {
                                 ontapVolumeName: singleRowData?.volumeName,
-                                ontapVolumeUuid: singleRowData?.id
+                                ontapVolumeUuid: singleRowData?.ontapVolumeUuid
                             }
                         }
                     ]
