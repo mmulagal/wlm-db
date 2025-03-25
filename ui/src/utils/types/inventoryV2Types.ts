d27091efcc940599d344729f652331fe7b879f48
Yadav, Sonam <Sonam.Sonam@netapp.com>
2025-03-25 14:08:16 +0530

    DBS-5466: deployment type n/a fix

--- ui/src/utils/types/inventoryV2Types.ts
+++ ui/src/utils/types/inventoryV2Types.ts
@@ -142,6 +142,7 @@ export interface InventoryTableInstanceDatInterface {
         serverEdition?: string;
         serverVersion?: string;
     };
+    sqlServerDeploymentType?: string;
 }
 
 export interface StorageInterface {
