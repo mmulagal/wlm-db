db7b6ecac3624a1824559068a9144ec0acf7b0f0
wahidx <wahidh@netapp.com>
2025-04-25 12:56:28 +0530

    removing unwanted changes

--- server/src/operations/workloads/mssql/common-templates.ts
+++ server/src/operations/workloads/mssql/common-templates.ts
@@ -174,13 +174,8 @@ const compressResponse = `
 
         $bytes = $memoryStream.ToArray()
         $encodedString = [Convert]::ToBase64String($bytes)
-        
-        # If compressed string's length is greater than 24000, return the original string
-        if ($encodedString.length -ge 24000) {
-            return $stringToCompress
-        } else {
-            return $encodedString
-        }
+
+        return $encodedString
     }
 `;
 
