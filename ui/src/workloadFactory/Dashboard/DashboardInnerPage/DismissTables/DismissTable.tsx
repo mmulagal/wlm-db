438a3e5a20a07448b8c892a97bebcbf6cea4f8dd
rpanwar@netapp.com <rpanwar@netapp.com>
2025-04-25 12:34:28 +0530

    DBS-5621 Dismiss tables- added cred, region and account columns

--- ui/src/workloadFactory/Dashboard/DashboardInnerPage/DismissTables/DismissTable.tsx
+++ ui/src/workloadFactory/Dashboard/DashboardInnerPage/DismissTables/DismissTable.tsx
@@ -1,4 +1,4 @@
-import { Table, useTable, TableTopBar, ButtonWithDropdown, DsTypography } from '@netapp/design-system';
+import { ButtonWithDropdown, DsTypography } from '@netapp/design-system';
 import { ColumnProps } from '@netapp/design-system/dist/components/Table';
 
 import styles from './DismissTables.module.scss';
@@ -19,6 +19,10 @@ import { ReactComponent as Warning } from '../../../../assets/warning.svg';
 import { ReactComponent as Info } from '../../../../assets/info.svg';
 import BulkDismissContainer from '../../../../common/BulkAction/BulkDismissContainer';
 import { GENERAL } from '../../../../utils/appConstants';
+import { initialDashboardInnerPageOptimizeColState } from '../../../../utils/manageColumnUtils';
+import { useTable } from '../../../../common/Lib/Table/useTable';
+import { TableTopBar } from '../../../../common/Lib/Table/TableTopBar';
+import { Table } from '../../../../common/Lib/Table/Table';
 
 interface StorageTierTableProps {
     handleBulkAction: any;
@@ -78,14 +82,14 @@ const DismissTable = ({ handleBulkAction, handleSingleAction, tableData, type }:
             Header: 'Host name',
             accessor: 'hostName',
             id: '2',
-            width: '403px',
+            width: '200px',
             filterOptions: 'auto'
         },
         {
             Header: 'Analysis state',
             accessor: 'configState',
             id: '3',
-            width: 'auto',
+            width: '200px',
             filterOptions: 'auto',
             renderCell: (cellData: string, rowData: any) => {
                 return (
@@ -99,10 +103,32 @@ const DismissTable = ({ handleBulkAction, handleSingleAction, tableData, type }:
             }
         },
         {
+            id: '4',
+            Header: 'AWS credentials',
+            accessor: 'credentialName',
+            filterOptions: 'auto',
+            width: '180px'
+        },
+        {
+            id: '5',
+            Header: 'AWS account',
+            accessor: 'accountId',
+            filterOptions: 'auto',
+            width: '180px'
+        },
+        {
+            id: '6',
+            Header: 'Region',
+            accessor: 'regionName',
+            filterOptions: 'auto',
+            width: '180px'
+        },
+        {
             Header: 'Action',
             accessor: '',
-            id: '4',
-            width: '220px',
+            id: '7',
+            width: '150px',
+            isSticky: true,
             renderCell: (cellData: any, rowData: any) => {
                 return (
                     <div
@@ -158,13 +184,15 @@ const DismissTable = ({ handleBulkAction, handleSingleAction, tableData, type }:
     const tableProps = useTable({
         //@ts-ignore
         manageColumnsProps: false,
-        isHorizontalScroll: false,
+        isHorizontalScroll: true,
         isSorting: false,
         columns: TableColDefs,
         rows: tableData || [],
         pageSize: 50,
         selectionType: 'multiple',
-        defaultSelectedRows: []
+        defaultSelectedRows: [],
+        isManagedColumns: true,
+        initialColumnState: initialDashboardInnerPageOptimizeColState
     });
 
     useEffect(() => {
