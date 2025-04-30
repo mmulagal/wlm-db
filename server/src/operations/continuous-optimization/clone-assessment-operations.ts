5988d263d028a34e936e838935cf92fb9ed05e78
Varun <varunsub@netapp.com>
2025-04-30 11:15:56 +0530

    DBS-5551 - Clone assessment changed >=CLONE_AGE- need to change after staging tests

--- server/src/operations/continuous-optimization/clone-assessment-operations.ts
+++ server/src/operations/continuous-optimization/clone-assessment-operations.ts
@@ -359,7 +359,8 @@ async function runCloneAssessment(
                 // Calculate the number of days since the data volume was created only if the volume type is 'data'
                 if (cloneVolumeType === 'data') {
                     cloneAge = calculateDaysSince(cloneVolumeCreateTime);
-                    if (cloneAge > CLONE_AGE) {
+                    // Change it to > CLONE_AGE after staging testing
+                    if (cloneAge >= CLONE_AGE) {
                         oldClones += 1;
                     }
                 }
@@ -373,8 +374,8 @@ async function runCloneAssessment(
                 ...databaseObject,
                 clonedBy: 'netapp_wf'
             };
-
-            if (databaseObject.cloneAge !== undefined && databaseObject.cloneAge > CLONE_AGE) {
+            // Change it to > CLONE_AGE after staging testing
+            if (databaseObject.cloneAge !== undefined && databaseObject.cloneAge >= CLONE_AGE) {
                 oldCloneDetails.push(modifiedDatabaseObject);
                 oldCloneDatabaseNames.push(sandboxName);
             }
@@ -423,7 +424,8 @@ async function runCloneAssessment(
                     }
 
                     // If the clone is old, ensure it's added to oldCloneDetails and oldCloneDatabaseNames
-                    if (cloneAge > CLONE_AGE && !oldCloneDatabaseNames.includes(clonedDatabaseName)) {
+                    // Change it to > CLONE_AGE after staging testing
+                    if (cloneAge >= CLONE_AGE && !oldCloneDatabaseNames.includes(clonedDatabaseName)) {
                         oldClones += 1;
                         oldCloneDetails.push(existingRecord);
                         oldCloneDatabaseNames.push(clonedDatabaseName);
@@ -441,7 +443,8 @@ async function runCloneAssessment(
                     };
 
                     // Add to oldCloneDetails and oldCloneDatabaseNames if it's an old clone
-                    if (cloneAge !== undefined && cloneAge > CLONE_AGE) {
+                    // Change it to > CLONE_AGE after staging testing
+                    if (cloneAge !== undefined && cloneAge >= CLONE_AGE) {
                         oldClones += 1;
                         oldCloneDetails.push(databaseObject);
                         oldCloneDatabaseNames.push(clonedDatabaseName);
