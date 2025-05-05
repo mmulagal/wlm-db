f958c0c4116c9e8c0571d0be0e0e9c9e578f543a
nithinr <nithinr@netapp.com>
2025-05-05 13:19:55 +0530

    updates

--- server/resources/mssql/scripts/SQLServer-Data-Collector/SQLServerDataCollector.ps1
+++ server/resources/mssql/scripts/SQLServer-Data-Collector/SQLServerDataCollector.ps1
@@ -26,6 +26,7 @@ The collected data is output in JSON format, which can be used for further analy
 5. Script should be run with a Windows local login OR domain login that has admin rights on the system. Domain login is needed for cluster configuration.
 6. User should have admin privileges on SQL Server instance to get full report.
 7. If the Windows login does not have required access to the SQL Server instance, provide the local SQL user credentials for collection with '-SqlUserName' parameter.
+8. Collection should not have any performance impact on the SQL Server instance and finishes within few minutes.
 
 .PARAMETER instanceNames
 (Optional) Array of SQL Server instance names to query. If not specified, the script will attempt to gather information from all available SQL Server instances on the remote computer.
--- ui/src/workloadFactory/ExploreSavings/ExploreSavingHeader/ExploreSavingHeader.tsx
+++ ui/src/workloadFactory/ExploreSavings/ExploreSavingHeader/ExploreSavingHeader.tsx
@@ -251,7 +251,7 @@ const ExploreSavingHeader = () => {
                                                     </DsTypography>
                                                 </div>
                                                 <DsTypography variant="Regular_16">
-                                                    Run the script on the SQL Server host with Windows login having admin rights on the system and on SQL server. SQL local user credentials can be used if the Windows login does not have sufficient privileges.
+                                                    Run the script on the SQL Server host with user having admin rights on the system and on SQL server. More details can be found in the documentation within the script(DocString).
                                                 </DsTypography>
                                             </div>
 
