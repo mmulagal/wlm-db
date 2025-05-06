9b37757a86e031c7c77e45b53128e60633614588
bhaskarsen98 <bsen@netapp.com>
2025-05-06 12:51:43 +0530

    DBS-5188: add desc to path params for APIs for apidoc

--- server/src/utils/schema-description-consts.ts
+++ server/src/utils/schema-description-consts.ts
@@ -1,7 +1,21 @@
-const ACCOUNT_ID = 'Workload Factory account ID';
-const CREDENTIALS_ID = 'Workload Factory credentials ID';
-const AWS_REGION = 'AWS region hosting EC2 instances';
-const AWS_REGION_CODE = 'Region code for AWS region';
-const AWS_REGION_NAME = 'Region name for AWS region';
+const ACCOUNT_ID = 'Workload Factory account ID.';
+const CREDENTIALS_ID = 'Workload Factory credentials ID.';
+const AWS_REGION = 'AWS region hosting EC2 instances.';
+const AWS_REGION_CODE = 'Region code for AWS region.';
+const AWS_REGION_NAME = 'Region name for AWS region.';
+const DATABASE_HOST_ID =
+    'Unique identifier for database hosts managed by Workload Factory. The value for databaseHostId can be found using the GET database hosts API under Resources section in Database.';
+const DATABASE_INSTANCE_ID =
+    'Unique identifier for database instances managed by Workload Factory. The value for databaseInstanceId can be found using the GET database instances API under Resources section in Database.';
+const DATABASE_INSTANCE_NAME = 'SQL Server instance name';
 
-export { ACCOUNT_ID, CREDENTIALS_ID, AWS_REGION, AWS_REGION_CODE, AWS_REGION_NAME };
+export {
+    ACCOUNT_ID,
+    CREDENTIALS_ID,
+    AWS_REGION,
+    AWS_REGION_CODE,
+    AWS_REGION_NAME,
+    DATABASE_HOST_ID,
+    DATABASE_INSTANCE_ID,
+    DATABASE_INSTANCE_NAME
+};
