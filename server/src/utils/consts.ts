fe2d6f8cc359961eec1c47b09ab2de01c47eda73
S, Sathish <Sathish.S@netapp.com>
2025-05-06 14:51:25 +0530

    Clone Age added part of config file so its different for staging and prod

--- server/src/utils/consts.ts
+++ server/src/utils/consts.ts
@@ -1725,7 +1725,7 @@ enum CLONE_ACTION {
 const RESTRICTED_FSX_REGIONS: Array<string> = ['us-gov-east-1', 'us-gov-west-1', 'cn-north-1', 'cn-northwest-1'];
 
 const CLOUDWATCH_LOG_GROUP_FOR_SSM_RESPONSE = 'netapp/wlmdb/ssm-response';
-const CLONE_AGE = 60;
+const CLONE_AGE = config.get('clone-age');
 const OTHER_CLONE = 'other';
 
 export {
