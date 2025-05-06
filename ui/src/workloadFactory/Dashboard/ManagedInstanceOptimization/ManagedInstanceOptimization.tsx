b71279c4dea048c07dabaf08281818efdf6fb49f
Dinesh Varma Kanumuri <kanumurv@netapp.com>
2025-05-06 17:40:59 +0530

    DBS-5806-added-tooltip-only-for-postponed

--- ui/src/workloadFactory/Dashboard/ManagedInstanceOptimization/ManagedInstanceOptimization.tsx
+++ ui/src/workloadFactory/Dashboard/ManagedInstanceOptimization/ManagedInstanceOptimization.tsx
@@ -48,7 +48,7 @@ const ManagedInstanceOptimization = ({ openAccordion, setOpenAccordion }: any) =
 
                 <div className={styles.ManageInstanceTooltipSection}>
                     {loading && <FlashingDotsLoader />}
-                    <TooltipInfo>{GENERAL.MANAGE_INSTANCE_OPTIMIZATION_SCORE_TOOLTIP}</TooltipInfo>
+                    {instanceOptimizationSummary?.hasDismissedOrPostponed && (<TooltipInfo>{GENERAL.MANAGE_INSTANCE_OPTIMIZATION_SCORE_TOOLTIP}</TooltipInfo>)}
                 </div>
             </div>
 
