57a9fba459f92340835d4f22f2b88c47cce887b9
Kanika Jain <kanikaj@netapp.com>
2025-05-09 14:53:01 +0530

    DBS-5807 Added back the Optimization job type

--- server/prisma/migrations/20250509052144_well_architected_lingo_changes_in_job_type/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - The values [OPTIMIZATION] on the enum `job_type` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterTable
-ALTER TABLE `job` MODIFY `type` ENUM('DEPLOYMENT', 'CREATE_RESOURCE', 'PREPARE_RESOURCE', 'SANDBOX', 'ASSESSMENT', 'WELL_ARCHITECTED') NOT NULL;
