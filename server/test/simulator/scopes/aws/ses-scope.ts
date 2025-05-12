5cd0244a99a1568d4173141df148a69c059fd1cc
Varun <varunsub@netapp.com>
2025-05-12 16:27:36 +0530

    DBS-5778- nodemailer and aws ses changes reverted

--- server/test/simulator/scopes/aws/ses-scope.ts
+++ server/test/simulator/scopes/aws/ses-scope.ts
@@ -1,9 +1,9 @@
 // workaroud for the sdk type issue.. remove this @ts-nocheck once the sdk mock works fine
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-nocheck
-import { SESv2Client, SendRawEmailCommand } from '@aws-sdk/client-sesv2';
+import { SESClient, SendRawEmailCommand } from '@aws-sdk/client-ses';
 import { mockClient } from 'aws-sdk-client-mock';
 
-const sesMock = mockClient(SESv2Client);
+const sesMock = mockClient(SESClient);
 
 sesMock.on(SendRawEmailCommand).resolves({});
