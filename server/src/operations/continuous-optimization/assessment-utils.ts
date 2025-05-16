4e6f736e55618b08495e0d12515387c4c2d72333
Kanika Jain <kanikaj@netapp.com>
2025-05-16 10:07:14 +0530

    DBS-5898 Updated audit messages as per Well-architected lingo

--- server/src/operations/continuous-optimization/assessment-utils.ts
+++ server/src/operations/continuous-optimization/assessment-utils.ts
@@ -99,10 +99,7 @@ async function handleOptimizeJobCreation(
     if (job) {
         const timeDifferenceInMinutes = getTimeDifferenceInMinutes(job.startTime);
         if (timeDifferenceInMinutes <= 5) {
-            throw createError(
-                412,
-                `The following optimization is running: Job ID:  ${job.id}. Wait until it completes.`
-            );
+            throw createError(412, `The following fixing is running: Job ID:  ${job.id}. Wait until it completes.`);
         }
     }
 
