e23baf3c6d8ec7449c858cc73c6ee6d627efebca
Yadav, Sonam <Sonam.Sonam@netapp.com>
2025-05-19 15:58:12 +0530

    oracle and pgsql loading check in call completion

--- ui/src/workloadFactory/DatabaseHomePage/HeaderComponent/HeaderComponent.tsx
+++ ui/src/workloadFactory/DatabaseHomePage/HeaderComponent/HeaderComponent.tsx
@@ -165,6 +165,8 @@ const HeaderComponent = ({ tab }: Tab) => {
     const { loading: dashSandboxListLoading } = useAppSelector(state => state.inventoryV2.dashSandboxList);
     const { loading: dashSandboxSavingsLoading } = useAppSelector(state => state.inventoryV2.dashSandboxSavings);
     const { discoverHostLoading } = useAppSelector(state => state.inventoryV2.discoveredHosts);
+    const { discoverOracleHostLoading } = useAppSelector(state => state.inventoryV2.discoveredOracleHosts);
+    const { discoverPgsqlHostLoading } = useAppSelector(state => state.inventoryV2.discoveredPgsqlHosts);
     //Added for widget
     const [currentCred, setCurrentCred] = useState<string | null>(null);
     const [currentRegion, setCurrentRegion] = useState<string | null>(null);
@@ -465,6 +467,8 @@ const HeaderComponent = ({ tab }: Tab) => {
             !dashSandboxListLoading &&
             !dashSandboxSavingsLoading &&
             !discoverHostLoading &&
+            !discoverOracleHostLoading &&
+            !discoverPgsqlHostLoading &&
             !fsxCredentialStatusLoading
         ) {
             let currentCredId = headerSelectedCred?.data?.credentialsId;
