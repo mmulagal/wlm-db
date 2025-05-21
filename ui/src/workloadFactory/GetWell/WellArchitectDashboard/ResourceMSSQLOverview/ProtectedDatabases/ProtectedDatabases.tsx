7768c96c3e9fc38c8053c8a51abe90f9dedcb283
rpanwar@netapp.com <rpanwar@netapp.com>
2025-05-21 11:32:33 +0530

    DBS-5914 Removed the check

--- ui/src/workloadFactory/GetWell/WellArchitectDashboard/ResourceMSSQLOverview/ProtectedDatabases/ProtectedDatabases.tsx
+++ ui/src/workloadFactory/GetWell/WellArchitectDashboard/ResourceMSSQLOverview/ProtectedDatabases/ProtectedDatabases.tsx
@@ -46,15 +46,14 @@ const ProtectedDatabases = () => {
                 <div className={styles.barContainer}>
                     <div className={styles.valueSection}>
                         <DsTypography variant="Semibold_14">Remote protection (replicated volumes)</DsTypography>
-                        {protectionData?.crrEnabled && (
-                            <div className={styles.count}>
-                                <DsTypography variant="Regular_20">{protectionData?.crrEnabled}</DsTypography>
-                                <DsTypography variant="Regular_14">out of</DsTypography>
-                                <DsTypography variant="Regular_20">
-                                    {protectionData.protectedDb + protectionData.unprotectedDb}
-                                </DsTypography>
-                            </div>
-                        )}
+
+                        <div className={styles.count}>
+                            <DsTypography variant="Regular_20">{protectionData?.crrEnabled}</DsTypography>
+                            <DsTypography variant="Regular_14">out of</DsTypography>
+                            <DsTypography variant="Regular_20">
+                                {protectionData.protectedDb + protectionData.unprotectedDb}
+                            </DsTypography>
+                        </div>
                     </div>
 
                     <div className={styles.barSection}>
