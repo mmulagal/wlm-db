c0fc7ed2149139deddc6427690b2a54d7d015875
Rahul panwar <33785711+rahulpanwarNetapp@users.noreply.github.com>
2025-05-26 15:20:56 +0530

    DBS-5958 Adding coming soon on Windows auth Radio (#1543)

--- ui/src/workloadFactory/InventoryV2/InventoryTablesComponent/ManageInstanceWizard/DetectInstanceStep/DetectContent/DetectContent.tsx
+++ ui/src/workloadFactory/InventoryV2/InventoryTablesComponent/ManageInstanceWizard/DetectInstanceStep/DetectContent/DetectContent.tsx
@@ -1,4 +1,4 @@
-import { DsTypography, PasswordField, RadioButton, TextField, useWizard } from '@netapp/design-system';
+import { DsTypography, PasswordField, Popover, RadioButton, TextField, useWizard } from '@netapp/design-system';
 import styles from './DetectContent.module.scss';
 import { useDispatch } from 'react-redux';
 import { useAppSelector } from '../../../../../../store/storeHooks';
@@ -111,16 +111,25 @@ const DetectContent = () => {
                         children={AUTHENTICATION_TYPE.SQL_SERVER_AUTHENTICATION}
                         className=""
                     />
-                    <RadioButton
-                        id="select-windows-authentication"
-                        isDisabled={true} // Currently windows auth is not supported
-                        isChecked={authenticationType === AUTHENTICATION_TYPE.WINDOWS_AUTHENTICATION}
-                        onChange={() => {
-                            dispatch(setAuthenticationType(AUTHENTICATION_TYPE.WINDOWS_AUTHENTICATION));
-                            setState({ authenticationTypeSelected: AUTHENTICATION_TYPE.WINDOWS_AUTHENTICATION });
-                        }}
-                        children={AUTHENTICATION_TYPE.WINDOWS_AUTHENTICATION}
-                        className=""
+                    <Popover
+                        children={GENERAL.COMING_SOON}
+                        trigger="hover"
+                        container={
+                            <RadioButton
+                                id="select-windows-authentication"
+                                isDisabled={true} // Currently windows auth is not supported
+                                title="Windows authentication is not supported yet"
+                                isChecked={authenticationType === AUTHENTICATION_TYPE.WINDOWS_AUTHENTICATION}
+                                onChange={() => {
+                                    dispatch(setAuthenticationType(AUTHENTICATION_TYPE.WINDOWS_AUTHENTICATION));
+                                    setState({
+                                        authenticationTypeSelected: AUTHENTICATION_TYPE.WINDOWS_AUTHENTICATION
+                                    });
+                                }}
+                                children={AUTHENTICATION_TYPE.WINDOWS_AUTHENTICATION}
+                                className=""
+                            />
+                        }
                     />
                 </div>
             )}
