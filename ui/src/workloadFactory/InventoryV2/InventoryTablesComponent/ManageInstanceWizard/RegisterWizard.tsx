883e4c808bff9034fdb8e4557ec4684ec5906241
Yadav, Sonam <Sonam.Sonam@netapp.com>
2025-06-03 11:40:23 +0530

    register entry update for bulk

---
+++ ui/src/workloadFactory/InventoryV2/InventoryTablesComponent/ManageInstanceWizard/RegisterWizard.tsx
@@ -0,0 +1,18 @@
+import { useAppSelector } from "../../../../store/storeHooks";
+import ManageInstanceWizard from "./ManageInstanceWizard";
+import RegisterBulkWizard from "./RegisterBulkWizard";
+
+const RegisterWizard = () => {
+    const { wizardOperationType } = useAppSelector(state => state.inventoryV2);
+    return (
+        <>
+            {wizardOperationType === 'bulk' ? (
+                <RegisterBulkWizard />
+            ) : (
+                <ManageInstanceWizard />
+            )}
+        </>
+    )
+};
+
+export default RegisterWizard;
