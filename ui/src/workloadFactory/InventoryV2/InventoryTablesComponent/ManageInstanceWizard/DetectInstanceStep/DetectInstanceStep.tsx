c9bcda4b8d8a48b0506e5cdd39d1b6db9f6cf248
Yadav, Sonam <Sonam.Sonam@netapp.com>
2025-06-04 11:50:36 +0530

    detect form update for bulk

--- ui/src/workloadFactory/InventoryV2/InventoryTablesComponent/ManageInstanceWizard/DetectInstanceStep/DetectInstanceStep.tsx
+++ ui/src/workloadFactory/InventoryV2/InventoryTablesComponent/ManageInstanceWizard/DetectInstanceStep/DetectInstanceStep.tsx
@@ -25,8 +25,9 @@ export const Content = () => {
 
             {wizardOperationType === ACTION_TYPE.BULK && isAuth && <AuthenticatedScreen />}
 
-            {(wizardOperationType !== ACTION_TYPE.BULK ||
-                (wizardOperationType === ACTION_TYPE.BULK && !isAuth)) && <DetectContent />}
+            {(wizardOperationType !== ACTION_TYPE.BULK || (wizardOperationType === ACTION_TYPE.BULK && !isAuth)) && (
+                <DetectContent />
+            )}
         </div>
     );
 };
