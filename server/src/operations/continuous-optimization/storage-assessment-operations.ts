629701f42ba0e12e98419d0506a9ab60deac068f
krithib <krithib@netapp.com>
2025-06-16 15:24:02 +0530

    DBS-6143: Remove mpio-timeout from assessment response for prod patch

--- server/src/operations/continuous-optimization/storage-assessment-operations.ts
+++ server/src/operations/continuous-optimization/storage-assessment-operations.ts
@@ -469,23 +469,24 @@ async function calculateStorageDrift(
                         value: policyDetail.policy,
                         objectType: ASSESSMENT_RESOURCE_TYPE.DRIVE
                     }));
-            } else if (key === 'mpio-timeout') {
-                assessmentDetails = Object.entries(os)
-                    .filter(([type]) => type === 'mpio-timeout')
-                    .map(([, data]) => data)
-                    .flat();
-                objectsInViolation = assessmentDetails
-                    .filter(
-                        timeoutDetail =>
-                            timeoutDetail['mpio-timeout'] && timeoutDetail['mpio-timeout'] !== goldenData.value
-                    )
-                    .map(timeoutDetail => ({
-                        objectName: timeoutDetail.accessPath || timeoutDetail.disk || '',
-                        value: timeoutDetail.timeout,
-                        objectType: ASSESSMENT_RESOURCE_TYPE.DRIVE
-                    }));
-                value = Number(value);
             }
+            // else if (key === 'mpio-timeout') {
+            //     assessmentDetails = Object.entries(os)
+            //         .filter(([type]) => type === 'mpio-timeout')
+            //         .map(([, data]) => data)
+            //         .flat();
+            //     objectsInViolation = assessmentDetails
+            //         .filter(
+            //             timeoutDetail =>
+            //                 timeoutDetail['mpio-timeout'] && timeoutDetail['mpio-timeout'] !== goldenData.value
+            //         )
+            //         .map(timeoutDetail => ({
+            //             objectName: timeoutDetail.accessPath || timeoutDetail.disk || '',
+            //             value: timeoutDetail.timeout,
+            //             objectType: ASSESSMENT_RESOURCE_TYPE.DRIVE
+            //         }));
+            //     value = Number(value);
+            // }
             const status = goldenData?.value === value ? AssessmentStatus.OPTIMIZED : AssessmentStatus.NOT_OPTIMIZED;
             driftAssessmentData.configuration.os.push({
                 name: key,
