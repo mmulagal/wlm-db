0f99b5e5e48308d6d07b46c86c245e1f78c4cbae
bhaskarsen98 <bsen@netapp.com>
2025-06-19 10:05:45 +0530

    DBS-6072: make protection/appconsistentbackup field optional

--- server/src/routes/types/database-hosts.types.ts
+++ server/src/routes/types/database-hosts.types.ts
@@ -129,7 +129,9 @@ const ProtectionPerStorageTypeResponse = Type.Object({
         ebs: Type.Boolean({ default: false })
     }),
     isFsxOntapSnapshotsEnabled: Type.Union([Type.Boolean({ default: false }), Type.String({ enum: ['N/A'] })]),
-    isAppConsistentBackupEnabled: Type.Union([Type.Boolean({ default: false }), Type.String({ enum: ['N/A'] })]),
+    isAppConsistentBackupEnabled: Type.Optional(
+        Type.Union([Type.Boolean({ default: false }), Type.String({ enum: ['N/A'] })])
+    ),
     protectedDatabases: Type.Optional(Type.Number({ description: 'Number of protected databases' }))
 });
 type ProtectionPerStorageTypeResponseType = Static<typeof ProtectionPerStorageTypeResponse>;
