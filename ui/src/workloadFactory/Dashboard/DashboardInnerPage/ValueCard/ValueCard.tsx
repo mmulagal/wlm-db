585c40a97c7a2f1fcffd31b39d820ac6652ecaca
apekshac <Apeksha.C@netapp.com>
2025-06-24 16:51:33 +0530

    DBS-6207 i18 changes for well architect dashboard vaule card

--- ui/src/workloadFactory/Dashboard/DashboardInnerPage/ValueCard/ValueCard.tsx
+++ ui/src/workloadFactory/Dashboard/DashboardInnerPage/ValueCard/ValueCard.tsx
@@ -1,4 +1,5 @@
 import { DsTypography, Popover, TooltipInfo } from '@netapp/design-system';
+import { useTranslation } from 'react-i18next';
 import styles from './ValueCard.module.scss';
 import { ReactComponent as Edit } from '../../../../assets/ic_edit.svg';
 import { GENERAL } from '../../../../utils/appConstants';
@@ -26,71 +27,88 @@ const ValueCard = ({
     tooltipText,
     type,
     handleEdit
-}: ValueCardProps) => (
-    <>
-        {from === 'innerPage' && (
-            <div className={styles.valueCard}>
-                <div className={styles.block} style={{ borderRight: '1px solid var(--border)' }}>
-                    <DsTypography variant="Regular_24" style={{ lineHeight: 'unset' }}>
-                        {optimizedInstances}
-                    </DsTypography>
-                    <DsTypography variant="Regular_14">Well-architected instances</DsTypography>
-                </div>
-
-                <div className={styles.block} style={{ borderRight: '1px solid var(--border)' }}>
-                    <DsTypography variant="Regular_24" style={{ lineHeight: 'unset' }}>
-                        {notOptimizedInstances}
-                    </DsTypography>
-                    <DsTypography variant="Regular_14">Not-optimized instances</DsTypography>
-                </div>
-
-                <div className={styles.block} style={{ borderRight: '1px solid var(--border)' }}>
-                    <DsTypography variant="Semibold_14">{severity}</DsTypography>
-                    <DsTypography variant="Regular_14">Severity</DsTypography>
-                </div>
-
-                <div className={styles.block}>
-                    <div className={styles.configState}>
-                        <DsTypography variant="Semibold_14">{configurationState}</DsTypography>
-                        <Popover
-                            children={GENERAL.MANAGE_ANALYSIS_STATE}
-                            trigger="hover"
-                            container={
-                                <div onClick={() => handleEdit(type)}>
-                                    <Edit />
-                                </div>
-                            }
-                        />
-                    </div>
-                    <DsTypography variant="Regular_14">Analysis state</DsTypography>
-                </div>
-            </div>
-        )}
-        {from === 'dismissPage' && (
-            <div className={styles.valueCard}>
-                <div className={styles.block} style={{ borderRight: '1px solid var(--border)' }}>
-                    <DsTypography variant="Regular_24" style={{ lineHeight: 'unset' }}>
-                        {instances}
-                    </DsTypography>
-                    <DsTypography variant="Regular_14">Instances</DsTypography>
-                </div>
-
-                <div className={styles.block} style={{ borderRight: '1px solid var(--border)' }}>
-                    <div className={styles.configContainer}>
-                        {tooltipText && <TooltipInfo>{tooltipText}</TooltipInfo>}
-                        <DsTypography variant="Semibold_14">{configurationState}</DsTypography>
+}: ValueCardProps) => {
+    const { t } = useTranslation();
+    return (
+        <>
+            {from === 'innerPage' && (
+                <div className={styles.valueCard}>
+                    <div className={styles.block} style={{ borderRight: '1px solid var(--border)' }}>
+                        <DsTypography variant="Regular_24" style={{ lineHeight: 'unset' }}>
+                            {optimizedInstances}
+                        </DsTypography>
+                        <DsTypography variant="Regular_14">
+                            {t('databases.well-architect.valueCard.well-architected-instances')}
+                        </DsTypography>
                     </div>
 
-                    <DsTypography variant="Regular_14">Analysis state</DsTypography>
-                </div>
+                    <div className={styles.block} style={{ borderRight: '1px solid var(--border)' }}>
+                        <DsTypography variant="Regular_24" style={{ lineHeight: 'unset' }}>
+                            {notOptimizedInstances}
+                        </DsTypography>
+                        <DsTypography variant="Regular_14">
+                            {t('databases.well-architect.valueCard.not-optimized-instances')}
+                        </DsTypography>
+                    </div>
 
-                <div className={styles.block}>
-                    <DsTypography variant="Semibold_14">{severity}</DsTypography>
-                    <DsTypography variant="Regular_14">Severity</DsTypography>
+                    <div className={styles.block} style={{ borderRight: '1px solid var(--border)' }}>
+                        <DsTypography variant="Semibold_14">{severity}</DsTypography>
+                        <DsTypography variant="Regular_14">
+                            {t('databases.well-architect.valueCard.severity')}
+                        </DsTypography>
+                    </div>
+
+                    <div className={styles.block}>
+                        <div className={styles.configState}>
+                            <DsTypography variant="Semibold_14">{configurationState}</DsTypography>
+                            <Popover
+                                children={GENERAL.MANAGE_ANALYSIS_STATE}
+                                trigger="hover"
+                                container={
+                                    <div onClick={() => handleEdit(type)}>
+                                        <Edit />
+                                    </div>
+                                }
+                            />
+                        </div>
+                        <DsTypography variant="Regular_14">
+                            {t('databases.well-architect.valueCard.analysis-state')}
+                        </DsTypography>
+                    </div>
                 </div>
-            </div>
-        )}
-    </>
-);
+            )}
+            {from === 'dismissPage' && (
+                <div className={styles.valueCard}>
+                    <div className={styles.block} style={{ borderRight: '1px solid var(--border)' }}>
+                        <DsTypography variant="Regular_24" style={{ lineHeight: 'unset' }}>
+                            {instances}
+                        </DsTypography>
+                        <DsTypography variant="Regular_14">
+                            {t('databases.well-architect.valueCard.instances')}
+                        </DsTypography>
+                    </div>
+
+                    <div className={styles.block} style={{ borderRight: '1px solid var(--border)' }}>
+                        <div className={styles.configContainer}>
+                            {tooltipText && <TooltipInfo>{tooltipText}</TooltipInfo>}
+                            <DsTypography variant="Semibold_14">{configurationState}</DsTypography>
+                        </div>
+
+                        <DsTypography variant="Regular_14">
+                            {t('databases.well-architect.valueCard.analysis-state')}
+                        </DsTypography>
+                    </div>
+
+                    <div className={styles.block}>
+                        <DsTypography variant="Semibold_14">{severity}</DsTypography>
+                        <DsTypography variant="Regular_14">
+                            {t('databases.well-architect.valueCard.severity')}
+                        </DsTypography>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
 
 export default ValueCard;
