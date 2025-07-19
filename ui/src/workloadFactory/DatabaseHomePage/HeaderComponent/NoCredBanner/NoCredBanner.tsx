6ff33add6d7a0310bc67e751086022bfd02f3b90
rpanwar@netapp.com <rpanwar@netapp.com>
2025-07-19 19:03:30 +0530

    GH-2315 Adding direct credential page link in BlueXP

--- ui/src/workloadFactory/DatabaseHomePage/HeaderComponent/NoCredBanner/NoCredBanner.tsx
+++ ui/src/workloadFactory/DatabaseHomePage/HeaderComponent/NoCredBanner/NoCredBanner.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import { DsButton, DsTypography } from '@tlveng/wlm-ds';
+import { useAppSelector } from '../../../../store/storeHooks';
 import { BlueXPListeners, Button, postBlueXPMessage } from '@netapp/design-system';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
@@ -11,18 +12,30 @@ import styles from './NoCredBanner.module.scss';
 
 const NoCredBanner = ({ width }: any) => {
     const [isVisible, setIsVisible] = useState(true);
+    const { isWorkloadFactory } = useAppSelector(state => state?.auth);
     const navigate = useNavigate();
     const { t } = useTranslation();
 
     const openCred = () => {
-        navigate('../../credentials');
-        postBlueXPMessage({
-            type: BlueXPListeners.navigate,
-            payload: {
-                pathname: '../../credentials',
-                replace: true
-            }
-        });
+        if (!isWorkloadFactory) {
+            navigate('../..//credentials/wlf');
+            postBlueXPMessage({
+                type: BlueXPListeners.navigate,
+                payload: {
+                    pathname: '../..//credentials/wlf',
+                    replace: true
+                }
+            });
+        } else {
+            navigate('../../credentials');
+            postBlueXPMessage({
+                type: BlueXPListeners.navigate,
+                payload: {
+                    pathname: '../../credentials',
+                    replace: true
+                }
+            });
+        }
     };
 
     const learnMore = () => {
