0107c3e64cdd2b11e6cc6fd04497ae422d5e2da1
rpanwar@netapp.com <rpanwar@netapp.com>
2025-07-20 16:48:21 +0530

    GH-2174: Esty's feedback- changing LUN to LUNS

--- ui/src/workloadFactory/ResourcePage/InformationSection/StorageCompute/LunsDialogContent/LunsDialogContent.tsx
+++ ui/src/workloadFactory/ResourcePage/InformationSection/StorageCompute/LunsDialogContent/LunsDialogContent.tsx
@@ -32,11 +32,11 @@ const LunsDialogContent = ({ resourceDetails }: any) => {
                     {t('databases.resource-overview.associated_volumes')}
                 </DsTypography>
                 <DsTypography variant="Semibold_14" className={styles.tableCell}>
-                    {t('databases.resource-overview.associated_luns')}
+                    {t('databases.resource-overview.associated_lun')}
                 </DsTypography>
             </div>
-            {data.map((item: any, idx: number) => (
-                <div key={idx} className={styles.tableRow}>
+            {data.map((item: any) => (
+                <div key={item.volume} className={styles.tableRow}>
                     <DsTypography variant="Regular_14" className={styles.tableCellFsx} title={item.fsxn}>
                         {item.fsxn}
                     </DsTypography>
