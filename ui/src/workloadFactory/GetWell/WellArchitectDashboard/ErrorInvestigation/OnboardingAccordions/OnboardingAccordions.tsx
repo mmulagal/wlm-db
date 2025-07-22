1e23f3e5ed1bb9d30541272d2e419860ea4cbb25
rpanwar@netapp.com <rpanwar@netapp.com>
2025-07-22 19:55:10 +0530

    GH-2345 Accordion component for agentic ai onboarding 2

--- ui/src/workloadFactory/GetWell/WellArchitectDashboard/ErrorInvestigation/OnboardingAccordions/OnboardingAccordions.tsx
+++ ui/src/workloadFactory/GetWell/WellArchitectDashboard/ErrorInvestigation/OnboardingAccordions/OnboardingAccordions.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { ReactComponent as ReviewDisabled } from '../../../../..//assets/Review-disabled.svg';
+import { ReactComponent as ReviewDisabled } from '../../../../../assets/Review-disabled.svg';
 import {
     AccordionItem,
     ManageInstanceAccordion
