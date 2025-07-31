57db4aee6d1c88f94d0248483b4497d471df7062
nithinr <nithinr@netapp.com>
2025-07-31 19:30:27 +0530

    GH-2443:correction

--- server/resources/mssql/terraform/modules/ec2/variables.tf
+++ server/resources/mssql/terraform/modules/ec2/variables.tf
@@ -84,9 +84,9 @@ variable "ad_dns_ip_addresses" {
 
 variable "domain_member_sg_id" {
   description = "ID of the domain member security group (for example, sg-9cb7d0e5)."
-   type        = string
+   type       = string
 }
- 
+
 variable "preferred_domain_controller" {
   description = "(optional) Preferred domain controller to use for domain join."
   type        = string
