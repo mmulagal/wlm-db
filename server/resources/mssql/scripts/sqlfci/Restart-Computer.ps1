87d2bbd3fe80dad20260695dba101173914579be
ntapsrigowri <47638406+ntapsrigowri@users.noreply.github.com>
2025-09-04 13:45:41 +0000

    GH-3849: Update to remove unused files (#3847)
    * Update to remove unused files
    
    * Update to remove unused files - added back used files
    
    * Update to remove unused files - added back used files
    
    * Update to remove unused files - added back used files
    
    * Update to remove unused files - added back used files
    
    ---------
    
    Co-authored-by: U, Srigowri <Srigowri.U@netapp.com>

--- server/resources/mssql/scripts/sqlfci/Restart-Computer.ps1
+++
@@ -1,7 +0,0 @@
-﻿[CmdletBinding()]
-param()
-
-Start-Transcript -Path C:\cfn\log\Restart-Computer.ps1.txt -Append
-$ErrorActionPreference = "SilentlyContinue"
-
-Start-Process -FilePath "shutdown.exe" -ArgumentList @("/r", "/t 10") -Wait -NoNewWindow
