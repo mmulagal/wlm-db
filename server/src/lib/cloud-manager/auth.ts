2c63a030af6a403f77a44b61fcd481cefe5493e6
krithib30 <krithib@netapp.com>
2025-09-09 11:27:57 +0530

    GH-3855: Reverts to auth  (#3917)
    * GH-3855: Oracle assessment - For fresh register instances, API should not return timestamp of last assessed data
    
    * GH-3858:Oracle assessment should be n/a when mapped ontap volume fails
    
    * GH-3904: Allow shared route table in FSx MAZ deployment for PGSQL
    
    * GH-3855: Auth reverts
    
    * GH-3855: Auth reverts

--- server/src/lib/cloud-manager/auth.ts
+++ server/src/lib/cloud-manager/auth.ts
@@ -8,6 +8,7 @@ import {
     BXP_TOKEN,
     HEADERS,
     REQUEST_IN_PROGRESS_TYPE,
+    SECRETS,
     WF_SVC_TOKEN_TYPE,
     WF_TOKEN,
     WLMDB,
@@ -55,8 +56,8 @@ async function getWfServiceToken(): Promise<{ token: string; expiresIn: number }
         } = await gotInstanceForInternalRequest
             .post(`${WORKLOAD_FACTORY_ENDPOINT}/auth/v1/auth/token`, {
                 json: {
-                    client_id: 'f4b33665-e96d-4615-9028-ac2a1012226d',
-                    client_secret: 'ddfa645c-14a6-4857-ac28-1d89d5db9921',
+                    client_id: SECRETS.AUTH_CLIENT_ID,
+                    client_secret: SECRETS.AUTH_CLIENT_SECRET,
                     grant_type: 'client_credentials'
                 }
             })
