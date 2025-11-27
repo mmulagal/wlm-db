56db31811f2914b9dbf937ee6f3b9870cd0fe94d
Murali Mulagalapati <155512440+mmulagal@users.noreply.github.com>
2025-11-27 20:10:48 +0530

    GH-5901: Cors issue demo (#5903)
    * GH-1234: CORS demo fix
    
    * GH-1234: CORS demo fix 1
    
    * GH-1234: Address review comments

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
+import path from 'path';
 import config from 'config';
 import randomize from 'randomatic';
 import { JwtPayload } from 'jsonwebtoken';
+import fs from 'fs';
 import './utils/tracer';
 import fastify, { FastifyReply, FastifyRequest } from 'fastify';
 import cors from '@fastify/cors';
@@ -108,7 +110,23 @@ interface Headers {
 await initiateSecrets();
 logger.info('Secrets initiated');
 logger.info('Initializing app');
-const ALLOWED_ORIGINS = new Set(config.get<string[]>('cors.allowed-origins') || []);
+
+// The NODE_ENV is 'demo' for both prod demo and staging demo.
+// As we want to use the urls from production config for prod demo, we need this extra check to identify prod demo.
+const PROD_CONFIG_FILE = path.join(__dirname, 'config/production.json');
+const DEFAULT_CONFIG_FILE = path.join(__dirname, 'config/default.json');
+const ALLOWED_ORIGINS = new Set(
+    process.env.NODE_ENV === 'demo' && process.env.ENVIRONMENT === 'production'
+        ? config.util.loadFileConfigs(PROD_CONFIG_FILE).cors['allowed-origins']
+        : config.get<string[]>('cors.allowed-origins') || []
+);
+
+// This is for checking the env, will remove this code
+if (process.env.NODE_ENV === 'demo' && process.env.ENVIRONMENT !== 'production') {
+    logger.info('I am staging demo', process.env);
+    logger.info(fs.readFileSync(DEFAULT_CONFIG_FILE, 'utf-8'));
+}
+
 const app = fastify({
     trustProxy: true,
     genReqId: () => `WLM-DB-${randomize('Aa0', 8)}`,
