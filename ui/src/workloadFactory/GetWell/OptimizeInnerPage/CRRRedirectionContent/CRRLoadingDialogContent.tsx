9bb67d76f8f9600e3a6e103132ce6c32ffed4efa
Rahul panwar <33785711+rahulpanwarNetapp@users.noreply.github.com>
2026-03-30 20:33:31 +0530

    GH-8315 region correction (#8436)
    * GH-8400 Back button change for BXP
    
    * GH-8400 Back button change for BXP
    
    * GH-8315 fsxVolumeId added for redirection
    
    * GH-8315 region correction

--- ui/src/workloadFactory/GetWell/OptimizeInnerPage/CRRRedirectionContent/CRRLoadingDialogContent.tsx
+++ ui/src/workloadFactory/GetWell/OptimizeInnerPage/CRRRedirectionContent/CRRLoadingDialogContent.tsx
@@ -153,7 +153,7 @@ const CRRLoadingDialogContent = ({ rowData }: CRRLoadingDialogContentProps) => {
                     postBlueXPMessage({
                         type: BlueXPListeners.navigate,
                         payload: {
-                            pathname: `../../wlmfsx/fsx/clusters/${selectedGwInstanceCredId}/ap-southeast-1/${fsxDetails?.id}/replication/create?selectedSrcVolumeIds=${fsxVolumeId}&from=/databases/inventory`,
+                            pathname: `../../wlmfsx/fsx/clusters/${selectedGwInstanceCredId}/${fsxDetails?.region}/${fsxDetails?.id}/replication/create?selectedSrcVolumeIds=${fsxVolumeId}&from=/databases/inventory`,
                             replace: true
                         }
                     });
