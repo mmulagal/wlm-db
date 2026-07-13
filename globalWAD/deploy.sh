08c31758606bf48c7a696653105fc66ee8154d8d
TomerDahan <tomer.dahan@netapp.com>
2026-07-13 15:56:31 +0300

    Update deploy.sh

--- globalWAD/deploy.sh
+++ globalWAD/deploy.sh
@@ -5,7 +5,7 @@ echo "distribution-id $DISTRIBUTION_ID"
 echo "using $1-workloads-components bucket"
 
 echo 'uploading wad config'
-aws s3 cp build-wad/wad-config.json s3://$bucket/wlmdb/wad-config.json --content-type "application/json" --cache-control='public,max-age=300,stale-while-revalidate=60'
+aws s3 cp build/wad-config.json s3://$bucket/wlmdb/wad-config.json --content-type "application/json" --cache-control='public,max-age=300,stale-while-revalidate=60'
 
 echo 'uploading wad tables bundle'
 aws s3 cp build-wad/wad-db-tables.bundle.js s3://$bucket/wlmdb/wad-db-tables.bundle.js --cache-control='public,max-age=300,stale-while-revalidate=60'
