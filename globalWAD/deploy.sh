e90a677e28a74341dd598f05ed2a936f81db9703
TomerDahan <tomer.dahan@netapp.com>
2026-07-15 10:05:45 +0300

    Update deploy.sh

--- globalWAD/deploy.sh
+++ globalWAD/deploy.sh
@@ -5,13 +5,13 @@ echo "distribution-id $DISTRIBUTION_ID"
 echo "using $1-workloads-components bucket"
 
 echo 'uploading wad config'
-aws s3 cp build/wad-config.json s3://$bucket/wlmdb/wad-config.json --content-type "application/json" --cache-control='public,max-age=300,stale-while-revalidate=60'
+aws s3 cp build/wad-config.json s3://$bucket/wlmwad-services/wlmdb/wad-config.json --content-type "application/json" --cache-control='public,max-age=300,stale-while-revalidate=60'
 
 echo 'uploading wad tables bundle'
-aws s3 cp build-wad/wad-db-tables.bundle.js s3://$bucket/wlmdb/wad-db-tables.bundle.js --cache-control='public,max-age=300,stale-while-revalidate=60'
+aws s3 cp build-wad/wad-db-tables.bundle.js s3://$bucket/wlmwad-services/wlmdb/wad-db-tables.bundle.js --cache-control='public,max-age=300,stale-while-revalidate=60'
 
 echo 'uploading wad modals bundle'
-aws s3 cp build-wad/wad-db-modals.bundle.js s3://$bucket/wlmdb/wad-db-modals.bundle.js --cache-control='public,max-age=300,stale-while-revalidate=60'
+aws s3 cp build-wad/wad-db-modals.bundle.js s3://$bucket/wlmwad-services/wlmdb/wad-db-modals.bundle.js --cache-control='public,max-age=300,stale-while-revalidate=60'
 
 if [ -n "$DISTRIBUTION_ID" ]; then
     aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths \/\*
