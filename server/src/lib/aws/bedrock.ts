ab0198abc4fed6456c31bdaf1ebc1dd4abf097a0
Murali Mulagalapati <155512440+mmulagal@users.noreply.github.com>
2026-07-17 12:29:31 +0530

    GH-11127: package update (#11289)
    * GH-11127: Blackduck security issues
    
    * GH-11127: fix bedrock import
    
    * GH-11127: fix nodemailer import

--- server/src/lib/aws/bedrock.ts
+++ server/src/lib/aws/bedrock.ts
@@ -7,7 +7,7 @@ import {
     paginateListInferenceProfiles
 } from '@aws-sdk/client-bedrock';
 import { BedrockRuntimeClient, InvokeModelWithResponseStreamCommand } from '@aws-sdk/client-bedrock-runtime';
-import { Sha256 } from '@aws-crypto/sha256-js';
+import { Sha256 } from '@smithy/core/checksum';
 import { SignatureV4 } from '@smithy/signature-v4';
 import { HttpRequest } from '@smithy/protocol-http';
 import { NodeHttpHandler, streamCollector } from '@smithy/node-http-handler';
