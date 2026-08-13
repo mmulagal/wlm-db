68e368daaafd48520d9f115b5136bcb84a13d4d0
krithib30 <krithib@netapp.com>
2026-08-13 14:17:03 +0530

    GH-11704: Block simulator rmq connection (#11856)

--- server/src/index.ts
+++ server/src/index.ts
@@ -417,7 +417,7 @@ try {
 } catch (error) {
     logger.error('Failed to build and publish wlmdb.json to s3', error);
 }
-if (isActiveInstance()) {
+if (process.env.NODE_ENV !== 'demo' && process.env.NODE_ENV !== 'simulator' && isActiveInstance()) {
     startWadSubscriber().catch(error => {
         logger.error('Failed to start WAD Manager subscriber', error);
     });
