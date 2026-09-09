daaadd4c76b1749d119d16eef759016b3236c9f9
Nithin Radhakrishnan <103916648+nithinr84@users.noreply.github.com>
2026-09-09 16:27:04 +0530

    GH-12207: Skip abort in validate-ami to capture error for mathematica (#12213)
    * GH-12207: Fix standalone deployment of SQL 2025
    
    * GH-12207: Update validate-ami

--- server/resources/mssql/scripts/validation/Validate-Ami.ps1
+++ server/resources/mssql/scripts/validation/Validate-Ami.ps1
@@ -57,11 +57,16 @@ If($ValidWindowsVersion -ne $true) {
 
 #Validate SQL server version
 $sqlServiceList = Get-WmiObject win32_service | ?{$_.DisplayName -like 'sql server (*'}
+Write-Host "SQL service list: $sqlServiceList"
 $ValidSqlVersion = $false
 ForEach ($sqlService in $sqlServiceList) {
 $sqlServiceBinaryPath = $sqlService.PathName  -Replace "-s.*", ""
       If (Test-Path $sqlServiceBinaryPath.Replace('"', '')) {
         $SqlVersion = Invoke-Expression -Command "(dir $sqlServiceBinaryPath).VersionInfo"}
+        Write-Host "VersionInfo:"
+        Write-Host $SqlVersion
+        Write-Host "Path:$sqlServiceBinaryPath"
+        Write-Host "Version:",$SqlVersion.ProductVersion
         $ValidSqlVersion = $SqlVersion.ProductVersion -match '^1[3-9]'
         If($ValidSqlVersion -eq $true) {
         break
@@ -74,9 +79,9 @@ If($ValidSqlVersion -ne $true) {
     if ($IsTerraform) {
         throw $FailureReason
     }
-    Start-Process "cfn-signal.exe" -ArgumentList "-e 1 -r $FailureReason $WaitHandler" -Wait -NoNewWindow
-    Send-CFNResourceSignal -StackName $Stackname -Status FAILURE -LogicalResourceId $ResourceID -UniqueId $InstanceId
-    exit(1)
+    #Start-Process "cfn-signal.exe" -ArgumentList "-e 1 -r $FailureReason $WaitHandler" -Wait -NoNewWindow
+    #Send-CFNResourceSignal -StackName $Stackname -Status FAILURE -LogicalResourceId $ResourceID -UniqueId $InstanceId
+    #exit(1)
 }
 
 #Check if image was created to be part of domain and if domain passed is same.
